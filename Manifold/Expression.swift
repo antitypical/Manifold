62c2edd5855d8e443eb0ee6845b3a77d66bd601a
Rob Rix <rob.rix@github.com>
2015-02-05 02:28:06 -0500

    Expressions.

--- Manifold/Expression.swift
+++ Manifold/Expression.swift
@@ -1,2 +1,11 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
+public enum Expression {
+	case Value(Manifold.Value)
+	case Application(Box<Expression>, Box<Expression>)
+}
+
+
+// MARK: - Imports
+
+import Box
