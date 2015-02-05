d5761316d30c09c043446233b9da0eeda73b4635
Rob Rix <rob.rix@github.com>
2015-02-05 02:36:47 -0500

    Constraints.

--- Manifold/Constraint.swift
+++ Manifold/Constraint.swift
@@ -1,3 +1,12 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
-public typealias Constraint = ()
+public enum Constraint {
+	case And(Box<Constraint>, Box<Constraint>)
+	case Existential(Int, Box<Constraint>)
+	case Congruence(Type, Type)
+}
+
+
+// MARK: - Imports
+
+import Box
