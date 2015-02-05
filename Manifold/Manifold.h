39647fabf3ef0cc114c37893f8ea2e340a9514dd
Rob Rix <rob.rix@github.com>
2015-02-05 02:20:57 -0500

    Initial Commit

---
+++ Manifold/Manifold.h
@@ -0,0 +1,8 @@
+//  Copyright (c) 2015 Rob Rix. All rights reserved.
+
+/// Project version number for Manifold.
+extern double ManifoldVersionNumber;
+
+/// Project version string for Manifold.
+extern const unsigned char ManifoldVersionString[];
+
