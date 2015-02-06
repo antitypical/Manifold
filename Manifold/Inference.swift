144e32620b77f56bee3c0afe5a44150fe6c33e0b
Rob Rix <rob.rix@github.com>
2015-02-06 01:42:35 -0500

    Type inference returns a new constraint.

--- Manifold/Inference.swift
+++ Manifold/Inference.swift
@@ -1,6 +1,6 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
-public func typeOf(term: Expression, constraint: Constraint, environment: Environment) -> Either<Error, Type> {
+public func typeOf(term: Expression, constraint: Constraint, environment: Environment) -> Either<Error, (Constraint, Type)> {
 	return .left("unimplemented")
 }
 
