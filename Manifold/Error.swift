26764544aaeef9e0b506803380fe3ea6975733a7
Rob Rix <rob.rix@github.com>
2015-02-06 01:35:08 -0500

    Errors are Printable.

--- Manifold/Error.swift
+++ Manifold/Error.swift
@@ -1,6 +1,18 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
-public enum Error {
+public enum Error: Printable {
 	case Leaf(String)
 	case Branch([Error])
+
+
+	// MARK: Printable
+
+	public var description: String {
+		switch self {
+		case let Leaf(reason):
+			return reason
+		case let Branch(errors):
+			return join("\n", lazy(errors).map(toString))
+		}
+	}
 }
