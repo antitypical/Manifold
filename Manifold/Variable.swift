f92ddd472db0c13c02b1cc18dc9895fd5b6be191
Rob Rix <rob.rix@github.com>
2015-02-05 22:18:05 -0500

    Type variables are Equatable.

--- Manifold/Variable.swift
+++ Manifold/Variable.swift
@@ -1,5 +1,10 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
-public struct Variable {
+public struct Variable: Equatable {
 	let value: Int
+}
+
+
+public func == (left: Variable, right: Variable) -> Bool {
+	return left.value == right.value
 }
