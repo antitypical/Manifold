e74aac68a58647c44ee1dcb69cb0f27ddfffb046
Rob Rix <rob.rix@github.com>
2015-02-08 14:56:56 -0500

    Add a public constructor for Scheme.

--- Manifold/Scheme.swift
+++ Manifold/Scheme.swift
@@ -1,6 +1,11 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
 public struct Scheme {
+	public init(_ variables: [Variable], _ type: Type) {
+		self.variables = variables
+		self.type = type
+	}
+
 	public let variables: [Variable]
 	public let type: Type
 }
