0a80e1d80294e7d592ce3b9036db00bfc94aad13
Rob Rix <rob.rix@github.com>
2015-02-08 14:13:50 -0500

    Case analysis for Type.

--- Manifold/Type.swift
+++ Manifold/Type.swift
@@ -3,6 +3,17 @@
 public enum Type {
 	case Variable(Manifold.Variable)
 	case Function(Box<Type>, Box<Type>)
+
+
+	public func analysis<T>(ifVariable: Manifold.Variable -> T, _ ifFunction: (Type, Type) -> T) -> T {
+		switch self {
+		case let Variable(v):
+			return ifVariable(v)
+
+		case let Function(t1, t2):
+			return ifFunction(t1.value, t2.value)
+		}
+	}
 }
 
 
