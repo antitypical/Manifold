08f6060c2c0c1c777655b3c9a952bf1503421b60
Rob Rix <rob.rix@github.com>
2015-02-08 17:35:19 -0500

    Environments can be constructed with dictionaries.

--- Manifold/Environment.swift
+++ Manifold/Environment.swift
@@ -1,6 +1,11 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
 public struct Environment: DictionaryLiteralConvertible {
+	public init(_ bindings: [Int: Scheme]) {
+		self.bindings = bindings
+	}
+
+
 	public var freeVariables: Set<Variable> {
 		return reduce(lazy(bindings.values).map { $0.freeVariables }, [], +)
 	}
