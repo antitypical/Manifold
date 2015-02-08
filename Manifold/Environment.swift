8528647773e56766d728e7bc563d0d105a639ec0
Rob Rix <rob.rix@github.com>
2015-02-08 17:32:41 -0500

    Environments have free variables.

--- Manifold/Environment.swift
+++ Manifold/Environment.swift
@@ -1,10 +1,21 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
 public struct Environment: DictionaryLiteralConvertible {
+	public var freeVariables: Set<Variable> {
+		return reduce(lazy(bindings.values).map { $0.freeVariables }, [], +)
+	}
+
+
 	// MARK: DictionaryLiteralConvertible
 	public init(dictionaryLiteral elements: (Int, Scheme)...) {
 		self.bindings = [:] + elements
 	}
 
+
 	private let bindings: [Int: Scheme]
 }
+
+
+// MARK: - Imports
+
+import Set
