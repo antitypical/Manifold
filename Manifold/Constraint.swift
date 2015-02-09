5b54bdb5e90efca0cde967553600c0397a1d21f6
Rob Rix <rob.rix@github.com>
2015-02-09 02:19:37 -0500

    Constraints are Equatable.

--- Manifold/Constraint.swift
+++ Manifold/Constraint.swift
@@ -1,5 +1,15 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
-public enum Constraint {
+public enum Constraint: Equatable {
 	case Equality(Type, Type)
+}
+
+public func == (left: Constraint, right: Constraint) -> Bool {
+	switch (left, right) {
+	case let (.Equality(x1, y1), .Equality(x2, y2)):
+		return x1 == x2 && y1 == y2
+
+	default:
+		return false
+	}
 }
