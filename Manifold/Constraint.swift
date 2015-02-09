4bb00864230b91b8d4c1d114550ef5bdfed7370f
Rob Rix <rob.rix@github.com>
2015-02-09 02:18:08 -0500

    Remove all but Equality constraints.

--- Manifold/Constraint.swift
+++ Manifold/Constraint.swift
@@ -1,25 +1,5 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
 public enum Constraint {
-	public init(_ c1: Constraint, and c2: Constraint) {
-		self = And(Box(c1), Box(c2))
-	}
-
-	public init(exists: Variable, inConstraint: Constraint) {
-		self = Existential(exists, Box(inConstraint))
-	}
-
-	public init(_ t1: Type, equals t2: Type) {
-		self = Congruence(t1, t2)
-	}
-
-
-	case And(Box<Constraint>, Box<Constraint>)
-	case Existential(Variable, Box<Constraint>)
-	case Congruence(Type, Type)
+	case Equality(Type, Type)
 }
-
-
-// MARK: - Imports
-
-import Box
