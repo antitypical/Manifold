2d20d6560c5d2e341734355cdce11826057f4280
Rob Rix <rob.rix@github.com>
2015-02-10 02:13:55 -0500

    AssumptionSet is an array of pairs.

--- Manifold/Inference.swift
+++ Manifold/Inference.swift
@@ -1,13 +1,13 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
-public typealias AssumptionSet = [Expression: Scheme]
+public typealias AssumptionSet = [(Expression, Scheme)]
 
 public typealias ConstraintSet = Multiset<Constraint>
 
-public func typeOf(expression: Expression, _ assumptions: AssumptionSet = [:], _ constraints: ConstraintSet = []) -> Either<Error, (AssumptionSet, ConstraintSet)> {
+public func typeOf(expression: Expression, _ assumptions: AssumptionSet = [], _ constraints: ConstraintSet = []) -> Either<Error, (AssumptionSet, ConstraintSet)> {
 
 	return expression.analysis(
-		const(.right([ expression: Scheme([], Type(Variable())) ], constraints)),
+		const(.right([ (expression, Scheme([], Type(Variable()))) ], constraints)),
 		const(.left("unimplemented")),
 		const(.left("unimplemented"))
 	)
