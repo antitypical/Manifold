0c9f09113437e68b378dd4c34949c02d152f232b
Rob Rix <rob.rix@github.com>
2015-02-10 02:19:25 -0500

    I dunno where that came from.

--- Manifold/Inference.swift
+++ Manifold/Inference.swift
@@ -5,7 +5,6 @@ public typealias AssumptionSet = [(Expression, Scheme)]
 public typealias ConstraintSet = Multiset<Constraint>
 
 public func typeOf(expression: Expression, _ assumptions: AssumptionSet = [], _ constraints: ConstraintSet = []) -> Either<Error, (AssumptionSet, ConstraintSet)> {
-
 	return expression.analysis(
 		const(.right([ (expression, Scheme([], Type(Variable()))) ], constraints)),
 		const(.left("unimplemented")),
