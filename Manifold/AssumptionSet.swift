f832ebabed4eb502cd76b511a7782c72408f2268
Rob Rix <rob.rix@github.com>
2015-02-10 20:25:04 -0500

    AssumptionSet is a struct with subscripts.

--- Manifold/AssumptionSet.swift
+++ Manifold/AssumptionSet.swift
@@ -1,12 +1,36 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
-public typealias AssumptionSet = [Int: [Scheme]]
+public struct AssumptionSet: DictionaryLiteralConvertible, SequenceType {
+	public subscript(variable: Int) -> [Scheme] {
+		get { return assumptions[variable] ?? [] }
+		set { assumptions[variable] = self[variable] + newValue }
+	}
+
+
+	// MARK: DictionaryLiteralConvertible
+
+	public init(dictionaryLiteral elements: (Int, [Scheme])...) {
+		assumptions = [:] + elements
+	}
+
+
+	// MARK: SequenceType
+
+	public func generate() -> GeneratorOf<(Int, [Scheme])> {
+		return GeneratorOf(assumptions.generate())
+	}
+
+
+	// MARK: Private
+
+	private var assumptions: [Int: [Scheme]]
+}
 
 
 public func + (left: AssumptionSet, right: AssumptionSet) -> AssumptionSet {
 	var result = left
 	for (variable, schemes) in right {
-		result[variable] = result[variable] ?? [] + schemes
+		result[variable] = result[variable] + schemes
 	}
 	return result
 }
