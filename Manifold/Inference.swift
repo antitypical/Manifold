b8ab05f7ff74ab4985875a3710a1918ddc162957
Rob Rix <rob.rix@github.com>
2015-02-10 23:59:28 -0500

    typeOf returns labelled tuples.

--- Manifold/Inference.swift
+++ Manifold/Inference.swift
@@ -2,17 +2,21 @@
 
 public typealias ConstraintSet = Multiset<Constraint>
 
-public func typeOf(expression: Expression) -> Either<Error, (Type, AssumptionSet, ConstraintSet)> {
+public func typeOf(expression: Expression) -> Either<Error, (Type, assumptions: AssumptionSet, constraints: ConstraintSet)> {
 	return expression.analysis(
 		ifVariable: { v in
 			let type = Type(Variable())
-			return .right(type, [ v: [ Scheme([], type) ] ], [])
+			return .right(type,
+				assumptions: [ v: [ Scheme([], type) ] ],
+				constraints: [])
 		},
 		ifAbstraction: const(.left("unimplemented")),
 		ifApplication: { e1, e2 in (typeOf(e1) && typeOf(e2)) >>- { e1, e2 in
 			let type = Type(Variable())
 			let c = e1.0 === (e2.0 --> type)
-			return .right(type, e1.1 + e2.1, e1.2 + e2.2 + [ c ])
+			return .right(type,
+				assumptions: e1.assumptions + e2.assumptions,
+				constraints: e1.constraints + e2.constraints + [ c ])
 		}})
 }
 
