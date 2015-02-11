f666ffb0d45da9a6bae6b0c04ca22e069c8e1792
Rob Rix <rob.rix@github.com>
2015-02-10 23:47:20 -0500

    Use === to construct constraints.

--- Manifold/Inference.swift
+++ Manifold/Inference.swift
@@ -11,7 +11,7 @@ public func typeOf(expression: Expression) -> Either<Error, (Type, AssumptionSet
 		ifAbstraction: const(.left("unimplemented")),
 		ifApplication: { e1, e2 in (typeOf(e1) && typeOf(e2)) >>- { e1, e2 in
 			let type = Type(Variable())
-			let c = Constraint(equality: e1.0, e2.0 --> type)
+			let c = e1.0 === (e2.0 --> type)
 			return .right(type, e1.1 + e2.1, e1.2 + e2.2 + [ c ]) }
 		})
 }
