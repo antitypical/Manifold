1cd1dc54a8956f8df6dac3b41b84601139aedac1
Rob Rix <rob.rix@github.com>
2015-02-10 20:17:06 -0500

    AssumptionSet is once more a Dictionary of term variables to type schemes.

--- Manifold/Inference.swift
+++ Manifold/Inference.swift
@@ -1,14 +1,23 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
-public typealias AssumptionSet = [(Int, Scheme)]
+public typealias AssumptionSet = [Int: Scheme]
 
 public typealias ConstraintSet = Multiset<Constraint>
 
 public func typeOf(expression: Expression) -> Either<Error, (AssumptionSet, ConstraintSet)> {
 	return expression.analysis(
-		{ .right([ ($0, Scheme([], Type(Variable()))) ], []) },
+		{ .right([ $0: Scheme([], Type(Variable())) ], []) },
 		const(.left("unimplemented")),
 		{ (typeOf($0) && typeOf($1)) >>- { .right($0.0 + $1.0, $0.1 + $1.1) } })
+}
+
+
+private func + (left: AssumptionSet, right: AssumptionSet) -> AssumptionSet {
+	var result = left
+	for (variable, scheme) in right {
+		result[variable] = scheme
+	}
+	return result
 }
 
 
