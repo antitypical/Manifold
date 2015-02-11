3342cd0d892aa26ce8cd6d71e1c7513f04959762
Rob Rix <rob.rix@github.com>
2015-02-10 23:28:05 -0500

    Nicer formatting.

--- Manifold/Inference.swift
+++ Manifold/Inference.swift
@@ -4,12 +4,16 @@ public typealias ConstraintSet = Multiset<Constraint>
 
 public func typeOf(expression: Expression) -> Either<Error, (Type, AssumptionSet, ConstraintSet)> {
 	return expression.analysis(
-		ifVariable: { v in Type(Variable()) |> { type in .right(type, [ v: [ Scheme([], type) ] ], []) } },
+		ifVariable: { v in
+			let type = Type(Variable())
+			return .right(type, [ v: [ Scheme([], type) ] ], [])
+		},
 		ifAbstraction: const(.left("unimplemented")),
 		ifApplication: { e1, e2 in (typeOf(e1) && typeOf(e2)) >>- { e1, e2 in
 			let type = Type(Variable())
 			let c = Constraint(equality: e1.0, Type(function: e2.0, type))
-			return .right(type, e1.1 + e2.1, e1.2 + e2.2 + [ c ]) } })
+			return .right(type, e1.1 + e2.1, e1.2 + e2.2 + [ c ]) }
+		})
 }
 
 
