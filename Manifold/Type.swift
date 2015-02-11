b10bf8c9c72c8ed410ee18c2fca61068ff87c195
Rob Rix <rob.rix@github.com>
2015-02-11 00:34:45 -0500

    Types include universal quantifiers.
    This subsumes Scheme.

--- Manifold/Type.swift
+++ Manifold/Type.swift
@@ -12,22 +12,27 @@ public enum Type: Hashable {
 
 	case Variable(Manifold.Variable)
 	case Function(Box<Type>, Box<Type>)
+	case Universal(Set<Manifold.Variable>, Box<Type>)
 
 
 	public var freeVariables: Set<Manifold.Variable> {
 		return analysis(
 			ifVariable: { [ $0 ] },
-			ifFunction: { $0.freeVariables.union($1.freeVariables) })
+			ifFunction: { $0.freeVariables.union($1.freeVariables) },
+			ifUniversal: { $1.freeVariables.subtract($0) })
 	}
 
 
-	public func analysis<T>(#ifVariable: Manifold.Variable -> T, ifFunction: (Type, Type) -> T) -> T {
+	public func analysis<T>(#ifVariable: Manifold.Variable -> T, ifFunction: (Type, Type) -> T, ifUniversal: (Set<Manifold.Variable>, Type) -> T) -> T {
 		switch self {
 		case let Variable(v):
 			return ifVariable(v)
 
 		case let Function(t1, t2):
 			return ifFunction(t1.value, t2.value)
+
+		case let Universal(a, t):
+			return ifUniversal(a, t.value)
 		}
 	}
 
@@ -37,7 +42,8 @@ public enum Type: Hashable {
 	public var hashValue: Int {
 		return analysis(
 			ifVariable: { $0.hashValue },
-			ifFunction: { $0.hashValue ^ $1.hashValue }
+			ifFunction: { $0.hashValue ^ $1.hashValue },
+			ifUniversal: { $0.hashValue ^ $1.hashValue }
 		)
 	}
 }
@@ -51,6 +57,9 @@ public func == (left: Type, right: Type) -> Bool {
 
 	case let (.Function(x1, x2), .Function(y1, y2)):
 		return x1.value == y1.value && x2.value == y2.value
+
+	case let (.Universal(a1, t1), .Universal(a2, t2)):
+		return a1 == a2 && t1 == t2
 
 	default:
 		return false
