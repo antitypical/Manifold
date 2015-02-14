48dbac756a31e94f1642a0df3c07c530293db061
Rob Rix <rob.rix@github.com>
2015-02-14 14:09:37 -0500

    Use isVariable/isFunction to test inference.

--- ManifoldTests/InferenceTests.swift
+++ ManifoldTests/InferenceTests.swift
@@ -2,20 +2,22 @@
 
 final class InferenceTests: XCTestCase {
 	func testVariablesAreAssignedAFreshTypeVariable() {
-		assertEqual(infer(0).0, Type(Variable()))
+		assertEqual(infer(0).0.isVariable, true)
 	}
 
 	func testApplicationsAreAssignedAFreshTypeVariable() {
 		let application = 0 <| 0 // fixme: left operand should be an abstraction
-		assertEqual(infer(application).0, Type(Variable()))
+		assertEqual(infer(application).0.isVariable, true)
 	}
 
 	func testAbstractionsAreAssignedAFunctionType() {
 		let abstraction = 0 .. 0
-		assertEqual(infer(abstraction).0, Type(Variable()) --> Type(Variable()))
+		assertEqual(infer(abstraction).0.isFunction, true)
 	}
 }
 
+
+// MARK: - Imports
 
 import Manifold
 import Prelude
