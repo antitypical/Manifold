a24d93e0290edbf12e51d41239c04d71c96850a1
Rob Rix <rob.rix@github.com>
2015-02-15 14:04:01 -0500

    Test Type descriptions.

--- ManifoldTests/TypeTests.swift
+++ ManifoldTests/TypeTests.swift
@@ -22,9 +22,34 @@ final class TypeTests: XCTestCase {
 	}
 
 
+	func testVariableTypesPrintAsSubscriptsOfTau() {
+		let t: Type = .Variable(1234567890)
+		assert(t.description, ==, "τ₁₂₃₄₅₆₇₈₉₀")
+	}
+
 	func testFunctionTypesPrintWithArrow() {
 		let t: Type = .Bool --> .Bool
-		println(t)
+		assert(t.description, ==, "Bool → Bool")
+	}
+
+	func testFunctionTypesParenthesizeParameterFunctions() {
+		let t: Type = (.Bool --> .Bool) --> .Bool
+		assert(t.description, ==, "(Bool → Bool) → Bool")
+	}
+
+	func testFunctionTypesParenthesizeQuantifiedParameterFunctions() {
+		let t: Type = Type(forall: [ 0 ], Type(0) --> .Bool) --> .Bool
+		assert(t.description, ==, "(∀{α₀}.α₀ → Bool) → Bool")
+	}
+
+	func testFunctionTypesDoNotParenthesizeReturnedFunctions() {
+		let t: Type = .Bool --> .Bool --> .Bool
+		assert(t.description, ==, "Bool → Bool → Bool")
+	}
+
+	func testUniversalTypesPrintWithQuantifier() {
+		let t = Type(forall: [ 1, 2 ], .Variable(1) --> .Variable(2) --> .Variable(3))
+		assert(t.description, ==, "∀{α₁,α₂}.α₁ → α₂ → τ₃")
 	}
 }
 
