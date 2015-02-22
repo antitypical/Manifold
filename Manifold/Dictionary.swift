c32fd088410afcb9844ae612ab5792cfff0e96e9
Rob Rix <rob.rix@github.com>
2015-02-22 15:20:10 -0500

    Make the Dictionary initializer explicitly internal.

--- Manifold/Dictionary.swift
+++ Manifold/Dictionary.swift
@@ -1,7 +1,7 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
 extension Dictionary {
-	init<S: SequenceType where S.Generator.Element == Element>(_ elements: S) {
+	internal init<S: SequenceType where S.Generator.Element == Element>(_ elements: S) {
 		self.init()
 		for (key, value) in elements {
 			self[key] = value
