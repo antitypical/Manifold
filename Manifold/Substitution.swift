197b9b420e331465f84b9e200d1a4d051b3ea40c
Rob Rix <rob.rix@github.com>
2015-02-26 21:55:07 -0500

    Predicate find.

--- Manifold/Substitution.swift
+++ Manifold/Substitution.swift
@@ -61,6 +61,13 @@ public func == (left: Substitution, right: Substitution) -> Bool {
 	return left.elements == right.elements
 }
 
+public func find<C: CollectionType>(collection: C, predicate: C.Generator.Element -> Bool) -> C.Index? {
+	for (index, each) in zip(indices(collection), collection) {
+		if predicate(each) { return index }
+	}
+	return nil
+}
+
 
 // MARK: - Imports
 
