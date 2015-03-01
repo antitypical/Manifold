b2d90d2d7090ce793e946225f1fed849eab66b2e
Rob Rix <rob.rix@github.com>
2015-03-01 15:32:59 -0500

    Rename Term’s property to type.

--- Manifold/Term.swift
+++ Manifold/Term.swift
@@ -1,13 +1,13 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
 public struct Term: FixpointType {
-	public init(_ out: Constructor<Term>) {
-		self.out = out
+	public init(_ type: Constructor<Term>) {
+		self.type = type
 	}
 
 	public static func out(term: Term) -> Constructor<Term> {
-		return term.out
+		return term.type
 	}
 
-	public let out: Constructor<Term>
+	public let type: Constructor<Term>
 }
