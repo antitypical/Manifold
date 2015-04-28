bd4cd3da2bae77a7ab63b3363f93bba8d9937d26
Rob Rix <rob.rix@github.com>
2015-04-28 18:52:53 -0400

    Subscript instead of using first.

--- Manifold/Constraint.swift
+++ Manifold/Constraint.swift
@@ -104,7 +104,7 @@ public func unify(t1: Term, t2: Term) -> Either<Error, Substitution> {
 
 public func checkForInconsistencies(partition: [Term]) -> (Error?, Substitution) {
 	typealias Result = (Error?, Substitution, Term)
-	let result: Result = reduce(dropFirst(partition), (nil, [:], first(partition)!)) { into, each in
+	let result: Result = reduce(dropFirst(partition), (nil, [:], partition[0])) { into, each in
 		unify(into.2, each).either(
 			ifLeft: { error in (into.0.map { $0 + error } ?? error, into.1, each) },
 			ifRight: { (into.0, into.1.compose($0), each) })
