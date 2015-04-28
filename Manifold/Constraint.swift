a713980680c5e86cc56bcc49a5eaf6209ce0ed5e
Rob Rix <rob.rix@github.com>
2015-04-28 18:51:15 -0400

    Remove an unused variable.

--- Manifold/Constraint.swift
+++ Manifold/Constraint.swift
@@ -104,7 +104,6 @@ public func unify(t1: Term, t2: Term) -> Either<Error, Substitution> {
 
 public func checkForInconsistencies(partition: [Term]) -> (Error?, Substitution) {
 	typealias Result = (Error?, Substitution, Term)
-	let initial: Result = (nil, [:], Term(Variable()))
 	let result: Result = reduce(dropFirst(partition), (nil, [:], first(partition)!)) { into, each in
 		unify(into.2, each).either(ifLeft: { error in (into.0.map { $0 + error } ?? error, into.1, each) }, ifRight: { (into.0, into.1.compose($0), each) })
 	}
