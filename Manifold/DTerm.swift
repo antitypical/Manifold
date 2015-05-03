4b8b7802d6b7216b42c6e99d2f12bb4e7a46734e
Rob Rix <rob.rix@github.com>
2015-05-03 14:37:42 -0400

    Add cases to DExpression.

--- Manifold/DTerm.swift
+++ Manifold/DTerm.swift
@@ -6,4 +6,14 @@ public struct DTerm {
 
 public enum DExpression<Recur> {
 
+	// MARK: Cases
+
+	case Constant
+	case Variable(Int)
+	case Application(Box<Recur>, Box<Recur>)
+	case Abstraction(Int, Box<Recur>, Box<Recur>)
+	case Quantification(Int, Box<Recur>, Box<Recur>)
 }
+
+
+import Box
