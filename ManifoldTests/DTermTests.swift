5af2e5dfec1346f3e0c9d710b4f0a34c498d4da8
Rob Rix <rob.rix@github.com>
2015-05-03 18:08:49 -0400

    Test that higher-order construction is safe.

---
+++ ManifoldTests/DTermTests.swift
@@ -0,0 +1,15 @@
+//  Copyright (c) 1005 Rob Rix. All rights reserved.
+
+final class DTermTests: XCTestCase {
+	func testHigherOrderConstruction() {
+		let identity = DTerm.lambda { A in (DTerm.type, DTerm.lambda { a in (A, a) }) }
+		let expected = DTerm(.Abstraction(Box(DTerm(.Variable(1, Box(.type)))), Box(DTerm(.Abstraction(Box(DTerm(.Variable(0, Box(DTerm(.Variable(1, Box(.type))))))), Box(DTerm(.Variable(0, Box(DTerm(.Variable(1, Box(DTerm.type))))))))))))
+		assert(identity, ==, expected)
+	}
+}
+
+
+import Assertions
+import Box
+import Manifold
+import XCTest
