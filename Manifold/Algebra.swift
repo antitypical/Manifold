6f7e79edc900046645806be47a9016bf78f50657
Rob Rix <rob.rix@github.com>
2015-05-03 20:03:39 -0400

    Add morphisms over DExpression.

--- Manifold/Algebra.swift
+++ Manifold/Algebra.swift
@@ -7,6 +7,9 @@ public protocol FixpointType {
 	var out: Recur { get }
 }
 
+
+// MARK: Fix: FixpointType over Type<Fix>
+
 public func cata<T, Fix: FixpointType where Fix.Recur == Type<Fix>>(f: Type<T> -> T)(_ term: Fix) -> T {
 	return term |> (out >>> (flip(uncurry(Type.map)) <| cata(f)) >>> f)
 }
@@ -26,6 +29,30 @@ public func ana<T, Fix: FixpointType where Fix.Recur == Type<Fix>>(f: T -> Type<
 public func apo<T, Fix: FixpointType where Fix.Recur == Type<Fix>>(f: T -> Type<Either<Fix, T>>)(_ seed: T) -> Fix {
 	let fanin = flip(uncurry(Either<Fix, T>.either)) <| (id, { apo(f)($0) })
 	return seed |> (`in` <<< (flip(uncurry(Type.map)) <| fanin) <<< f)
+}
+
+
+// MARK: Fix: FixpointType over DExpression<Fix>
+
+public func cata<T, Fix: FixpointType where Fix.Recur == DExpression<Fix>>(f: DExpression<T> -> T)(_ term: Fix) -> T {
+	return term |> (out >>> (flip(uncurry(DExpression.map)) <| cata(f)) >>> f)
+}
+
+
+public func para<T, Fix: FixpointType where Fix.Recur == DExpression<Fix>>(f: DExpression<(Fix, T)> -> T)(_ term: Fix) -> T {
+	let fanout = { ($0, para(f)($0)) }
+	return term |> (out >>> (flip(uncurry(DExpression.map)) <| fanout) >>> f)
+}
+
+
+public func ana<T, Fix: FixpointType where Fix.Recur == DExpression<Fix>>(f: T -> DExpression<T>)(_ seed: T) -> Fix {
+	return seed |> (`in` <<< (flip(uncurry(DExpression.map)) <| ana(f)) <<< f)
+}
+
+
+public func apo<T, Fix: FixpointType where Fix.Recur == DExpression<Fix>>(f: T -> DExpression<Either<Fix, T>>)(_ seed: T) -> Fix {
+	let fanin = flip(uncurry(Either<Fix, T>.either)) <| (id, { apo(f)($0) })
+	return seed |> (`in` <<< (flip(uncurry(DExpression.map)) <| fanin) <<< f)
 }
 
 
