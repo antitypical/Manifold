fc7c8376230cafe7a2032713246d5d31a58f1309
Rob Rix <rob.rix@github.com>
2015-05-03 22:20:17 -0400

    Move out into a FixpointType section.

--- Manifold/DTerm.swift
+++ Manifold/DTerm.swift
@@ -69,10 +69,6 @@ public struct DTerm: FixpointType, Hashable, Printable {
 
 	public let expression: DExpression<DTerm>
 
-	public var out: DExpression<DTerm> {
-		return expression
-	}
-
 
 	// MARK: Type-checking
 
@@ -128,6 +124,13 @@ public struct DTerm: FixpointType, Hashable, Printable {
 			ifVariable: { DTerm.variable($0, $1.substitute(value, forVariable: variable)) },
 			ifApplication: { DTerm.application($0.substitute(value, forVariable: variable), $1.substitute(value, forVariable: variable)) },
 			ifAbstraction: { DTerm.abstraction($0.substitute(value, forVariable: variable), $1.substitute(value, forVariable: variable)) })
+	}
+
+
+	// MARK: FixpointType
+
+	public var out: DExpression<DTerm> {
+		return expression
 	}
 
 
