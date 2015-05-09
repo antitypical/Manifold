1ec7ff54182b68bd9249fb583682ec132bab0509
Rob Rix <rob.rix@github.com>
2015-05-09 11:34:36 -0400

    Test nested quotation.

--- ManifoldTests/ValueTests.swift
+++ ManifoldTests/ValueTests.swift
@@ -4,10 +4,16 @@ final class ValueTests: XCTestCase {
 	func testQuotation() {
 		assert(Value.pi(.Type, id).quote, ==, Term.lambda(.type, id))
 	}
+
+	func testNestedQuotation() {
+		// α-convertible to Term.lambda(.type) { Term.lambda(.kind, const($0)) } — inside out vs. outside in assignment of indices
+		assert(Value.pi(.Type) { Value.pi(.Kind, const($0)) }.quote, ==, Term(.Pi(0, Box(.type), Box(Term(.Pi(1, Box(.kind), Box(Term(.Variable(1)))))))))
+	}
 }
 
 
 import Assertions
+import Box
 import Manifold
 import Prelude
 import XCTest
