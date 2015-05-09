919bff7a534dd3a9f714c6bab95ab80ef0e15189
Rob Rix <rob.rix@github.com>
2015-05-08 22:35:06 -0400

    Case analysis over Value.

--- Manifold/Value.swift
+++ Manifold/Value.swift
@@ -1,6 +1,28 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
 public enum Value {
+	// MARK: Analyses
+
+	public func analysis<T>(
+		@noescape #ifKind: () -> T,
+		@noescape ifType: () -> T,
+		@noescape ifPi: (Value, Value -> Value) -> T,
+		@noescape ifSigma: (Value, Value -> Value) -> T) -> T {
+		switch self {
+		case .Kind:
+			return ifKind()
+		case .Type:
+			return ifType()
+		case let .Pi(type, body):
+			return ifPi(type.value, body)
+		case let .Sigma(type, body):
+			return ifSigma(type.value, body)
+		}
+	}
+
+
+	// MARK: Cases
+
 	case Kind
 	case Type
 	case Pi(Box<Value>, Value -> Value)
