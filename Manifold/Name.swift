08a7cc417eab34db363b18c777bbf6c67e07601a
Rob Rix <rob.rix@github.com>
2015-05-09 20:36:32 -0400

    Names have a value.

--- Manifold/Name.swift
+++ Manifold/Name.swift
@@ -1,6 +1,16 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
 public enum Name {
+	// MARK: Destructors
+
+	public var value: Int {
+		switch self {
+		case let .Local(n):
+			return n
+		case let .Free(n):
+			return n
+		}
+	}
 	// MARK: Cases
 
 	case Local(Int)
