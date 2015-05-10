9f3e5ef691d2f5d98137a19f90b0953e7db9a6b9
Rob Rix <rob.rix@github.com>
2015-05-09 20:04:47 -0400

    Test quotation of variables.

--- ManifoldTests/ValueTests.swift
+++ ManifoldTests/ValueTests.swift
@@ -8,10 +8,19 @@ final class ValueTests: XCTestCase {
 	func testNestedQuotation() {
 		assert(Value.pi(.Type) { Value.pi(.Type, const($0)) }.quote, ==, Term.lambda(.type) { Term.lambda(.type, const($0)) })
 	}
+
+	func testQuotationMapsQuotesToVariables() {
+		assert(Value.Quote(1).quote, ==, Term(.Variable(-2)))
+	}
+
+	func testQuotationMapsNestedQuotesToVariables() {
+		assert(Value.Pi(Box(.Type)) { _ in Value.Pi(Box(.Type), unit) }.quote, ==, Term(.Pi(0, Box(.type), Box(Term(.Pi(1, Box(.type), Box(Term(.Variable(0)))))))))
+	}
 }
 
 
 import Assertions
+import Box
 import Manifold
 import Prelude
 import XCTest
