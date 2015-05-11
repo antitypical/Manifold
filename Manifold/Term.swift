4bbb3f0ee880bf16e574724d2c8a94b958a324a1
Rob Rix <rob.rix@github.com>
2015-05-11 15:50:25 -0400

    Don’t typecheck the body against the argument type.

--- Manifold/Term.swift
+++ Manifold/Term.swift
@@ -120,11 +120,11 @@ public struct Term: DebugPrintable, FixpointType, Hashable, Printable {
 			},
 			ifPi: { i, t, b -> Either<Error, Value> in
 				t.typecheck(environment, .Type)
-					.map { t in Value.Pi(Box(t)) { _ in b.typecheck(environment + [ i: t ], t) } }
+					.map { t in Value.Pi(Box(t)) { _ in b.typecheck(environment + [ i: t ]) } }
 			},
 			ifSigma: { i, t, b -> Either<Error, Value> in
 				t.typecheck(environment, .Type)
-					.map { t in Value.Sigma(Box(t)) { _ in b.typecheck(environment + [ i: t ], t) } }
+					.map { t in Value.Sigma(Box(t)) { _ in b.typecheck(environment + [ i: t ]) } }
 			})
 	}
 
