cfc40dae85c307f487a960dbb71589e5254a23a2
Rob Rix <rob.rix@github.com>
2015-05-13 19:55:30 -0400

    Constant destructor on Term.

--- Manifold/Term.swift
+++ Manifold/Term.swift
@@ -40,6 +40,12 @@ public struct Term: DebugPrintable, FixpointType, Hashable, Printable {
 			otherwise: const(false))
 	}
 
+	public var constant: (Any, Term)? {
+		return expression.analysis(
+			ifConstant: unit,
+			otherwise: const(nil))
+	}
+
 	public var bound: Int? {
 		return expression.analysis(
 			ifBound: unit,
