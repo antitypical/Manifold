9dd077f0bb6affce42c32426f5a44a8b8b52104b
Rob Rix <rob.rix@github.com>
2015-05-16 15:46:29 -0400

    Constructor for the end case.

--- Manifold/Description.swift
+++ Manifold/Description.swift
@@ -1,6 +1,9 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
 public enum Description<Index> {
+	public static func end(index: Index) -> Description {
+		return .End(Box(index))
+	}
 
 	case End(Box<Index>)
 }
