c39028027818c79e16a755121cc97cc3c12bede8
Rob Rix <rob.rix@github.com>
2015-05-26 16:38:29 -0500

    Correct the type of Natural.

--- Manifold/Natural.swift
+++ Manifold/Natural.swift
@@ -1,7 +1,7 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
 public let naturalEnvironment: Environment = [
-	"Natural": Value.forall(const(Value.forall(const(Value.forall(const(Value.forall(const(.Type)))))))),
+	"Natural": Value.forall(const(Value.function(Value.function(.Type, .Type), Value.forall(const(.Type))))),
 	"Zero": Value.forall { F in Value.forall { X in .pi(F, const(.pi(X, id))) } }, // ∀ F : Type . ∀ X : Type . λ f : F . λ x : X . x
 	"Successor": Value.pi(Value.Free("Natural")) { $0.constant.flatMap { i, t in (i as? Int).map { ($0 + 1, t) } }.map(Value.constant) ?? $0 },
 ]
