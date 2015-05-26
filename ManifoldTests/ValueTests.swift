9c3024f360932b5fcce8a525783ede616c5095d2
Rob Rix <rob.rix@github.com>
2015-05-26 13:32:36 -0700

    Use parameters in the tests.

--- ManifoldTests/ValueTests.swift
+++ ManifoldTests/ValueTests.swift
@@ -10,7 +10,7 @@ final class ValueTests: XCTestCase {
 	}
 
 	func testQuotationMapsQuotedVariablesToBoundVariables() {
-		assert(Value.Free(.Quote(1)).quote, ==, Term(.Bound(1)))
+		assert(Value.parameter(.Quote(1)).quote, ==, Term(.Bound(1)))
 	}
 
 	func testQuotationMapsNestedBoundVariablesToBoundVariables() {
