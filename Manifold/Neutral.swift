50f611f6a3c13658612013177fc4c20547bb1d64
Rob Rix <rob.rix@github.com>
2015-05-25 22:46:43 -0700

    Neutral is DebugPrintable.

--- Manifold/Neutral.swift
+++ Manifold/Neutral.swift
@@ -1,6 +1,20 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
-public enum Neutral {
+public enum Neutral: DebugPrintable {
+	// MARK: DebugPrintable
+
+	public var debugDescription: String {
+		switch self {
+		case let .Parameter(n):
+			return n.debugDescription
+		case let .Application(n, v):
+			return "\(toDebugString(n))(\(toDebugString(v)))"
+		}
+	}
+
+
+	// MARK: Cases
+
 	case Parameter(Name)
 	case Application(Box<Neutral>, Value)
 }
