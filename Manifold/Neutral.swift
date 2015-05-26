ac690c73a5bb573813dbee9397c101b9ed15803d
Rob Rix <rob.rix@github.com>
2015-05-25 22:50:09 -0700

    Add analysis to Neutral.

--- Manifold/Neutral.swift
+++ Manifold/Neutral.swift
@@ -1,6 +1,18 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
 public enum Neutral: DebugPrintable {
+	// MARK: Analyses
+
+	func analysis<T>(@noescape #ifParameter: Name -> T, @noescape ifApplication: (Neutral, Value) -> T) -> T {
+		switch self {
+		case let .Parameter(n):
+			return ifParameter(n)
+		case let .Application(n, v):
+			return ifApplication(n.value, v)
+		}
+	}
+
+
 	// MARK: DebugPrintable
 
 	public var debugDescription: String {
