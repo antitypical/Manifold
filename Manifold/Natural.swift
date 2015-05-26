e0a0d6122379af76cdfbaa7307f2743b0cd84329
Rob Rix <rob.rix@github.com>
2015-05-26 16:43:56 -0500

    Church-encoded Successor.

--- Manifold/Natural.swift
+++ Manifold/Natural.swift
@@ -4,7 +4,7 @@ public let naturalEnvironment: Environment = [
 	"Natural": Value.forall(const(Value.function(Value.function(.Type, .Type), Value.forall(const(.Type))))),
 	"Zero": Value.forall { X in .pi(.function(X, X), const(.pi(X, id))) }, // ∀ X : Type . λ f : X → X . λ x : X . x
 	"One": Value.forall { X in Value.pi(.function(X, X)) { f in Value.pi(X) { x in Value.application(f.neutral!, x) } } }, // ∀ F : Type . ∀ X : Type . λ f : F . λ x : X . f(x)
-	"Successor": Value.pi(Value.Free("Natural")) { $0.constant.flatMap { i, t in (i as? Int).map { ($0 + 1, t) } }.map(Value.constant) ?? $0 },
+	"Successor": Value.forall { N in Value.forall { X in Value.pi(N) { n in Value.pi(.function(X, X)) { f in Value.pi(X) { x in .application(f.neutral!, .application(.application(n.neutral!, f), x)) } } } } }, // ∀ N . ∀ F . ∀ X . λ n : N . λ f : F . λ x : X . f ((n f) x)
 ]
 
 
