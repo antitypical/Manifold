46e03a8959e90f30b4a88c1d444bf01a27bd0db5
Rob Rix <rob.rix@github.com>
2015-05-27 18:19:56 -0400

    The Natural environment is one again.

--- Manifold/Natural.swift
+++ Manifold/Natural.swift
@@ -1,11 +1,11 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
-public let naturalEnvironment: [Name: Value] = [
+public let naturalEnvironment: Environment = Environment([], [
 	"Natural": Value.forall(const(Value.function(Value.function(.Type, .Type), Value.forall(const(.Type))))),
 	"Zero": Value.forall { X in .pi(.function(X, X), const(.pi(X, id))) }, // ∀ X : Type . λ f : X → X . λ x : X . x
 	"One": Value.forall { X in Value.pi(.function(X, X)) { f in Value.pi(X) { x in Value.application(f.neutral!, x) } } }, // ∀ F : Type . ∀ X : Type . λ f : F . λ x : X . f(x)
 	"Successor": Value.forall { N in Value.forall { X in Value.pi(N) { n in Value.pi(.function(X, X)) { f in Value.pi(X) { x in .application(f.neutral!, .application(.application(n.neutral!, f), x)) } } } } }, // ∀ N . ∀ F . ∀ X . λ n : N . λ f : F . λ x : X . f ((n f) x)
-]
+])
 
 
 import Prelude
