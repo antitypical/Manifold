262033b44e9ad5c6d916e02b335738a8facf3322
Rob Rix <rob.rix@github.com>
2015-05-26 19:44:09 -0500

    Lookup in contexts.

--- Manifold/Context.swift
+++ Manifold/Context.swift
@@ -3,3 +3,10 @@
 public typealias Type = Value
 
 public typealias Context = [(Name, Type)]
+
+public func lookup<A: Equatable, B, S: SequenceType where S.Generator.Element == (A, B)>(sequence: S, key: A) -> B? {
+	for (k, v) in sequence {
+		if k == key { return v }
+	}
+	return nil
+}
