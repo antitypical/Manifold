6adf4c93d1bd9c9f919a611b1ee0c9deede75970
Rob Rix <rob.rix@github.com>
2015-05-27 16:18:17 -0400

    Implement lookup over dictionaries.

--- Manifold/Context.swift
+++ Manifold/Context.swift
@@ -4,6 +4,10 @@ public typealias Type = Value
 
 public typealias Context = [(Name, Type)]
 
+public func lookup<A: Hashable, B>(dictionary: [A: B], key: A) -> B? {
+	return dictionary[key]
+}
+
 public func lookup<A: Equatable, B, S: SequenceType where S.Generator.Element == (A, B)>(sequence: S, key: A) -> B? {
 	for (k, v) in sequence {
 		if k == key { return v }
