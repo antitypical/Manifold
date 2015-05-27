db1c676ff9f5cc97b3fab51b0692a3608db8ce8c
Rob Rix <rob.rix@github.com>
2015-05-26 20:14:16 -0500

    Temporarily type the Boolean & Natural environments as dictionaries.

--- Manifold/Boolean.swift
+++ Manifold/Boolean.swift
@@ -1,7 +1,7 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
 /// An environment containing Church-encoded booleans.
-public let booleanEnvironment: Environment = [
+public let booleanEnvironment: [Name: Value] = [
 	"Boolean": Value.pi(.Type) { bool in Value.pi(bool, const(Value.pi(bool, const(bool)))) },
 	"True": Value.pi(.parameter("Boolean")) { t in Value.pi(.parameter("Boolean"), const(t)) },
 	"False": Value.pi(.parameter("Boolean")) { _ in Value.pi(.parameter("Boolean")) { f in f } },
--- Manifold/Natural.swift
+++ Manifold/Natural.swift
@@ -1,6 +1,6 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
-public let naturalEnvironment: Environment = [
+public let naturalEnvironment: [Name: Value] = [
 	"Natural": Value.forall(const(Value.function(Value.function(.Type, .Type), Value.forall(const(.Type))))),
 	"Zero": Value.forall { X in .pi(.function(X, X), const(.pi(X, id))) }, // ∀ X : Type . λ f : X → X . λ x : X . x
 	"One": Value.forall { X in Value.pi(.function(X, X)) { f in Value.pi(X) { x in Value.application(f.neutral!, x) } } }, // ∀ F : Type . ∀ X : Type . λ f : F . λ x : X . f(x)
