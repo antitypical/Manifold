8c9603fd7cc915a6ebac6cf62479307f50c01848
Rob Rix <rob.rix@github.com>
2015-05-27 18:21:24 -0400

    Attempt to give the correct type for Natural.

--- ManifoldTests/NaturalTests.swift
+++ ManifoldTests/NaturalTests.swift
@@ -1,14 +1,13 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
 final class NaturalTests: XCTestCase {
-//	func testReferencesToZeroAreWellTyped() {
-//		let value = Zero.typecheck(naturalEnvironment).right
-//		let type = Natural.evaluate(naturalEnvironment).right
-//		assert(value, !=, nil)
-//		assert(type, !=, nil)
-//		assert(value?.quote, ==, type?.quote)
-//	}
-//
+	func testReferencesToZeroAreWellTyped() {
+		let value = Zero.typecheck(naturalEnvironment.global, from: 0).right
+		let type = Natural.evaluate(naturalEnvironment)
+		assert(value, !=, nil)
+		assert(value?.quote, ==, type.quote)
+	}
+
 //	func testReferencesToOneAreWellTyped() {
 //		let value = One.typecheck(naturalEnvironment).right
 //		let type = Natural.evaluate(naturalEnvironment).right
