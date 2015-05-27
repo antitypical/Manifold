3643f746bc7abc293c15ca830112d22d606fb1a9
Rob Rix <rob.rix@github.com>
2015-05-27 16:18:46 -0400

    Remove the old lookup function.

--- Manifold/Context.swift
+++ Manifold/Context.swift
@@ -7,10 +7,3 @@ public typealias Context = [Name: Type]
 public func lookup<A: Hashable, B>(dictionary: [A: B], key: A) -> B? {
 	return dictionary[key]
 }
-
-public func lookup<A: Equatable, B, S: SequenceType where S.Generator.Element == (A, B)>(sequence: S, key: A) -> B? {
-	for (k, v) in sequence {
-		if k == key { return v }
-	}
-	return nil
-}
