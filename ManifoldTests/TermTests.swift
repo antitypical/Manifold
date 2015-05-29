c8d3b1c3ee1631763c716fa558c191ae563187b0
Rob Rix <rob.rix@github.com>
2015-05-29 02:37:54 -0400

    Names can be constructed arbitrarily.

--- ManifoldTests/TermTests.swift
+++ ManifoldTests/TermTests.swift
@@ -64,6 +64,22 @@ final class TermTests: XCTestCase {
 	}
 }
 
+extension Name: Arbitrary {
+	public static func arbitrary() -> Gen<Name> {
+		return Gen.oneOf([
+			String.arbitrary().fmap(Name.global),
+			Int.arbitrary().fmap(Name.local),
+		])
+	}
+
+	public static func shrink(name: Name) -> [Name] {
+		return name.analysis(
+			ifGlobal: { String.shrink($0).map(Name.global) },
+			ifLocal: { Int.shrink($0).map(Name.local) },
+			ifQuote: const(shrinkNone(name)))
+	}
+}
+
 
 private let identity = Value.pi(.type) { A in .pi(A, id) }.quote
 private let constant = Value.pi(.type) { A in Value.pi(.type) { B in Value.pi(A) { a in Value.pi(B) { b in a } } } }.quote
@@ -74,4 +90,5 @@ import Box
 import Either
 import Manifold
 import Prelude
+import SwiftCheck
 import XCTest
