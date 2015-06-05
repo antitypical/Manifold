0cf59a86427cb6b39023599fc45cdd3b6030c507
Rob Rix <rob.rix@github.com>
2015-06-04 20:05:03 -0400

    Comment out Booleans while we go through some churn.

--- Manifold/Boolean.swift
+++ Manifold/Boolean.swift
@@ -1,11 +1,11 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
 /// An environment containing Church-encoded booleans.
-public let booleanEnvironment: Environment = Environment([], [
-	"Boolean": Value.pi(.type) { bool in Value.pi(bool, const(Value.pi(bool, const(bool)))) },
-	"True": Value.pi(.free("Boolean")) { t in Value.pi(.free("Boolean"), const(t)) },
-	"False": Value.pi(.free("Boolean")) { _ in Value.pi(.free("Boolean")) { f in f } },
-])
+//public let booleanEnvironment: Environment = Environment([], [
+//	"Boolean": Value.pi(.type) { bool in Value.pi(bool, const(Value.pi(bool, const(bool)))) },
+//	"True": Value.pi(.free("Boolean")) { t in Value.pi(.free("Boolean"), const(t)) },
+//	"False": Value.pi(.free("Boolean")) { _ in Value.pi(.free("Boolean")) { f in f } },
+//])
 
 
 import Prelude
