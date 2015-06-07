bc42095d523b9b4f504d56cae58c7668bb05078f
Rob Rix <rob.rix@github.com>
2015-06-07 17:38:56 -0400

    Add a (partially failing) test of projection types.

--- ManifoldTests/TermTests.swift
+++ ManifoldTests/TermTests.swift
@@ -62,6 +62,13 @@ final class TermTests: XCTestCase {
 	func testGlobalsPrintTheirNames() {
 		assert(Term(.Free("Global")).description, ==, "Global")
 	}
+
+
+	func testProjectionTypeIsTypeOfProjectedField() {
+		let product = Term.product(.type(1), .type(2))
+		assert(Term.projection(product, true).typecheck().right?.quote, ==, Term.type(3))
+		assert(Term.projection(product, false).typecheck().right?.quote, ==, Term.type(2))
+	}
 }
 
 
