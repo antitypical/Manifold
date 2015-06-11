b70222611cf8cf1fe1201e4be365e6a9b6f7a846
Rob Rix <rob.rix@github.com>
2015-06-11 18:21:27 -0400

    Add some simple tests.

--- ManifoldTests/EvaluationTests.swift
+++ ManifoldTests/EvaluationTests.swift
@@ -1,7 +1,17 @@
 //  Copyright © 2015 Rob Rix. All rights reserved.
 
 final class EvaluationTests: XCTestCase {
-	
+	func testUnitTermEvaluatesToItself() {
+		assert(Term.unitTerm.evaluate().quote, ==, Term.unitTerm)
+	}
+
+	func testUnitTypeEvaluatesToItself() {
+		assert(Term.unitType.evaluate().quote, ==, Term.unitType)
+	}
+
+	func testTypeEvaluatesToItself() {
+		assert(Term.type.evaluate().quote, ==, Term.type)
+	}
 }
 
 import Assertions
