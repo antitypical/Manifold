e748f075b87d9adb5ecadfeec6367e9cc4120a2f
Rob Rix <rob.rix@github.com>
2015-06-10 21:40:21 -0400

    Go to some effort to avoid Swift compiler crashes.
    …but still suffer other Swift compiler crashes.

--- Manifold/Algebra.swift
+++ Manifold/Algebra.swift
@@ -11,26 +11,30 @@ public protocol FixpointType {
 // MARK: Fix: FixpointType over Checkable<Fix>
 
 public func cata<T, Fix: FixpointType where Fix.Recur == Checkable<Fix>>(f: Checkable<T> -> T)(_ term: Fix) -> T {
-	return term |> (out >>> (flip(uncurry(Checkable.map)) <| cata(f)) >>> f)
+	return term |> (out >>> (map <| cata(f)) >>> f)
 }
 
 
 public func para<T, Fix: FixpointType where Fix.Recur == Checkable<Fix>>(f: Checkable<(Fix, T)> -> T)(_ term: Fix) -> T {
 	let fanout = { ($0, para(f)($0)) }
-	return term |> (out >>> (flip(uncurry(Checkable.map)) <| fanout) >>> f)
+	return term |> (out >>> (map <| fanout) >>> f)
 }
 
 
 public func ana<T, Fix: FixpointType where Fix.Recur == Checkable<Fix>>(f: T -> Checkable<T>)(_ seed: T) -> Fix {
-	return seed |> (`in` <<< (flip(uncurry(Checkable.map)) <| ana(f)) <<< f)
+	return seed |> (`in` <<< (map <| ana(f)) <<< f)
 }
 
 
 public func apo<T, Fix: FixpointType where Fix.Recur == Checkable<Fix>>(f: T -> Checkable<Either<Fix, T>>)(_ seed: T) -> Fix {
-	let fanin = flip(uncurry(Either<Fix, T>.either)) <| (id, { apo(f)($0) })
-	return seed |> (`in` <<< (flip(uncurry(Checkable.map)) <| fanin) <<< f)
+	let fanin: Either<Fix, T> -> Fix = { $0.either(ifLeft: id, ifRight: { apo(f)($0) }) }
+	return seed |> (`in` <<< (map <| fanin) <<< f)
 }
 
+
+private func map<T, U>(f: T -> U)(_ c: Checkable<T>) -> Checkable<U> {
+	return Checkable.map(c)(f)
+}
 
 private func `in`<Fix: FixpointType>(v: Fix.Recur) -> Fix {
 	return Fix(v)
