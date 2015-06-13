57e19180049ad40fd10f60bd70acd61037cf3f3c
Rob Rix <rob.rix@github.com>
2015-06-13 19:29:35 -0400

    And use Value equality directly here.

--- Manifold/Term.swift
+++ Manifold/Term.swift
@@ -185,7 +185,7 @@ public struct Term: CustomDebugStringConvertible, FixpointType, Hashable, Custom
 		return typecheck(context, from: i)
 			.flatMap { t in
 				let (q, r) = (t.quote, against.quote)
-				return (t == against) || (against == .type && q == Value.function(.type, .type).quote)
+				return (t == against) || (against == .type && t == Value.function(.type, .type))
 					? Either.right(t)
 					: Either.left("type mismatch: expected (\(String(reflecting: self))) : (\(String(reflecting: r))), actually (\(String(reflecting: self))) : (\(String(reflecting: q))) in environment \(context)")
 			}
