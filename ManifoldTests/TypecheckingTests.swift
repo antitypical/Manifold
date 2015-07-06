ce2b6cfd92fb971f1aad13dfce9fae908bd4b2b2
Rob Rix <rob.rix@github.com>
2015-07-05 21:27:10 -0400

    Add a test of dependent product typechecking.

--- ManifoldTests/TypecheckingTests.swift
+++ ManifoldTests/TypecheckingTests.swift
@@ -44,6 +44,11 @@ final class TypecheckingTests: XCTestCase {
 	func testProductTypechecksToSigmaType() {
 		assert(Expression.Product(Term(.Unit), Term(false)).typecheck().right, ==, Expression.lambda(Term(.UnitType), const(Term(.BooleanType))))
 	}
+
+	func testDependentProductTypechecksToSigmaType() {
+		let type = Expression.lambda(Term(.BooleanType)) { c in Term(.If(c, Term(.UnitType), Term(.BooleanType))) }
+		assert(Expression.Annotation(Term(.Product(Term(true), Term(.Unit))), Term(type)).typecheck().right, ==, type)
+	}
 }
 
 import Assertions
