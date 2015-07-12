6f91877c06a5af84bc920790dcbabf1032915547
Rob Rix <rob.rix@github.com>
2015-07-12 15:59:09 -0400

    Avoid some pointless abbreviations.

--- Manifold/Description.swift
+++ Manifold/Description.swift
@@ -2,18 +2,18 @@
 
 extension Expression where Recur: FixpointType {
 	public static var description: Module<Recur> {
-		// Tag : λ _ : Enum . Type
-		// Tag = λ E : Enum . λ _ : Boolean . Tag E
+		// Tag : λ _ : Enumeration . Type
+		// Tag = λ E : Enumeration . λ _ : Boolean . Tag E
 		let Tag = Binding("Tag",
-			lambda(.Variable("Enum")) { E in Recur.lambda(.BooleanType) { _ in
+			lambda(.Variable("Enumeration")) { E in Recur.lambda(.BooleanType) { _ in
 				.Application(.Variable("Tag"), E)
 			} },
-			lambda(.Variable("Enum"), const(.Type(0))))
+			lambda(.Variable("Enumeration"), const(.Type(0))))
 
 		return Module([ list ], [
 			Binding("String", .Axiom(String.self, .Type(0)), .Type(0)),
 			Binding("Label", .Variable("String"), .Type(0)),
-			Binding("Enum", .Application(.Variable("List"), .Variable("Label")), .Type(0)),
+			Binding("Enumeration", .Application(.Variable("List"), .Variable("Label")), .Type(0)),
 
 			Tag,
 		])
