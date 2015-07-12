91e5ea0cccae8fbe4b97298cbb7ac1466d075180
Rob Rix <rob.rix@github.com>
2015-07-11 23:45:58 -0400

    Annotate the types/terms of List & Natural definitions with comments.

--- Manifold/List.swift
+++ Manifold/List.swift
@@ -2,18 +2,24 @@
 
 extension Expression where Recur: FixpointType {
 	public static var List: Definition {
+		// List : λ A : Type . Type
+		// List = λ A : Type . λ tag : Boolean . if tag then (A, List A) else Unit
 		return (symbol: "List",
 			value: lambda(.Type) { A in Recur.lambda(.BooleanType) { .If($0, .Product(A, .Application(.Variable("List"), A)), .UnitType) } },
 			type: lambda(.Type, const(.Type)))
 	}
 
 	public static var `nil`: Definition {
+		// nil : List
+		// nil = (false, ()) : List
 		return (symbol: "nil",
 			value: .Annotation(.Product(.Boolean(false), .Unit), .Variable("List")),
 			type: .Variable("List"))
 	}
 
 	public static var cons: Definition {
+		// cons : λ A : Type . λ _ : A . λ _ : List A . List A
+		// cons = λ A : Type . λ first : A . λ rest : List A . (true, (first, rest)) : List A
 		return (symbol: "cons",
 			value: lambda(.Type) { A in Recur.lambda(A) { first in Recur.lambda(.Application(.Variable("List"), A)) { rest in .Annotation(.Product(.Boolean(true), .Product(first, rest)), .Variable("List")) } } },
 			type: .Variable("List"))
--- Manifold/Natural.swift
+++ Manifold/Natural.swift
@@ -2,18 +2,24 @@
 
 extension Expression where Recur: FixpointType {
 	public static var Natural: Definition {
+		// Natural : Type
+		// Natural = λ tag : Boolean . if tag then Natural else Unit
 		return (symbol: "Natural",
 			value: lambda(.BooleanType) { .If($0, .Variable("Natural"), .UnitType) },
 			type: lambda(.BooleanType, const(.Type(0))))
 	}
 
 	public static var zero: Definition {
+		// zero : Natural
+		// zero = (false, ()) : Natural
 		return (symbol: "zero",
 			value: .Annotation(.Product(.Boolean(false), .Unit), .Variable("Natural")),
 			type: .Variable("Natural"))
 	}
 
 	public static var successor: Definition {
+		// successor : Natural -> Natural
+		// successor = λ n : Natural . (true, n) : Natural
 		return (symbol: "successor",
 			value: lambda(.Variable("Natural")) { predecessor in .Annotation(.Product(.Boolean(true), predecessor), .Variable("Natural")) },
 			type: lambda(.Variable("Natural"), const(.Variable("Natural"))))
