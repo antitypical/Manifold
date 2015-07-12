411d9220842e4ad13da457cd2a1eacb68a365915
Rob Rix <rob.rix@github.com>
2015-07-12 01:00:04 -0400

    Bindings have symbols, not names.

--- Manifold/Binding.swift
+++ Manifold/Binding.swift
@@ -1,13 +1,13 @@
 //  Copyright © 2015 Rob Rix. All rights reserved.
 
 public struct Binding<Recur> {
-	public init(_ name: String, _ value: Expression<Recur>, _ type: Expression<Recur>) {
-		self.name = name
+	public init(_ symbol: String, _ value: Expression<Recur>, _ type: Expression<Recur>) {
+		self.symbol = symbol
 		self.value = value
 		self.type = type
 	}
 
-	public let name: String
+	public let symbol: String
 	public let value: Expression<Recur>
 	public let type: Expression<Recur>
 }
