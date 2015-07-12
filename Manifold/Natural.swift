d577a718d69d78e772b1ba4b32b5b667bf5ed1fb
Rob Rix <rob.rix@github.com>
2015-07-12 09:40:48 -0400

    Hide Natural bindings.

--- Manifold/Natural.swift
+++ Manifold/Natural.swift
@@ -1,31 +1,25 @@
 //  Copyright © 2015 Rob Rix. All rights reserved.
 
 extension Expression where Recur: FixpointType {
-	public static var Natural: Binding<Recur> {
+	public static var natural: Module<Recur> {
 		// Natural : Type
 		// Natural = λ tag : Boolean . if tag then Natural else Unit
-		return Binding("Natural",
+		let Natural = Binding("Natural",
 			lambda(.BooleanType) { .If($0, .Variable("Natural"), .UnitType) },
 			lambda(.BooleanType, const(.Type(0))))
-	}
 
-	public static var zero: Binding<Recur> {
 		// zero : Natural
 		// zero = (false, ()) : Natural
-		return Binding("zero",
-			.Annotation(.Product(.Boolean(false), .Unit), .Variable("Natural")),
+		let zero = Binding("zero",
+			.Annotation(Recur.Product(.Boolean(false), .Unit), .Variable("Natural")),
 			.Variable("Natural"))
-	}
 
-	public static var successor: Binding<Recur> {
 		// successor : Natural -> Natural
 		// successor = λ n : Natural . (true, n) : Natural
-		return Binding("successor",
+		let successor = Binding("successor",
 			lambda(.Variable("Natural")) { predecessor in .Annotation(.Product(.Boolean(true), predecessor), .Variable("Natural")) },
 			lambda(.Variable("Natural"), const(.Variable("Natural"))))
-	}
 
-	public static var natural: Module<Recur> {
 		return Module([ Natural, zero, successor ])
 	}
 }
