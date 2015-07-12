7c4c40174b0b58ed74b98ab8fdacd0f1c18a4337
Rob Rix <rob.rix@github.com>
2015-07-11 23:45:37 -0400

    Style I guess.

--- Manifold/Natural.swift
+++ Manifold/Natural.swift
@@ -2,22 +2,19 @@
 
 extension Expression where Recur: FixpointType {
 	public static var Natural: Definition {
-		return (
-			symbol: "Natural",
+		return (symbol: "Natural",
 			value: lambda(.BooleanType) { .If($0, .Variable("Natural"), .UnitType) },
 			type: lambda(.BooleanType, const(.Type(0))))
 	}
 
 	public static var zero: Definition {
-		return (
-			symbol: "zero",
+		return (symbol: "zero",
 			value: .Annotation(.Product(.Boolean(false), .Unit), .Variable("Natural")),
 			type: .Variable("Natural"))
 	}
 
 	public static var successor: Definition {
-		return (
-			symbol: "successor",
+		return (symbol: "successor",
 			value: lambda(.Variable("Natural")) { predecessor in .Annotation(.Product(.Boolean(true), predecessor), .Variable("Natural")) },
 			type: lambda(.Variable("Natural"), const(.Variable("Natural"))))
 	}
