f0848c3abbfa090e6f38b7cbf0ddf27baed4b9a7
Rob Rix <rob.rix@github.com>
2015-07-11 20:01:19 -0400

    Define the Natural context/environment in a single pass.

--- Manifold/Natural.swift
+++ Manifold/Natural.swift
@@ -13,20 +13,12 @@ extension Expression where Recur: FixpointType {
 		return lambda(Recur(.Variable("Natural"))) { predecessor in Recur(lambda(Recur(true), const(predecessor))) }
 	}
 
-	public static var naturalEnvironment: [Name: Expression] {
-		return [
-			"Natural": Natural,
-			"zero": zero,
-			"successor": successor,
-		]
-	}
-
-	public static var naturalContext: [Name: Expression] {
-		return [
-			"Natural": lambda(Recur(.BooleanType), const(Recur(.Type(0)))),
-			"zero": .Variable("Natural"),
-			"successor": lambda(Recur(.Variable("Natural")), const(Recur(.Variable("Natural")))),
-		]
+	public static var natural: Space {
+		return defineSpace([
+			("Natural", Natural, lambda(Recur(.BooleanType), const(Recur(.Type(0))))),
+			("zero", zero, .Variable("Natural")),
+			("successor", successor, lambda(Recur(.Variable("Natural")), const(Recur(.Variable("Natural")))))
+		])
 	}
 }
 
