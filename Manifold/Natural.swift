9b9cdd97d2f31c64c4fa8b218616b03eb7fbea0a
Rob Rix <rob.rix@github.com>
2015-07-11 22:24:54 -0400

    Define Natural in terms of FixpointType constructors.

--- Manifold/Natural.swift
+++ Manifold/Natural.swift
@@ -2,22 +2,22 @@
 
 extension Expression where Recur: FixpointType {
 	public static var Natural: Expression {
-		return lambda(Recur(.BooleanType)) { Recur(.If($0, Recur(.Variable("Natural")), Recur(.UnitType))) }
+		return lambda(.BooleanType) { .If($0, .Variable("Natural"), .UnitType) }
 	}
 
 	public static var zero: Expression {
-		return .Annotation(Recur(.Product(Recur(false), Recur(.Unit))), Recur(.Variable("Natural")))
+		return .Annotation(.Product(.Boolean(false), .Unit), .Variable("Natural"))
 	}
 
 	public static var successor: Expression {
-		return lambda(Recur(.Variable("Natural"))) { predecessor in Recur(.Annotation(Recur(lambda(Recur(true), const(predecessor))), Recur(.Variable("Natural")))) }
+		return lambda(.Variable("Natural")) { predecessor in .Annotation(.lambda(.Boolean(true), const(predecessor)), .Variable("Natural")) }
 	}
 
 	public static var natural: Space {
 		return defineSpace([
-			("Natural", Natural, lambda(Recur(.BooleanType), const(Recur(.Type(0))))),
+			("Natural", Natural, lambda(.BooleanType, const(.Type(0)))),
 			("zero", zero, .Variable("Natural")),
-			("successor", successor, lambda(Recur(.Variable("Natural")), const(Recur(.Variable("Natural")))))
+			("successor", successor, lambda(.Variable("Natural"), const(.Variable("Natural"))))
 		])
 	}
 }
