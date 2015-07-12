dca15b0c10bb261a9bfa495ba3a56c563c837179
Rob Rix <rob.rix@github.com>
2015-07-11 23:21:10 -0400

    Natural is exposed as top-level definitions.

--- Manifold/Natural.swift
+++ Manifold/Natural.swift
@@ -1,23 +1,32 @@
 //  Copyright © 2015 Rob Rix. All rights reserved.
 
 extension Expression where Recur: FixpointType {
-	public static var Natural: Expression {
-		return lambda(.BooleanType) { .If($0, .Variable("Natural"), .UnitType) }
+	public static var Natural: Definition {
+		return (
+			symbol: "Natural",
+			value: lambda(.BooleanType) { .If($0, .Variable("Natural"), .UnitType) },
+			type: lambda(.BooleanType, const(.Type(0))))
 	}
 
-	public static var zero: Expression {
-		return .Annotation(.Product(.Boolean(false), .Unit), .Variable("Natural"))
+	public static var zero: Definition {
+		return (
+			symbol: "zero",
+			value: .Annotation(.Product(.Boolean(false), .Unit), .Variable("Natural")),
+			type: .Variable("Natural"))
 	}
 
-	public static var successor: Expression {
-		return lambda(.Variable("Natural")) { predecessor in .Annotation(.lambda(.Boolean(true), const(predecessor)), .Variable("Natural")) }
+	public static var successor: Definition {
+		return (
+			symbol: "successor",
+			value: lambda(.Variable("Natural")) { predecessor in .Annotation(.lambda(.Boolean(true), const(predecessor)), .Variable("Natural")) },
+			type: lambda(.Variable("Natural"), const(.Variable("Natural"))))
 	}
 
 	public static var natural: Space {
 		return defineSpace([
-			("Natural", Natural, lambda(.BooleanType, const(.Type(0)))),
-			("zero", zero, .Variable("Natural")),
-			("successor", successor, lambda(.Variable("Natural"), const(.Variable("Natural"))))
+			Natural,
+			zero,
+			successor
 		])
 	}
 }
