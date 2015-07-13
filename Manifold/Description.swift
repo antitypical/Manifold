2a40dd087b8bab7e032ec2086fab442e4847085e
Rob Rix <rob.rix@github.com>
2015-07-12 21:08:02 -0400

    Rename tag/Tag to swap them.

--- Manifold/Description.swift
+++ Manifold/Description.swift
@@ -2,7 +2,7 @@
 
 extension Expression where Recur: FixpointType {
 	public static var description: Module<Recur> {
-		let tag = Recur("Tag")
+		let Tag = Recur("Tag")
 		let enumeration = Recur("Enumeration")
 		let label = Recur("Label")
 		let cons = Recur("::")
@@ -10,21 +10,21 @@ extension Expression where Recur: FixpointType {
 
 		// Tag : λ _ : Enumeration . Type
 		// Tag = λ E : Enumeration . λ _ : Boolean . Tag E
-		let Tag = Binding("Tag",
-			lambda(enumeration, .BooleanType) { E, _ in tag[E] },
+		let tag = Binding("Tag",
+			lambda(enumeration, .BooleanType) { E, _ in Tag[E] },
 			lambda(enumeration, const(.Type(0))))
 
 		// here : λ first : Label . λ rest : Enumeration . Tag (first :: rest)
 		// here = λ first : Label . (true, first) : Tag (first :: rest)
 		let here = Binding("here",
-			lambda(label, enumeration) { first, rest in .Annotation(.Product(.Boolean(false), .Unit), tag[cons[first, rest]]) },
-			lambda(label, enumeration) { first, rest in tag[cons[first, rest]] })
+			lambda(label, enumeration) { first, rest in .Annotation(.Product(.Boolean(false), .Unit), Tag[cons[first, rest]]) },
+			lambda(label, enumeration) { first, rest in Tag[cons[first, rest]] })
 
 		// there : λ first : Label . λ rest : Enumeration . Tag (first :: rest)
 		// there = λ first : Label . (false, rest) : Tag (first :: rest)
 		let there = Binding("there",
-			lambda(label, enumeration) { first, rest in Recur.lambda(tag[rest]) { next in .Annotation(.Product(.Boolean(true), next), tag[cons[first, rest]]) } },
-			lambda(label, enumeration) { first, rest in tag[cons[first, rest]] })
+			lambda(label, enumeration) { first, rest in Recur.lambda(Tag[rest]) { next in .Annotation(.Product(.Boolean(true), next), Tag[cons[first, rest]]) } },
+			lambda(label, enumeration) { first, rest in Tag[cons[first, rest]] })
 
 
 		// Branches : λ E : Enumeration . λ _ : (λ _ : Tag E . Type) . Type
@@ -33,18 +33,18 @@ extension Expression where Recur: FixpointType {
 		//     else Unit
 		let Branches = Binding("Branches",
 			lambda(enumeration) { E in
-				Recur.lambda(.lambda(tag[E], const(.Type))) { P in
+				Recur.lambda(.lambda(Tag[E], const(.Type))) { P in
 					.If(.Projection(E, false),
 						.Product(
 							P[Recur("here")],
-							branches[E, Recur.lambda(tag[E]) { t in
+							branches[E, Recur.lambda(Tag[E]) { t in
 								P[Recur("there")[t]]
 							}]),
 						.Unit)
 				}
 			},
 			lambda(enumeration) { E in
-				.lambda(.lambda(tag[E], const(.Type)), const(.Type))
+				.lambda(.lambda(Tag[E], const(.Type)), const(.Type))
 			})
 
 		// case : λ E : Enumeration . λ P : (λ _ : Tag E . Type) . λ cs : Branches E P . λ t : Tag E . P t
@@ -53,17 +53,17 @@ extension Expression where Recur: FixpointType {
 		//     else cs.0
 		let `case` = Binding("case",
 			lambda(enumeration) { E in
-				Recur.lambda(Recur.lambda(tag[E], const(.Type))) { P in
-					Recur.lambda(branches[E, P]) { cs in Recur.lambda(tag[E]) { t in
+				Recur.lambda(Recur.lambda(Tag[E], const(.Type))) { P in
+					Recur.lambda(branches[E, P]) { cs in Recur.lambda(Tag[E]) { t in
 						.If(.Projection(t, false),
-							Recur("case")[E, Recur.lambda(tag[E]) { t in P[Recur("there")[t]] }, .Projection(cs, true), t],
+							Recur("case")[E, Recur.lambda(Tag[E]) { t in P[Recur("there")[t]] }, .Projection(cs, true), t],
 							.Projection(cs, false))
 					} }
 				}
 			},
 			lambda(enumeration) { E in
-				Recur.lambda(.lambda(tag[E], const(.Type))) { P in
-					.lambda(branches[E, P], const(Recur.lambda(tag[E]) { t in P[t] }))
+				Recur.lambda(.lambda(Tag[E], const(.Type))) { P in
+					.lambda(branches[E, P], const(Recur.lambda(Tag[E]) { t in P[t] }))
 				}
 			})
 
@@ -72,7 +72,7 @@ extension Expression where Recur: FixpointType {
 			Binding("Label", "String", .Type(0)),
 			Binding("Enumeration", Recur("List")[label].out, .Type(0)),
 
-			Tag,
+			tag,
 			here,
 			there,
 
