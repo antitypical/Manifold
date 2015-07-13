f8d75ca77301e843580737903b613a54ac5fc2e5
Rob Rix <rob.rix@github.com>
2015-07-13 13:19:09 -0400

    Test that List typechecks as a function from type to type.

--- ManifoldTests/ListTests.swift
+++ ManifoldTests/ListTests.swift
@@ -1,12 +1,14 @@
 //  Copyright © 2015 Rob Rix. All rights reserved.
 
 final class ListTests: XCTestCase {
-	func testListTypechecks() {
+	func testListTypechecksAsAHigherOrderType() {
 		let kind = Expression<Term>.Variable("List").typecheck(Expression.list.context)
 		assert(kind.left, ==, nil)
+		assert(kind.right, ==, Expression<Term>.lambda(.Type(0), const(.Type(0))))
 	}
 }
 
 import Assertions
 @testable import Manifold
+import Prelude
 import XCTest
