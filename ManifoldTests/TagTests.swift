b3f1936219f73459590a5ccaad947005a0b4acc9
Rob Rix <rob.rix@github.com>
2015-07-19 16:03:20 -0400

    Test tag construction.

--- ManifoldTests/TagTests.swift
+++ ManifoldTests/TagTests.swift
@@ -4,6 +4,13 @@ final class TagTests: XCTestCase {
 	func testConstructsOneTagPerLabel() {
 		assert(Tag.tags([ "a", "b", "c" ]).count, ==, 3)
 	}
+
+	func testConstructsTagsInOrder() {
+		let tags = Tag.tags([ "a", "b", "c" ])
+		assert(tags[0], ==, Tag.Here("a", ["b", "c"]))
+		assert(tags[1], ==, Tag.There("a", ["b", "c"], { Tag.Here("b", ["c"]) }))
+		assert(tags[2], ==, Tag.There("a", ["b", "c"], { Tag.There("b", ["c"], { Tag.Here("c", []) }) }))
+	}
 }
 
 
