a162749651899a547b2fa25731ac8a7ade8245b7
Rob Rix <rob.rix@github.com>
2015-07-19 11:35:00 -0400

    Uncons.

--- Manifold/Sliceable.swift
+++ Manifold/Sliceable.swift
@@ -1,6 +1,14 @@
 //  Copyright © 2015 Rob Rix. All rights reserved.
 
 extension Sliceable {
+	public var uncons: (first: SubSlice.Generator.Element, rest: SubSlice)? {
+		if !isEmpty {
+			let some = self[startIndex..<advance(startIndex, 1)]
+			return (first: some[some.startIndex], rest: dropFirst(self))
+		}
+		return nil
+	}
+
 	public var rest: SubSlice {
 		return isEmpty
 			? self[startIndex..<endIndex]
