3972e82a31b5d0cb196c4cb140bc0989ee4385d2
Rob Rix <rob.rix@github.com>
2015-07-18 20:21:33 -0400

    Tags have properties for their labels and enumerations.

--- Manifold/Tag.swift
+++ Manifold/Tag.swift
@@ -7,4 +7,22 @@ public typealias Enumeration = List<Label>
 public enum Tag {
 	case Here(Label, Enumeration)
 	case There(Label, Enumeration, () -> Tag)
+
+	var label: Label {
+		switch self {
+		case let .Here(l, _):
+			return l
+		case let .There(l, _, _):
+			return l
+		}
+	}
+
+	var enumeration: Enumeration {
+		switch self {
+		case let .Here(_, e):
+			return e
+		case let .There(_, e, _):
+			return e
+		}
+	}
 }
