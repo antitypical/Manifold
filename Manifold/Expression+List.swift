1a85217d81e4adc6714bc1ccea97e938ad02de19
Rob Rix <rob.rix@github.com>
2015-07-22 22:12:41 -0400

    Add an eliminator for List.

--- Manifold/Expression+List.swift
+++ Manifold/Expression+List.swift
@@ -22,7 +22,13 @@ extension Expression where Recur: FixpointType {
 			lambda(.Type) { A in Recur.lambda(A) { first in Recur.lambda(.Application(List, A)) { rest in .Annotation(.Product(.Boolean(true), .Product(first, rest)), List) } } },
 			lambda(.Type) { A in .lambda(A, const(.lambda(List[A], const(List[A])))) })
 
-		return Module([ list, `nil`, cons ])
+		// uncons : λ A : Type . λ B : Type . λ ifCons : (λ _ : A . λ _ : List A . B) . λ ifNil : (λ _ : Unit . B) . λ list : List A . B
+		// uncons = λ A : Type . λ B : Type . λ ifCons : (λ _ : A . λ _ : List A . B) . λ ifNil : (λ _ : Unit . B) . λ list : List A . if list.0 then ifCons list.1.0 list.1.1 else ifNil ()
+		let uncons = Binding("uncons",
+			lambda(.Type, .Type) { A, B in Recur.lambda(Recur.FunctionType([ A, List[A], B ]), Recur.FunctionType([ Recur.UnitType, B ]), List[A]) { ifCons, ifNil, list in .If(list.first, ifCons[list.second.first, list.second.second], ifNil[Recur.Unit]) } },
+			lambda(.Type, .Type) { A, B in Recur.lambda(Recur.FunctionType([ A, List[A], B ]), Recur.FunctionType([ .UnitType, B ]), List[A]) { _ in B } })
+
+		return Module([ list, `nil`, cons, uncons ])
 	}
 }
 
