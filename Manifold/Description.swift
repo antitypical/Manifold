8b4c6a6ca503ed197c63230ff611eb6f2e3fe888
Rob Rix <rob.rix@github.com>
2015-07-24 22:05:12 -0400

    Add a binding for the End constructor of Description.

--- Manifold/Description.swift
+++ Manifold/Description.swift
@@ -2,7 +2,33 @@
 
 extension Expression where Recur: FixpointType {
 	public static var description: Module<Recur> {
-		return Module([ list, tag ], [
+		let Tag: Recur -> Recur = { Recur("Tag")[$0] }
+		let Description = Recur("Description")
+		let cons: (Recur, Recur) -> Recur = { Recur("::")[$0, $1] }
+		let `nil` = Recur("[]")
+
+		let label: String -> Recur = {
+			.Axiom($0, Recur("String"))
+		}
+
+		let list: [Recur] -> Recur = fix { list in
+			{ $0.uncons.map { cons($0, list(Array($1))) } ?? `nil` }
+		}
+
+		let here: (Recur, Recur) -> Recur = {
+			Recur("here")[$0, $1]
+		}
+
+		let endTag = here(label("End"), list(["Recur", "Argument"].map(label)))
+
+		// End : λ I : Type . λ _ : I . Description I
+		// End = λ I : Type . λ i : I . (:End, i) : Description I
+		let end = Binding("End",
+			lambda(.Type) { I in Recur.lambda(I) { i in .Annotation(.Product(endTag, i), Description[I]) } },
+			lambda(.Type) { I in Recur.lambda(I, const(Description[I])) })
+
+		return Module([ Expression.list, tag ], [
+			end,
 		])
 	}
 }
