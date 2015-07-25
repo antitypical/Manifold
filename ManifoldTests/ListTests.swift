3af31df500d3722883c0469eef1f7df6c3808097
Rob Rix <rob.rix@github.com>
2015-07-25 14:53:26 -0400

    Test elimination of unary lists.

--- ManifoldTests/ListTests.swift
+++ ManifoldTests/ListTests.swift
@@ -11,6 +11,11 @@ final class ListTests: XCTestCase {
 		let eliminated = uncons[Term(.UnitType), Term(.BooleanType), Term.lambda(Term(.UnitType), const(Term.lambda(List[Term(.UnitType)], const(Term(false))))), Term.lambda(Term(.UnitType), const(Term(true))), empty]
 		assert(eliminated.out.evaluate(environment), ==, true)
 	}
+
+	func testNonEmptyListsEliminateWithFirstArgumentToUncons() {
+		let eliminated = uncons[Term(.UnitType), Term(.BooleanType), Term.lambda(Term(.UnitType), const(Term.lambda(List[Term(.UnitType)], const(Term(true))))), Term.lambda(Term(.UnitType), const(Term(false))), unary]
+		assert(eliminated.out.evaluate(environment), ==, true)
+	}
 }
 
 
@@ -18,8 +23,10 @@ private let context = Expression<Term>.list.context
 private let environment = Expression<Term>.list.environment
 
 private let List = Term("List")
+private let cons = Term("::")
 private let uncons = Term("uncons")
 private let empty = Term("[]")
+private let unary = cons[Term(.UnitType), Term(.Unit), empty]
 
 
 import Assertions
