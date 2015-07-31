b2782db921bcfe60392ed261f39093950e673681
Rob Rix <rob.rix@github.com>
2015-07-31 16:29:39 -0400

    Use the error-logging assertion in TypecheckingTests.

--- ManifoldTests/TypecheckingTests.swift
+++ ManifoldTests/TypecheckingTests.swift
@@ -2,53 +2,53 @@
 
 final class TypecheckingTests: XCTestCase {
 	func testUnitTermTypechecksToUnitType() {
-		assert(Term(.Unit).out.inferType().right, ==, .UnitType)
+		assert(Term(.Unit).out.inferType(), ==, .UnitType)
 	}
 
 	func testUnitTypeTypechecksToType() {
-		assert(Term(.UnitType).out.inferType().right, ==, .Type(0))
+		assert(Term(.UnitType).out.inferType(), ==, .Type(0))
 	}
 
 	func testTypeTypechecksToNextTypeLevel() {
-		assert(Term(.Type(0)).out.inferType().right, ==, .Type(1))
+		assert(Term(.Type(0)).out.inferType(), ==, .Type(1))
 	}
 
 	func testApplicationOfIdentityAbstractionToUnitTermTypechecksToUnitType() {
 		let identity = Expression.lambda(Term(.UnitType), id)
-		assert(Term(.Application(Term(identity), Term(.Unit))).out.inferType().right, ==, .UnitType)
+		assert(Term(.Application(Term(identity), Term(.Unit))).out.inferType(), ==, .UnitType)
 	}
 
 	func testSimpleAbstractionTypechecksToAbstractionType() {
 		let identity = Expression.lambda(Term(.UnitType), id)
-		assert(identity.inferType().right, ==, .lambda(Term(.UnitType), const(Term(.UnitType))))
+		assert(identity.inferType(), ==, .lambda(Term(.UnitType), const(Term(.UnitType))))
 	}
 
 	func testAbstractedAbstractionTypechecks() {
-		assert(identity.inferType().right, ==, .Lambda(1, Term(.Type(0)), Term(.Lambda(0, Term(1), Term(1)))))
+		assert(identity.inferType(), ==, .Lambda(1, Term(.Type(0)), Term(.Lambda(0, Term(1), Term(1)))))
 	}
 
 	func testProjectionTypechecksToTypeOfProjectedField() {
 		let product = Expression.Product(Term(.Unit), Term(false))
-		assert(Term(.Projection(Term(product), false)).out.inferType().right, ==, .UnitType)
-		assert(Term(.Projection(Term(product), true)).out.inferType().right, ==, .BooleanType)
+		assert(Term(.Projection(Term(product), false)).out.inferType(), ==, .UnitType)
+		assert(Term(.Projection(Term(product), true)).out.inferType(), ==, .BooleanType)
 	}
 
 	func testIfWithEqualBranchTypesTypechecksToBranchType() {
-		assert(Term(.If(Term(true), Term(.Unit), Term(.Unit))).out.inferType().right, ==, .UnitType)
+		assert(Term(.If(Term(true), Term(.Unit), Term(.Unit))).out.inferType(), ==, .UnitType)
 	}
 
 	func testIfWithDisjointBranchTypesTypechecksToSumOfBranchTypes() {
-		assert(Term(.If(Term(true), Term(.Unit), Term(true))).out.inferType().right, ==, Expression.lambda(Term(.BooleanType)) { Term(.If($0, Term(.UnitType), Term(.BooleanType))) })
+		assert(Term(.If(Term(true), Term(.Unit), Term(true))).out.inferType(), ==, Expression.lambda(Term(.BooleanType)) { Term(.If($0, Term(.UnitType), Term(.BooleanType))) })
 	}
 
 	func testProductTypechecksToSigmaType() {
-		assert(Expression.Product(Term(.Unit), Term(false)).inferType().right, ==, Expression.lambda(Term(.UnitType), const(Term(.BooleanType))))
+		assert(Expression.Product(Term(.Unit), Term(false)).inferType(), ==, Expression.lambda(Term(.UnitType), const(Term(.BooleanType))))
 	}
 
 	func testDependentProductTypechecksToSigmaType() {
 		let type = Expression.lambda(Term(.BooleanType)) { c in Term(.If(c, Term(.UnitType), Term(.BooleanType))) }
-		assert(Expression.Annotation(Term(.Product(Term(true), Term(.Unit))), Term(type)).inferType().right, ==, type)
-		assert(Expression.Annotation(Term(.Product(Term(false), Term(.Boolean(true)))), Term(type)).inferType().right, ==, type)
+		assert(Expression.Annotation(Term(.Product(Term(true), Term(.Unit))), Term(type)).inferType(), ==, type)
+		assert(Expression.Annotation(Term(.Product(Term(false), Term(.Boolean(true)))), Term(type)).inferType(), ==, type)
 	}
 }
 
