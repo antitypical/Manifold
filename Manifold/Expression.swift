cd6cc31ccf57b2b3a51205cc91a8d981779de392
Rob Rix <rob.rix@github.com>
2015-07-31 10:34:17 -0400

    Move substitution into its own file.

--- Manifold/Expression.swift
+++ Manifold/Expression.swift
@@ -301,28 +301,6 @@ extension Expression where Recur: FixpointType {
 	}
 
 
-	// MARK: Substitution
-
-	func substitute(i: Int, _ expression: Expression) -> Expression {
-		return cata { t in
-			Recur(t.analysis(
-				ifVariable: {
-					$0.analysis(
-						ifGlobal: const(t),
-						ifLocal: { $0 == i ? expression : t })
-				},
-				ifApplication: Expression.Application,
-				ifLambda: Expression.Lambda,
-				ifProjection: Expression.Projection,
-				ifProduct: Expression.Product,
-				ifIf: Expression.If,
-				ifAnnotation: Expression.Annotation,
-				ifAxiom: Expression.Axiom,
-				otherwise: const(t)))
-		} (Recur(self)).out
-	}
-
-
 	// MARK: Variables
 
 	var maxBoundVariable: Int {
