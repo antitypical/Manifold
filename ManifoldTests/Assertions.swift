0f6b6a75b19379c29b53bb97ea9b5a6ed5385188
Rob Rix <rob.rix@github.com>
2015-07-31 16:28:00 -0400

    Add an assertion which automatically prints errors in Eithers.

---
+++ ManifoldTests/Assertions.swift
@@ -0,0 +1,18 @@
+//  Copyright © 2015 Rob Rix. All rights reserved.
+
+func assert<L, R>(@autoclosure expression1: () -> Either<L, R>, _ test: (R, R) -> Bool, @autoclosure _ expression2: () -> R, message: String = "", file: String = __FILE__, line: UInt = __LINE__) -> R? {
+	switch expression1() {
+	case let .Left(l):
+		failure("expected success, but got: \(String(reflecting: l))", file: file, line: line)
+		return nil
+	case let .Right(r):
+		let expected = expression2()
+		return test(r, expected)
+			? r
+			: failure("\(String(reflecting: r)) did not match \(String(reflecting: expected))")
+	}
+}
+
+
+import Assertions
+import Either
