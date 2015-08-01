a4d0cc2e6c94dbe0e724d29aa1c6306b85270975
Rob Rix <rob.rix@github.com>
2015-08-01 09:12:38 -0400

    Move ternary Location construction into a static method.

--- Manifold/Location.swift
+++ Manifold/Location.swift
@@ -39,6 +39,21 @@ public struct Location<A> {
 		return fl1
 	}
 
+	public static func loc(wv: (A -> Location) -> A -> Location, _ fl0: A -> A -> A -> Location) -> A -> A -> A -> Location {
+		func fl1(t1: A)(_ t2: A)(_ t3: A) -> Location {
+			let upd: (A -> A -> A -> Location) -> A -> Location = { fl in { t1 in fl(t1)(t2)(t3) } }
+			return Location(it: t1, down: wv(upd(fl1)), up: upd(fl0), left: upd(fl1), right: upd(fl2))
+		}
+		func fl2(t1: A)(_ t2: A)(_ t3: A) -> Location {
+			let upd: (A -> A -> A -> Location) -> A -> Location = { fl in { t2 in fl(t1)(t2)(t3) } }
+			return Location(it: t1, down: wv(upd(fl2)), up: upd(fl0), left: upd(fl1), right: upd(fl3))
+		}
+		func fl3(t1: A)(_ t2: A)(_ t3: A) -> Location {
+			let upd: (A -> A -> A -> Location) -> A -> Location = { fl in { t3 in fl(t1)(t2)(t3) } }
+			return Location(it: t1, down: wv(upd(fl3)), up: upd(fl0), left: upd(fl2), right: upd(fl3))
+		}
+		return fl1
+	}
 }
 
 public struct Weaver<A> {
@@ -65,7 +80,7 @@ public struct Weaver<A> {
 
 	public init(_ t1: A, _ t2: A, _ t3: A, _ k:  A -> A -> A -> A, _ wv: Weave) {
 		self.init { fl0 in
-			loc3(Weaver.call(wv), { t1 in { t2 in { t3 in fl0(k(t1)(t2)(t3)) } } })(t1)(t2)(t3)
+			Location.loc(Weaver.call(wv), { t1 in { t2 in { t3 in fl0(k(t1)(t2)(t3)) } } })(t1)(t2)(t3)
 		}
 	}
 
@@ -112,23 +127,6 @@ extension Expression where Recur: FixpointType {
 			return Weaver(a.out, b.out, c.out, curry { Expression.If(Recur($0), Recur($1), Recur($2)) }, weave)
 		}
 	}
-}
-
-
-public func loc3<A>(wv: (A -> Location<A>) -> A -> Location<A>, _ fl0: A -> A -> A -> Location<A>) -> A -> A -> A -> Location<A> {
-	func fl1(t1: A)(_ t2: A)(_ t3: A) -> Location<A> {
-		let upd: (A -> A -> A -> Location<A>) -> A -> Location<A> = { fl in { t1 in fl(t1)(t2)(t3) } }
-		return Location(it: t1, down: wv(upd(fl1)), up: upd(fl0), left: upd(fl1), right: upd(fl2))
-	}
-	func fl2(t1: A)(_ t2: A)(_ t3: A) -> Location<A> {
-		let upd: (A -> A -> A -> Location<A>) -> A -> Location<A> = { fl in { t2 in fl(t1)(t2)(t3) } }
-		return Location(it: t1, down: wv(upd(fl2)), up: upd(fl0), left: upd(fl1), right: upd(fl3))
-	}
-	func fl3(t1: A)(_ t2: A)(_ t3: A) -> Location<A> {
-		let upd: (A -> A -> A -> Location<A>) -> A -> Location<A> = { fl in { t3 in fl(t1)(t2)(t3) } }
-		return Location(it: t1, down: wv(upd(fl3)), up: upd(fl0), left: upd(fl2), right: upd(fl3))
-	}
-	return fl1
 }
 
 
