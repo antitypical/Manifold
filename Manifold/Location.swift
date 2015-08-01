9de649336f6281debd682f541b3f120678741c01
Rob Rix <rob.rix@github.com>
2015-08-01 01:01:43 -0400

    Add a Location type.

---
+++ Manifold/Location.swift
@@ -0,0 +1,18 @@
+//  Copyright © 2015 Rob Rix. All rights reserved.
+
+public struct Location<A> {
+	public init(it: A, down: A -> Location, up: A -> Location, left: A -> Location, right: A -> Location) {
+		self.it = it
+		self.left = left
+		self.right = right
+		self.up = up
+		self.down = down
+	}
+
+	public let it: A
+	public let down: A -> Location
+	public let up: A -> Location
+	public let left: A -> Location
+	public let right: A -> Location
+}
+
