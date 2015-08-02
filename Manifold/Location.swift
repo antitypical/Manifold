c75d9a6fc31a4ec448a436d72561bfb9a581c434
Rob Rix <rob.rix@github.com>
2015-08-02 11:28:33 -0400

    Move Expression weaving into its own file.

--- Manifold/Location.swift
+++ Manifold/Location.swift
@@ -57,39 +57,4 @@ public struct Location<A> {
 }
 
 
-extension Expression where Recur: FixpointType {
-	public static func weave(expression: Expression) -> Weaver<Expression> {
-		switch expression {
-		// MARK: Nullary
-		case .Unit, .UnitType, .Type, .Variable, .BooleanType, .Boolean:
-			return Weaver(expression, weave)
-
-		// MARK: Unary
-		case let .Projection(a, b):
-			return Weaver(a.out, weave) { Expression.Projection(Recur($0), b) }
-
-		case let .Axiom(any, type):
-			return Weaver(type.out, weave) { Expression.Axiom(any, Recur($0)) }
-
-		// MARK: Binary
-		case let .Application(a, b):
-			return Weaver(a.out, b.out, weave) { Expression.Application(Recur($0), Recur($1)) }
-
-		case let .Lambda(i, a, b):
-			return Weaver(a.out, b.out, weave) { Expression.Lambda(i, Recur($0), Recur($1)) }
-
-		case let .Product(a, b):
-			return Weaver(a.out, b.out, weave) { Expression.Product(Recur($0), Recur($1)) }
-
-		case let .Annotation(a, b):
-			return Weaver(a.out, b.out, weave) { Expression.Annotation(Recur($0), Recur($1)) }
-
-		// MARK: Ternary
-		case let .If(a, b, c):
-			return Weaver(a.out, b.out, c.out, weave) { Expression.If(Recur($0), Recur($1), Recur($2)) }
-		}
-	}
-}
-
-
 import Prelude
