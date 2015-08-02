9106c6734e4b04adea2520abd4ec3ce410164d97
Rob Rix <rob.rix@github.com>
2015-08-02 17:46:11 -0400

    Weave Expressions with Location alone.

--- Manifold/Expression+Weaving.swift
+++ Manifold/Expression+Weaving.swift
@@ -2,37 +2,37 @@
 
 extension Expression where Recur: FixpointType {
 	public func explore() -> Location<Expression> {
-		func weave(expression: Expression) -> Weaver<Expression> {
+		func weave(expression: Expression) -> Location<Expression>.Unweave {
 			switch expression {
 			// MARK: Nullary
 			case .Unit, .UnitType, .Type, .Variable, .BooleanType, .Boolean:
-				return Weaver()
+				return Location.nullary
 
 			// MARK: Unary
 			case let .Projection(a, b):
-				return Weaver(a.out, weave) { Expression.Projection(Recur($0), b) }
+				return Location.unary(a.out, weave, { Expression.Projection(Recur($0), b) })
 
 			case let .Axiom(any, type):
-				return Weaver(type.out, weave) { Expression.Axiom(any, Recur($0)) }
+				return Location.unary(type.out, weave, { Expression.Axiom(any, Recur($0)) })
 
 			// MARK: Binary
 			case let .Application(a, b):
-				return Weaver(a.out, b.out, weave) { Expression.Application(Recur($0), Recur($1)) }
+				return Location.binary(a.out, b.out, weave) { Expression.Application(Recur($0), Recur($1)) }
 
 			case let .Lambda(i, a, b):
-				return Weaver(a.out, b.out, weave) { Expression.Lambda(i, Recur($0), Recur($1)) }
+				return Location.binary(a.out, b.out, weave) { Expression.Lambda(i, Recur($0), Recur($1)) }
 
 			case let .Product(a, b):
-				return Weaver(a.out, b.out, weave) { Expression.Product(Recur($0), Recur($1)) }
+				return Location.binary(a.out, b.out, weave) { Expression.Product(Recur($0), Recur($1)) }
 
 			case let .Annotation(a, b):
-				return Weaver(a.out, b.out, weave) { Expression.Annotation(Recur($0), Recur($1)) }
+				return Location.binary(a.out, b.out, weave) { Expression.Annotation(Recur($0), Recur($1)) }
 
 			// MARK: Ternary
 			case let .If(a, b, c):
-				return Weaver(a.out, b.out, c.out, weave) { Expression.If(Recur($0), Recur($1), Recur($2)) }
+				return Location.ternary(a.out, b.out, c.out, weave) { Expression.If(Recur($0), Recur($1), Recur($2)) }
 			}
 		}
-		return Weaver.explore(weave)(self)
+		return Location.explore(weave)(self)
 	}
 }
