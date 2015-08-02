d5acaabd91fe01c71049e3ae00e948c430fb6169
Rob Rix <rob.rix@github.com>
2015-08-02 11:22:59 -0400

    Binary Location construction is uncurried.

--- Manifold/Location.swift
+++ Manifold/Location.swift
@@ -27,13 +27,13 @@ public struct Location<A> {
 		return fl1
 	}
 
-	public static func loc(wv: (A -> Location) -> A -> Location, _ fl0: A -> A -> Location) -> A -> A -> Location {
-		func fl1(t1: A)(_ t2: A) -> Location {
-			let upd: (A -> A -> Location) -> A -> Location = { fl in { t1 in fl(t1)(t2) } }
+	public static func loc(wv: (A -> Location) -> A -> Location, _ fl0: (A, A) -> Location) -> (A, A) -> Location {
+		func fl1(t1: A, _ t2: A) -> Location {
+			let upd: ((A, A) -> Location) -> A -> Location = { fl in { t1 in fl(t1, t2) } }
 			return Location(it: t1, down: wv(upd(fl1)), up: upd(fl0), left: upd(fl1), right: upd(fl2))
 		}
-		func fl2(t1: A)(_ t2: A) -> Location {
-			let upd: (A -> A -> Location) -> A -> Location = { fl in { t2 in fl(t1)(t2) } }
+		func fl2(t1: A, _ t2: A) -> Location {
+			let upd: ((A, A) -> Location) -> A -> Location = { fl in { t2 in fl(t1, t2) } }
 			return Location(it: t2, down: wv(upd(fl2)), up: upd(fl0), left: upd(fl1), right: upd(fl2))
 		}
 		return fl1
@@ -74,7 +74,7 @@ public struct Weaver<A> {
 
 	public init(_ t1: A, _ t2: A, _ k: (A, A) -> A, _ wv: Weave) {
 		self.init { fl0 in
-			Location.loc(Weaver.call(wv), { t1 in { t2 in fl0(k(t1, t2)) } })(t1)(t2)
+			Location.loc(Weaver.call(wv), { t1, t2 in fl0(k(t1, t2)) })(t1, t2)
 		}
 	}
 
