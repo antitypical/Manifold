d772bda5fe6bbcfaff74eda6694a9721c1e366ce
Rob Rix <rob.rix@github.com>
2015-08-02 13:42:38 -0400

    Test that top-level nodes have no parents or siblings.

--- ManifoldTests/LocationTests.swift
+++ ManifoldTests/LocationTests.swift
@@ -4,6 +4,15 @@ final class LocationTests: XCTestCase {
 	func testTraversingAnExpressionReturnsALocationAtTheTraversedExpression() {
 		assert(identity.explore().it, ==, identity)
 	}
+
+	func testTopLevelNodesHaveNoParents() {
+		assert(Expression<Term>.Unit.explore().up?.it, ==, nil)
+	}
+
+	func testTopLevelNodesHaveNoSiblings() {
+		assert(Expression<Term>.Unit.explore().left?.it, ==, nil)
+		assert(Expression<Term>.Unit.explore().right?.it, ==, nil)
+	}
 }
 
 
