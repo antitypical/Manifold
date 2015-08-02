f8af8ac7192255830b97392e0627964e921c6a80
Rob Rix <rob.rix@github.com>
2015-08-02 11:27:49 -0400

    Move Weaver into its own file.

---
+++ Manifold/Weaver.swift
@@ -0,0 +1,40 @@
+//  Copyright © 2015 Rob Rix. All rights reserved.
+
+public struct Weaver<A> {
+	public typealias Weave = A -> Weaver
+	public typealias Unweave = (A -> Location<A>) -> Location<A>
+
+	public init(_ k: A, _ wv: Weave) {
+		self.init { fl0 in
+			Location.loc(Weaver.call(wv), fl0(k))
+		}
+	}
+
+	public init(_ t1: A, _ wv: Weave, _ k: A -> A) {
+		self.init { fl0 in
+			Location.loc(Weaver.call(wv), { t1 in fl0(k(t1)) })(t1)
+		}
+	}
+
+	public init(_ t1: A, _ t2: A, _ wv: Weave, _ k: (A, A) -> A) {
+		self.init { fl0 in
+			Location.loc(Weaver.call(wv), { t1, t2 in fl0(k(t1, t2)) })(t1, t2)
+		}
+	}
+
+	public init(_ t1: A, _ t2: A, _ t3: A, _ wv: Weave, _ k:  (A, A, A) -> A) {
+		self.init { fl0 in
+			Location.loc(Weaver.call(wv), { t1, t2, t3 in fl0(k(t1, t2, t3)) })(t1, t2, t3)
+		}
+	}
+
+	public init(unweave: Unweave) {
+		self.unweave = unweave
+	}
+
+	public let unweave: Unweave
+
+	public static func call<T>(wv: T -> Weaver)(_ fl0: A -> Location<A>)(_ t: T) -> Location<A> {
+		return wv(t).unweave(fl0)
+	}
+}
