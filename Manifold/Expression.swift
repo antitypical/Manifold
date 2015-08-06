e027d2ca90577e95b4f36763ae5c6242a8a2b4df
Rob Rix <rob.rix@github.com>
2015-08-06 01:34:10 -0400

    Binary curried function type construction.

--- Manifold/Expression.swift
+++ Manifold/Expression.swift
@@ -245,6 +245,11 @@ extension Expression where Recur: FixpointType {
 		return .Lambda(-1, a, b)
 	}
 
+	/// Constructs a (non-dependent) function type from `A` to `B` to `C`.
+	public static func FunctionType(a: Recur, _ b: Recur, _ c: Recur) -> Expression {
+		return .FunctionType(a, Recur(FunctionType(b, c)))
+	}
+
 
 	// MARK: Higher-order construction
 
