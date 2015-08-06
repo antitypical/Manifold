8a43fef46f165f4cff1670937f14455766a68d2a
Rob Rix <rob.rix@github.com>
2015-08-05 23:31:21 -0400

    Test that the Natural module typechecks.

--- ManifoldTests/NaturalTests.swift
+++ ManifoldTests/NaturalTests.swift
@@ -1,6 +1,10 @@
 //  Copyright © 2015 Rob Rix. All rights reserved.
 
 final class NaturalTests: XCTestCase {
+	func testModuleTypechecks() {
+		module.typecheck().map { XCTFail($0.description) }
+	}
+
 	func testZeroTypechecksAsNatural() {
 		assert(zero.out.checkType("Natural", environment, context), ==, "Natural")
 		assert(zero.out.inferType(environment, context), ==, "Natural")
@@ -12,6 +16,7 @@ final class NaturalTests: XCTestCase {
 }
 
 private let module = Expression<Term>.natural
+private let environment = module.environment
 private let context = module.context
 
 private let successor = Term("successor")
