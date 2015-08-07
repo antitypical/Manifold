4f684996836eb9c8a146256c29fcd433310e1f90
Rob Rix <rob.rix@github.com>
2015-08-07 15:25:57 -0400

    Construct tags from arrays of labels.

--- Manifold/Tag.swift
+++ Manifold/Tag.swift
@@ -1,6 +1,15 @@
 //  Copyright © 2015 Rob Rix. All rights reserved.
 
 public enum Tag: Equatable {
+	public static func tags(labels: [String]) -> [Tag] {
+		return labels.conses.reduce((there: id, tags: [])) { into, each in
+			(there: { Tag.There(each.0, $0) }, tags: into.tags + [ into.there(Tag.Here(each.0, Array(each.1))) ])
+		}.tags
+	}
+
 	case Here(String, [String])
 	indirect case There(String, Tag)
 }
+
+
+import Prelude
