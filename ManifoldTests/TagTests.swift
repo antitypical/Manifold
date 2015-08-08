a12c2b21b4332cbfb01a89a9f0e1317060ca58d8
Rob Rix <rob.rix@github.com>
2015-08-08 09:44:55 -0400

    Remove the Tag module and tests.

--- ManifoldTests/TagTests.swift
+++ ManifoldTests/TagTests.swift
@@ -2,52 +2,25 @@
 
 final class TagTests: XCTestCase {
 	func testEmptyEnumerationYieldsEmptyTag() {
-		assert(Manifold.Tag.tags([]), ==, [])
+		assert(Tag.tags([]), ==, [])
 	}
 
 	func testFirstLabelMapsToHere() {
-		assert(Manifold.Tag.tags([ "unit" ]), ==, [ Manifold.Tag.Here("unit", []) ])
+		assert(Tag.tags([ "unit" ]), ==, [ Tag.Here("unit", []) ])
 	}
 
 	func testLaterLabelsMapToThere() {
-		assert(Manifold.Tag.tags([ "true", "false" ]), ==, [ Manifold.Tag.Here("true", [ "false" ]), Manifold.Tag.There("true", Manifold.Tag.Here("false", [])) ])
+		assert(Tag.tags([ "true", "false" ]), ==, [ Tag.Here("true", [ "false" ]), Tag.There("true", Tag.Here("false", [])) ])
 	}
 
 	func testNLabelsMapToNTags() {
-		assert(Manifold.Tag.tags([ "a", "b", "c" ]), ==, [
-			Manifold.Tag.Here("a", [ "b", "c" ]),
-			Manifold.Tag.There("a", Manifold.Tag.Here("b", [ "c" ])),
-			Manifold.Tag.There("a", Manifold.Tag.There("b", Manifold.Tag.Here("c", []))),
+		assert(Tag.tags([ "a", "b", "c" ]), ==, [
+			Tag.Here("a", [ "b", "c" ]),
+			Tag.There("a", Tag.Here("b", [ "c" ])),
+			Tag.There("a", Tag.There("b", Tag.Here("c", []))),
 		])
 	}
-
-	func testTagTypechecksAsFunction() {
-		let expected = Expression.FunctionType(Enumeration, Term(.Type(0)))
-		let actual = Tag.out.checkType(expected, environment, context)
-		assert(actual.left, ==, nil)
-		assert(actual.right, ==, expected)
-	}
-
-	func testBranchesProducesAType() {
-		let branches = Branches[Empty, Term(.lambda(Tag[Empty], const(Term(.UnitType))))].out.checkType(.Type(0), environment, context)
-		assert(branches.left, ==, nil)
-		assert(branches.right, ==, .Type(0))
-	}
-
-	func testBranchesOfEmptyEnumerationIsUnitType() {
-		assert(Branches[Empty, Term(.lambda(Tag[Empty], const(Term(.BooleanType))))].out.evaluate(environment), ==, .UnitType)
-	}
 }
-
-private let Enumeration = Term("Enumeration")
-private let Branches = Term("Branches")
-private let `nil` = Term("[]")
-private let Tag = Term("Tag")
-private let Empty = `nil`[Term("String")]
-
-private let module = Expression<Term>.tag
-private let context = module.context
-private let environment = module.environment
 
 
 import Assertions
