16c1a1640f0e6679b46ac613ea066a19c9ac1db3
Rob Rix <rob.rix@github.com>
2015-08-25 11:52:49 -0400

    Remove LazySequenceType.
    Swift stdlib includes it now.

--- Manifold/LazySequenceType.swift
+++
@@ -1,7 +0,0 @@
-//  Copyright © 2015 Rob Rix. All rights reserved.
-
-public protocol LazySequenceType: SequenceType {}
-extension LazySequence: LazySequenceType {}
-extension LazyForwardCollection: LazySequenceType {}
-extension LazyBidirectionalCollection: LazySequenceType {}
-extension LazyRandomAccessCollection: LazySequenceType {}
