67ecddbec06311f64cf4d6cd5ddc4ca9cd0234b7
Rob Rix <rob.rix@github.com>
2015-09-04 23:22:19 -0700

    Reintegrate SwiftCheck.

---
+++ ManifoldTests/Term+Arbitrary.swift
@@ -0,0 +1,46 @@
+//  Copyright © 2015 Rob Rix. All rights reserved.
+
+final class TermProperties: XCTestCase {
+	func testEqualityIsReflexive() {
+		property("reflexivity of equality") <- forAll { (term: Term) in
+			term == term
+		}
+	}
+}
+
+
+extension Term: Arbitrary {
+	public static var arbitrary: Gen<Term> {
+		func arbitrary(n: Int) -> Gen<Term> {
+			let topLevel: [Gen<Term>] = [
+				Gen.pure(Term.Unit),
+				Gen.pure(Term.UnitType),
+				Gen.pure(Term.BooleanType),
+				Bool.arbitrary.fmap(Term.Boolean),
+				Bool.arbitrary.fmap { $0 ? .Type : .Type(1) },
+				Gen.pure(()).bind {
+					Term.arbitrary.bind { a in Term.arbitrary.fmap { b in Term.Application(a, b) } }
+				},
+				Gen.pure(()).bind {
+					Term.arbitrary.bind { a in Bool.arbitrary.fmap { b in Term.Projection(a, b) } }
+				},
+				Gen.pure(()).bind {
+					Term.arbitrary.bind { a in Term.arbitrary.bind { b in Term.arbitrary.fmap { c in Term.If(a, b, c) } } }
+				},
+			]
+
+			let inBinder = [
+				Int.arbitrary.suchThat { $0 <= n }.fmap { Term.Variable(.Local($0)) },
+			]
+
+			return Gen.oneOf(topLevel + (n >= 0 ? inBinder : []))
+		}
+		return arbitrary(-1)
+	}
+}
+
+
+@testable import Manifold
+import Prelude
+import SwiftCheck
+import XCTest
