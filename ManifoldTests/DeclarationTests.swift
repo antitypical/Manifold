ff14f346239e99b92aef281820df7b187bafad73
Rob Rix <rob.rix@github.com>
2015-09-20 17:14:11 -0400

    Test that the context has data constructors in it too.

--- ManifoldTests/DeclarationTests.swift
+++ ManifoldTests/DeclarationTests.swift
@@ -5,6 +5,11 @@ final class DeclarationTests: XCTestCase {
 		let expected: Term = .Type
 		XCTAssertEqual(booleanModule.context["Boolean"].map(Term.init), expected)
 	}
+
+	func testDatatypeDeclarationsAddDataConstructorsToContext() {
+		XCTAssertEqual(booleanModule.context["true"].map(Term.init), Term.Variable("Boolean"))
+		XCTAssertEqual(booleanModule.context["false"].map(Term.init), Term.Variable("Boolean"))
+	}
 }
 
 
