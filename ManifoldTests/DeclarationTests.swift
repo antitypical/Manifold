72b44ee665b50f1623800cb73b014d68ce7c797c
Rob Rix <rob.rix@github.com>
2015-10-03 12:19:04 -0400

    Migrate DeclarationTests to Assertions.

--- ManifoldTests/DeclarationTests.swift
+++ ManifoldTests/DeclarationTests.swift
@@ -3,23 +3,23 @@
 final class DeclarationTests: XCTestCase {
 	func testDatatypeDeclarationsAddTypesToContext() {
 		let expected: Term = .Type
-		XCTAssertEqual(booleanModule.context["Boolean"].map(Term.init), expected)
+		assert(booleanModule.context["Boolean"].map(Term.init), ==, expected)
 	}
 
 	func testDatatypeDeclarationsAddDataConstructorsToContext() {
-		XCTAssertEqual(booleanModule.context["true"].map(Term.init), Term.Variable("Boolean"))
-		XCTAssertEqual(booleanModule.context["false"].map(Term.init), Term.Variable("Boolean"))
+		assert(booleanModule.context["true"].map(Term.init), ==, Term.Variable("Boolean"))
+		assert(booleanModule.context["false"].map(Term.init), ==, Term.Variable("Boolean"))
 	}
 
 	func testDatatypeDeclarationsAddDataConstructorsToEnvironment() {
-		XCTAssertEqual(booleanModule.environment["true"].map(Term.init), Term.Product(.Boolean(true), .Unit))
-		XCTAssertEqual(booleanModule.environment["false"].map(Term.init), Term.Product(.Boolean(false), .Unit))
+		assert(booleanModule.environment["true"].map(Term.init), ==, Term.Product(.Boolean(true), .Unit))
+		assert(booleanModule.environment["false"].map(Term.init), ==, Term.Product(.Boolean(false), .Unit))
 	}
 
 	func testDatatypeConstructorsProduceRightNestedValues() {
-		XCTAssertEqual(selfModule.environment["me"].map(Term.init), Term.Product(.Boolean(true), .Unit))
-		XCTAssertEqual(selfModule.environment["myself"].map(Term.init), Term.Product(.Boolean(false), .Product(.Boolean(true), .Unit)))
-		XCTAssertEqual(selfModule.environment["I"].map(Term.init), Term.Product(.Boolean(false), .Product(.Boolean(false), .Unit)))
+		assert(selfModule.environment["me"].map(Term.init), ==, Term.Product(.Boolean(true), .Unit))
+		assert(selfModule.environment["myself"].map(Term.init), ==, Term.Product(.Boolean(false), .Product(.Boolean(true), .Unit)))
+		assert(selfModule.environment["I"].map(Term.init), ==, Term.Product(.Boolean(false), .Product(.Boolean(false), .Unit)))
 	}
 }
 
@@ -41,6 +41,7 @@ private let selfModule = Module<Term>([], [
 ])
 
 
+import Assertions
 @testable import Manifold
 import Prelude
 import XCTest
