cca4020586ae302f1f37d3c245110449482b661b
Rob Rix <rob.rix@github.com>
2015-10-04 11:57:06 -0400

    Datatype is DictionaryLiteralConvertible.

--- Manifold/Datatype.swift
+++ Manifold/Datatype.swift
@@ -1,5 +1,13 @@
 //  Copyright © 2015 Rob Rix. All rights reserved.
 
-public struct Datatype {
+public struct Datatype: DictionaryLiteralConvertible {
+	public init(constructors: [(String, Telescope)]) {
+		self.constructors = constructors
+	}
+
+	public init(dictionaryLiteral: (String, Telescope)...) {
+		self.init(constructors: dictionaryLiteral)
+	}
+
 	public let constructors: [(String, Telescope)]
 }
