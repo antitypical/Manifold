23176fed020b6b3a494c4eb63aae62d2865b18da
Rob Rix <rob.rix@github.com>
2015-10-05 22:23:17 -0400

    Add some tests of telescope types.

--- ManifoldTests/TelescopeTests.swift
+++ ManifoldTests/TelescopeTests.swift
@@ -1,7 +1,21 @@
 //  Copyright © 2015 Rob Rix. All rights reserved.
 
 final class TelescopeTests: XCTestCase {
+	func testEmptyDataConstructorHasUnitType() {
+		assert(Telescope.End.type("A"), ==, .UnitType)
+	}
 
+	func testRecursiveDataConstructorHasFunctionType() {
+		assert(Telescope.Recursive(.End).type("A"), ==, .lambda("A", const(.UnitType)))
+	}
+
+	func testMultiplyRecursiveDataConstructorHasFunctionType() {
+		assert(Telescope.Recursive(.Recursive(.End)).type("A"), ==, .lambda("A", const(.lambda("A", const(.UnitType)))))
+	}
+
+	func testArgumentDataConstructorHasFunctionType() {
+		assert(Telescope.Argument("B", const(.Recursive(.End))).type("A"), ==, .lambda("B", const(.lambda("A", const(.UnitType)))))
+	}
 }
 
 
