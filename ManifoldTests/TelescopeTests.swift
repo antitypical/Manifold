42406c4a51c99556d0ad2b529cc0a27f701b2e34
Rob Rix <rob.rix@github.com>
2015-10-05 22:29:52 -0400

    Test that empty constructors have unit value.

--- ManifoldTests/TelescopeTests.swift
+++ ManifoldTests/TelescopeTests.swift
@@ -1,6 +1,8 @@
 //  Copyright © 2015 Rob Rix. All rights reserved.
 
 final class TelescopeTests: XCTestCase {
+	// MARK: Types
+
 	func testEmptyDataConstructorHasUnitType() {
 		assert(Telescope.End.type("A"), ==, .UnitType)
 	}
@@ -15,6 +17,13 @@ final class TelescopeTests: XCTestCase {
 
 	func testArgumentDataConstructorHasFunctionType() {
 		assert(Telescope.Argument("B", const(.Recursive(.End))).type("A"), ==, .lambda("B", const(.lambda("A", const(.UnitType)))))
+	}
+
+
+	// MARK: Values
+
+	func testEmptyDataConstructorHasUnitValue() {
+		assert(Telescope.End.value("A"), ==, .Unit)
 	}
 }
 
