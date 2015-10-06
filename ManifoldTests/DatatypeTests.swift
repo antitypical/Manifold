76507a7cb8a11b407f359b1842c6a27730e69bf7
Rob Rix <rob.rix@github.com>
2015-10-05 23:01:41 -0400

    Test that zero-constructor datatypes are unit.

--- ManifoldTests/DatatypeTests.swift
+++ ManifoldTests/DatatypeTests.swift
@@ -1,6 +1,9 @@
 //  Copyright © 2015 Rob Rix. All rights reserved.
 
 final class DatatypeTests: XCTestCase {
+	func testDatatypeWithZeroConstructorsIsUnitType() {
+		assert(Datatype(constructors: []).value("A"), ==, .UnitType)
+	}
 }
 
 
