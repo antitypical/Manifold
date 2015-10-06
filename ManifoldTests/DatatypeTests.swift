098f043a0812e5aff23728d35c5e2f24706eefd0
Rob Rix <rob.rix@github.com>
2015-10-05 23:31:14 -0400

    Test the value of single-constructor datatypes.

--- ManifoldTests/DatatypeTests.swift
+++ ManifoldTests/DatatypeTests.swift
@@ -4,9 +4,14 @@ final class DatatypeTests: XCTestCase {
 	func testDatatypeWithZeroConstructorsIsUnitType() {
 		assert(Datatype(constructors: []).value("A"), ==, .UnitType)
 	}
+
+	func testDatatypeWithOneConstructorIsTypeOfConstructedValue() {
+		assert(Datatype(constructors: [ ("a", .Argument(.BooleanType, const(.End))) ]).value("A"), ==, .lambda(.BooleanType, const(.Product(.BooleanType, .UnitType))))
+	}
 }
 
 
 import Assertions
 @testable import Manifold
+import Prelude
 import XCTest
