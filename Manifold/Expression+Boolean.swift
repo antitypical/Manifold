024b1b9c87089bb8c9c63c0b7dc635f3a0f47589
Rob Rix <rob.rix@github.com>
2015-10-10 19:04:43 -0400

    Define Boolean as a Datatype.

--- Manifold/Expression+Boolean.swift
+++ Manifold/Expression+Boolean.swift
@@ -2,23 +2,12 @@
 
 extension Expression where Recur: TermType {
 	public static var boolean: Module<Recur> {
-		let Boolean = Declaration("Boolean",
-			type: .Type(0),
-			value: lambda(.Type) { A in Recur.lambda(A, A, const(A)) })
-
-		let `true` = Declaration("true",
-			type: Boolean.ref.out,
-			value: lambda(.Type) { A in Recur.lambda(A, A) { a, _ in a } })
-
-		let `false` = Declaration("false",
-			type: Boolean.ref.out,
-			value: lambda(.Type) { A in Recur.lambda(A, A) { _, a in a } })
-
-		let not = Declaration("not",
-			type: FunctionType(Boolean.ref, Boolean.ref),
-			value: lambda(Boolean.ref, .Type) { b, A in Recur.lambda(A, A) { t, f in b[A, f, t] } })
-
-		return Module([ Boolean, `true`, `false`, not ])
+		return Module([
+			.Datatype("Boolean", [
+				"true": .End,
+				"false": .End
+			])
+		])
 	}
 }
 
