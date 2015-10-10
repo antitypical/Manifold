94f2f464b90dc90138a590fe9f634bad9408c6de
Rob Rix <rob.rix@github.com>
2015-10-10 16:27:01 -0400

    Omit the return keyword.

--- Manifold/Datatype.swift
+++ Manifold/Datatype.swift
@@ -14,7 +14,7 @@ public struct Datatype<Recur: TermType>: DictionaryLiteralConvertible {
 
 	public func definitions(recur: Recur) -> [Declaration<Recur>.DefinitionType] {
 		return constructors[constructors.indices].fold((definitions: [], transform: id)) { (each: (String, Telescope<Recur>), into: (definitions: [Declaration<Recur>.DefinitionType], transform: Recur -> Recur)) in
-			return (into.definitions + [ (symbol: each.0, type: each.1.type(recur).out, value: into.transform(into.definitions.count > 0
+			(into.definitions + [ (symbol: each.0, type: each.1.type(recur).out, value: into.transform(into.definitions.count > 0
 				? .Product(true, each.1.value(recur))
 				: each.1.value(recur)).out) ], into.transform)
 		}.definitions
