48708c86a6db0d4e0a46f51383c9ace2031e255a
Rob Rix <rob.rix@github.com>
2015-10-10 16:25:08 -0400

    Use the field labels in the combining function too.

--- Manifold/Datatype.swift
+++ Manifold/Datatype.swift
@@ -13,11 +13,11 @@ public struct Datatype<Recur: TermType>: DictionaryLiteralConvertible {
 
 
 	public func definitions(recur: Recur) -> [Declaration<Recur>.DefinitionType] {
-		return constructors[constructors.indices].fold((definitions: [], transform: id)) { (each: (String, Telescope<Recur>), into: ([Declaration<Recur>.DefinitionType], Recur -> Recur)) in
-			let value: Expression<Recur> = into.0.count > 0
+		return constructors[constructors.indices].fold((definitions: [], transform: id)) { (each: (String, Telescope<Recur>), into: (definitions: [Declaration<Recur>.DefinitionType], transform: Recur -> Recur)) in
+			let value: Expression<Recur> = into.definitions.count > 0
 				? Expression<Recur>.Product(true, each.1.value(recur))
 				: each.1.value(recur).out
-			return (into.0 + [ (symbol: each.0, type: each.1.type(recur).out, value: value) ], into.1)
+			return (into.definitions + [ (symbol: each.0, type: each.1.type(recur).out, value: value) ], into.transform)
 		}.definitions
 	}
 
