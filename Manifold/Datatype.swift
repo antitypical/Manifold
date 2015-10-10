1994fbb0a34c8bf4581da9549401a18c516bcbfe
Rob Rix <rob.rix@github.com>
2015-10-10 16:24:36 -0400

    Label the fields.

--- Manifold/Datatype.swift
+++ Manifold/Datatype.swift
@@ -13,12 +13,12 @@ public struct Datatype<Recur: TermType>: DictionaryLiteralConvertible {
 
 
 	public func definitions(recur: Recur) -> [Declaration<Recur>.DefinitionType] {
-		return constructors[constructors.indices].fold(([], id)) { (each: (String, Telescope<Recur>), into: ([Declaration<Recur>.DefinitionType], Recur -> Recur)) in
+		return constructors[constructors.indices].fold((definitions: [], transform: id)) { (each: (String, Telescope<Recur>), into: ([Declaration<Recur>.DefinitionType], Recur -> Recur)) in
 			let value: Expression<Recur> = into.0.count > 0
 				? Expression<Recur>.Product(true, each.1.value(recur))
 				: each.1.value(recur).out
 			return (into.0 + [ (symbol: each.0, type: each.1.type(recur).out, value: value) ], into.1)
-		}.0
+		}.definitions
 	}
 
 
