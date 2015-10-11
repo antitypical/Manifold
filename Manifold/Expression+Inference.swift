e84d5da66c4579a529a400786467ed818aefc424
Rob Rix <rob.rix@github.com>
2015-10-11 14:46:33 -0400

    Don’t annotate in the unannotated inferType.

--- Manifold/Expression+Inference.swift
+++ Manifold/Expression+Inference.swift
@@ -16,13 +16,13 @@ extension Expression where Recur: TermType, Recur: Equatable {
 			return .right(.BooleanType)
 
 		case let .If(condition, then, `else`):
-			return annotate(condition.checkType(.BooleanType, environment, context)
+			return condition.checkType(.BooleanType, environment, context)
 				>> (then.inferType(environment, context) &&& `else`.inferType(environment, context))
 					.map { a, b in
 						a == b
 							? a
 							: Expression.lambda(.BooleanType) { .If($0, Recur(a), Recur(b)) }
-					})
+					}
 
 		case .UnitType, .BooleanType:
 			return .right(.Type(0))
@@ -30,19 +30,19 @@ extension Expression where Recur: TermType, Recur: Equatable {
 			return .right(.Type(n + 1))
 
 		case let .Variable(i):
-			return annotate(context[i].map(Either.Right) ?? Either.Left("Unexpectedly free variable \(i)"))
+			return context[i].map(Either.Right) ?? Either.Left("Unexpectedly free variable \(i)")
 
 		case let .Lambda(i, type, body):
-			return annotate(type.checkIsType(environment, context)
+			return type.checkIsType(environment, context)
 				>> body.inferType(environment, context + [ .Local(i): type ])
-					.map { Expression.lambda(Recur(type), const(Recur($0))) })
+					.map { Expression.lambda(Recur(type), const(Recur($0))) }
 
 		case let .Product(a, b):
-			return annotate((a.inferType(environment, context) &&& b.inferType(environment, context))
-				.map { A, B in Expression.lambda(Recur(A), const(Recur(B))) })
+			return (a.inferType(environment, context) &&& b.inferType(environment, context))
+				.map { A, B in Expression.lambda(Recur(A), const(Recur(B))) }
 
 		case let .Application(a, b):
-			return annotate(a.inferType(environment, context)
+			return a.inferType(environment, context)
 				.flatMap { A in
 					A.weakHeadNormalForm(environment).analysis(
 						ifLambda: { i, type, body in
@@ -50,21 +50,21 @@ extension Expression where Recur: TermType, Recur: Equatable {
 								.map { _ in body.out.substitute(i, b) }
 						},
 						otherwise: const(Either.Left("illegal application of \(a) : \(A) to \(b)")))
-				})
+				}
 
 		case let .Projection(term, branch):
-			return annotate(term.inferType(environment, context)
+			return term.inferType(environment, context)
 				.flatMap { type in
 					type.analysis(
 						ifLambda: { i, A, B in
 							Either.Right(branch ? B.out.substitute(i, A.out) : A.out)
 						},
 						otherwise: const(Either.Left("illegal projection of field \(branch ? 1 : 0) of non-product value \(term) of type \(type)")))
-				})
+				}
 
 		case let .Annotation(term, type):
-			return annotate(term.checkType(type, environment, context)
-				.map(const(type)))
+			return term.checkType(type, environment, context)
+				.map(const(type))
 		}
 	}
 }
