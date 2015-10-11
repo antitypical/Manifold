33120369a3d23750425d541abd3f8d6ee9c74ea6
Rob Rix <rob.rix@github.com>
2015-10-11 15:54:57 -0400

    Implement evaluation over TermType.

---
+++ Manifold/TermType+Evaluation.swift
@@ -0,0 +1,37 @@
+//  Copyright © 2015 Rob Rix. All rights reserved.
+
+extension TermType {
+	public func evaluate(environment: [Name:Self] = [:]) -> Self {
+		switch out {
+		case let .Variable(i):
+			if let found = environment[i] {
+				return found
+			}
+			fatalError("Illegal free variable \(i)")
+		case let .Application(a, b):
+			let a = a.evaluate(environment)
+			if case let .Lambda(i, _, body) = a.out {
+				return body.substitute(i, b.evaluate(environment)).evaluate(environment)
+			}
+			fatalError("Illegal application of non-lambda term \(a) to \(b)")
+		case let .Projection(a, b):
+			let a = a.evaluate(environment)
+			if case let .Product(a0, a1) = a.out {
+				return (b ? a1 : a0)
+			}
+			fatalError("Illegal projection of non-product term \(a) field \(b ? 1 : 0)")
+		case let .If(condition, then, `else`):
+			let condition = condition.evaluate(environment)
+			if case let .Boolean(boolean) = condition.out {
+				return boolean
+					? then.evaluate(environment)
+					: `else`.evaluate(environment)
+			}
+			fatalError("Illegal branch on non-boolean term \(condition)")
+		case let .Annotation(term, _):
+			return term.evaluate(environment)
+		default:
+			return self
+		}
+	}
+}
