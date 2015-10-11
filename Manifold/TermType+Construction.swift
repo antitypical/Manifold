a681a791a2080f9931accd49be8a1647666e6c07
Rob Rix <rob.rix@github.com>
2015-10-11 16:18:57 -0400

    Add the maxBoundVariables/freeVariables properties to TermType.

--- Manifold/TermType+Construction.swift
+++ Manifold/TermType+Construction.swift
@@ -140,6 +140,36 @@ extension TermType {
 	public init(extendedGraphemeClusterLiteral: Self.StringLiteralType) {
 		self.init(stringLiteral: extendedGraphemeClusterLiteral)
 	}
+
+
+	// MARK: Variables
+
+	var maxBoundVariable: Int {
+		return cata {
+			$0.analysis(
+				ifApplication: max,
+				ifLambda: { max($0.0, $0.1) },
+				ifProjection: { $0.0 },
+				ifProduct: max,
+				ifIf: { max($0, $1, $2) },
+				ifAnnotation: max,
+				otherwise: const(-1))
+		} (self)
+	}
+
+	public var freeVariables: Set<Int> {
+		return cata {
+			$0.analysis(
+				ifVariable: { $0.local.map { [ $0 ] } ?? Set() },
+				ifApplication: uncurry(Set.union),
+				ifLambda: { $1.union($2.subtract([ $0 ])) },
+				ifProjection: { $0.0 },
+				ifProduct: uncurry(Set.union),
+				ifIf: { $0.union($1).union($2) },
+				ifAnnotation: uncurry(Set.union),
+				otherwise: const(Set()))
+		} (self)
+	}
 }
 
 
