55151e483b7adb0e555c4228a4c56d4f746ead6e
Rob Rix <rob.rix@github.com>
2015-10-11 15:29:15 -0400

    Implement substitution on TermType.

---
+++ Manifold/TermType+Substitution.swift
@@ -0,0 +1,24 @@
+//  Copyright © 2015 Rob Rix. All rights reserved.
+
+extension TermType {
+	func substitute(i: Int, _ expression: Self) -> Self {
+		return cata { (t: Expression<Self>) in
+			t.analysis(
+				ifVariable: {
+					$0.analysis(
+						ifGlobal: const(Self(t)),
+						ifLocal: { $0 == i ? expression : Self(t) })
+				},
+				ifApplication: Self.Application,
+				ifLambda: Self.Lambda,
+				ifProjection: Self.Projection,
+				ifProduct: Self.Product,
+				ifIf: Self.If,
+				ifAnnotation: Self.Annotation,
+				otherwise: const(Self(t)))
+		} (self)
+	}
+}
+
+
+import Prelude
