839f302bc020796b008cac226e3ae39a5e41a1bf
Rob Rix <rob.rix@github.com>
2015-10-12 09:58:01 -0400

    Test the expected type of `cons` specifically.

--- ManifoldTests/ListTests.swift
+++ ManifoldTests/ListTests.swift
@@ -4,10 +4,16 @@ final class ListTests: XCTestCase {
 	func testModuleTypechecks() {
 		module.typecheck().forEach { XCTFail($0.description) }
 	}
+
+	func testTypeOfConsIsParameterizedByAType() {
+		assert(module.context["cons"], ==, .lambda(.Type, { A in .lambda(A, const(.lambda(.Application("List", A), const(.Application("List", A))))) }))
+	}
 }
 
 private let module = Expression<Term>.list
 
 
+import Assertions
 @testable import Manifold
+import Prelude
 import XCTest
