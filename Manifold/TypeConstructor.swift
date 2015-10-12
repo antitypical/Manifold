9ad6357b47dc2b13e8b54ce6ad098f3cf621b176
Rob Rix <rob.rix@github.com>
2015-10-11 20:42:25 -0400

    TypeConstructors are DictionaryLiteralConvertible.

--- Manifold/TypeConstructor.swift
+++ Manifold/TypeConstructor.swift
@@ -1,6 +1,11 @@
 //  Copyright © 2015 Rob Rix. All rights reserved.
 
-public enum TypeConstructor<Recur: TermType> {
+public enum TypeConstructor<Recur: TermType>: DictionaryLiteralConvertible {
 	case Argument(Recur, Recur -> TypeConstructor)
 	case End(Datatype<Recur>)
+
+
+	public init(dictionaryLiteral: (String, Telescope<Recur>)...) {
+		self = .End(Datatype(constructors: dictionaryLiteral))
+	}
 }
