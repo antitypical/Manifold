e88aced9ea04b198ae1815a5aa2c3b69048e1a22
Rob Rix <rob.rix@github.com>
2015-10-18 19:04:55 -0400

    Test that the prelude module typechecks.

--- ManifoldTests/PreludeTests.swift
+++ ManifoldTests/PreludeTests.swift
@@ -1,6 +1,12 @@
 //  Copyright © 2015 Rob Rix. All rights reserved.
 
-final class PreludeTests: XCTestCase {}
+final class PreludeTests: XCTestCase {
+	func testModuleTypechecks() {
+		module.typecheck().forEach { XCTFail($0.description) }
+	}
+}
+
+private let module = Module<Term>.prelude
 
 
 import Manifold
