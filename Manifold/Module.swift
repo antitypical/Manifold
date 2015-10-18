f921ce5407ea1ea3e9b08b157493386e62c0c881
Rob Rix <rob.rix@github.com>
2015-10-18 16:02:26 -0400

    Annotate module typechecking errors with the module’s name.

--- Manifold/Module.swift
+++ Manifold/Module.swift
@@ -52,7 +52,7 @@ extension Module where Recur: TermType {
 		let context = self.context
 		return declarations
 			.lazy
-			.flatMap { $0.typecheck(environment, context) }
+			.flatMap { $0.typecheck(environment, context).map { $0.map { "\(self.name): \($0)" } } }
 	}
 }
 
