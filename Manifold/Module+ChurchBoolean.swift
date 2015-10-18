5ce93e8e726f6fb00583e00d5eec11dc819fcbf5
Rob Rix <rob.rix@github.com>
2015-10-17 23:30:41 -0400

    Add the type of booleans.

--- Manifold/Module+ChurchBoolean.swift
+++ Manifold/Module+ChurchBoolean.swift
@@ -2,6 +2,13 @@
 
 extension Module {
 	public static var churchBoolean: Module {
-		return Module([])
+		let Boolean = Declaration("Boolean",
+			type: .Type,
+			value: Recur.lambda(.Type) { Recur.lambda($0, $0, const($0)) })
+
+		return Module([ Boolean ])
 	}
 }
+
+
+import Prelude
