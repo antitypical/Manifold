9d80c5c45c6c69e1caa577d7f920533e34e07d29
Rob Rix <rob.rix@github.com>
2015-10-18 15:28:31 -0400

    Rename Pair to pair, since it’s the constructor, not the type.

--- Manifold/Module+ChurchPair.swift
+++ Manifold/Module+ChurchPair.swift
@@ -2,10 +2,10 @@
 
 extension Module {
 	public static var churchPair: Module {
-		let Pair = Declaration("Pair",
+		let pair = Declaration("pair",
 			type: Recur.lambda(.Type, .Type, .Type) { A, B, Result in Recur.lambda(A, B, Recur.FunctionType(A, B, Result), const(Result)) },
 			value: Recur.lambda(.Type, .Type, .Type) { A, B, Result in Recur.lambda(A, B, .FunctionType(A, B, Result)) { a, b, f in f[a, b] } })
-		return Module([ churchBoolean ], [ Pair ])
+		return Module([ churchBoolean ], [ pair ])
 	}
 }
 
