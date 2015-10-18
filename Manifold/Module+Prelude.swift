52d76d4b0608ee2328bad8f4d6978591fb18c558
Rob Rix <rob.rix@github.com>
2015-10-18 19:06:20 -0400

    Add the identity function.

--- Manifold/Module+Prelude.swift
+++ Manifold/Module+Prelude.swift
@@ -2,6 +2,13 @@
 
 extension Module {
 	public static var prelude: Module {
-		return Module("Prelude", [])
+		let identity = Declaration("identity",
+			type: Recur.lambda(.Type) { A in Recur.FunctionType(A, A) },
+			value: Recur.lambda(.Type) { A in Recur.lambda(A, id) })
+
+		return Module("Prelude", [ identity ])
 	}
 }
+
+
+import Prelude
