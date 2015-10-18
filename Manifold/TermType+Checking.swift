6d19ad52ad09175ddb6b2692ba87c1452053d250
Rob Rix <rob.rix@github.com>
2015-10-18 09:25:56 -0400

    Pretty-print contexts and environments with : and = respectively.

--- Manifold/TermType+Checking.swift
+++ Manifold/TermType+Checking.swift
@@ -68,7 +68,7 @@ extension TermType {
 		let keys = table.keys.sort().lazy
 		let maxLength: Int = keys.maxElement { $0.description.characters.count < $1.description.characters.count }?.description.characters.count ?? 0
 		let padding: Character = " "
-		let formattedContext = keys.map { "\(String(Self.describe($0), paddedTo: maxLength, with: padding)) : \(table[$0]!)" }.joinWithSeparator(",\n\t")
+		let formattedContext = keys.map { "\(String(Self.describe($0), paddedTo: maxLength, with: padding)) \(separator) \(table[$0]!)" }.joinWithSeparator(",\n\t")
 
 		return "[\n\t\(formattedContext)\n]"
 	}
