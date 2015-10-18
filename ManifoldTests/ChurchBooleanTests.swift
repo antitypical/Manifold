6e75d294f052e2c92305e01ca6e931b97b054bdf
Rob Rix <rob.rix@github.com>
2015-10-18 10:34:30 -0400

    Test the evaluation of `false`.

--- ManifoldTests/ChurchBooleanTests.swift
+++ ManifoldTests/ChurchBooleanTests.swift
@@ -8,10 +8,15 @@ final class ChurchBooleanTests: XCTestCase {
 	func testTrueReturnsItsFirstTermArgument() {
 		assert(`true`[.Type, .Boolean(true), .Boolean(false)].evaluate(module.environment), ==, true)
 	}
+
+	func testFalseReturnsItsSecondTermArgument() {
+		assert(`false`[.Type, .Boolean(true), .Boolean(false)].evaluate(module.environment), ==, false)
+	}
 }
 
 private let module = Module<Term>.churchBoolean
 private let `true`: Term = "true"
+private let `false`: Term = "false"
 
 
 import Assertions
