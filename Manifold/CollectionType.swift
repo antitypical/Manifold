d9d498953dc4485d0f1c5d6698ad72e4dc97df41
Rob Rix <rob.rix@github.com>
2015-10-18 19:20:54 -0400

    Remove the `uncons` property.

--- Manifold/CollectionType.swift
+++ Manifold/CollectionType.swift
@@ -1,15 +1,5 @@
 //  Copyright © 2015 Rob Rix. All rights reserved.
 
-extension CollectionType {
-	public var uncons: (first: SubSequence._Element, rest: SubSequence)? {
-		if !isEmpty {
-			let some = self[startIndex..<startIndex.advancedBy(1)]
-			return (first: some[some.startIndex], rest: dropFirst())
-		}
-		return nil
-	}
-}
-
 extension CollectionType where SubSequence == Self {
 	public func fold<Out>(initial: Out, @noescape combine: (Generator.Element, Out) -> Out) -> Out {
 		return first.map {
