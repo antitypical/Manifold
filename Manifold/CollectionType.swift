cf82a2509b326ff1862982377cf016760bcbb317
Rob Rix <rob.rix@github.com>
2015-10-18 19:22:16 -0400

    Remove `fold`.

--- Manifold/CollectionType.swift
+++
@@ -1,9 +0,0 @@
-//  Copyright © 2015 Rob Rix. All rights reserved.
-
-extension CollectionType where SubSequence == Self {
-	public func fold<Out>(initial: Out, @noescape combine: (Generator.Element, Out) -> Out) -> Out {
-		return first.map {
-			combine($0, dropFirst().fold(initial, combine: combine))
-		} ?? initial
-	}
-}
