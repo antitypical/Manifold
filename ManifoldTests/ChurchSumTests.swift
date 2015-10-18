534f040a11596642b8e262f4ef40234263981d05
Rob Rix <rob.rix@github.com>
2015-10-18 19:37:36 -0400

    Test that the church-encoded sum module typechecks.

--- ManifoldTests/ChurchSumTests.swift
+++ ManifoldTests/ChurchSumTests.swift
@@ -1,6 +1,12 @@
 //  Copyright © 2015 Rob Rix. All rights reserved.
 
-final class ChurchSumTests: XCTestCase {}
+final class ChurchSumTests: XCTestCase {
+	func testModuleTypechecks() {
+		module.typecheck().forEach { XCTFail($0.description) }
+	}
+}
+
+private let module = Module<Term>.churchSum
 
 
 import Manifold
