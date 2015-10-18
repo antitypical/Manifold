2cd45de7ec28a3366f505f566308f6e2cde851b6
Rob Rix <rob.rix@github.com>
2015-10-18 15:32:15 -0400

    Type pair as Pair.

--- Manifold/Module+ChurchPair.swift
+++ Manifold/Module+ChurchPair.swift
@@ -7,7 +7,7 @@ extension Module {
 			value: Recur.lambda(.Type, .Type, .Type) { A, B, Result in Recur.lambda(A, B, Recur.FunctionType(A, B, Result), const(Result)) })
 
 		let pair = Declaration("pair",
-			type: Recur.lambda(.Type, .Type, .Type) { A, B, Result in Recur.lambda(A, B, Recur.FunctionType(A, B, Result), const(Result)) },
+			type: Pair.ref,
 			value: Recur.lambda(.Type, .Type, .Type) { A, B, Result in Recur.lambda(A, B, .FunctionType(A, B, Result)) { a, b, f in f[a, b] } })
 
 		return Module([ churchBoolean ], [ Pair, pair ])
