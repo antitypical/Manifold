5f03b55b0ce4bd0ee213e383b57863ac62b72798
Rob Rix <rob.rix@github.com>
2015-10-18 14:57:17 -0400

    Elaborate the types of types.

--- Manifold/TermType+Elaboration.swift
+++ Manifold/TermType+Elaboration.swift
@@ -4,6 +4,9 @@ extension TermType {
 	public func elaborate(environment: [Name:Self], _ context: [Name:Self]) -> Either<Error, Elaborated<Self>> {
 		return cata {
 			switch $0 {
+			case let .Type(n):
+				return .Right(.Unroll(.Type(n + 1), .Type(n)))
+
 			default:
 				return .Left("unimplemented")
 			}
