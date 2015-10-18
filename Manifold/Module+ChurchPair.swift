0228d920f1088b0453f0c8dce595f574aa86e5b9
Rob Rix <rob.rix@github.com>
2015-10-18 16:06:47 -0400

    The Church pair type does not include the value parameters.
    They’re only needed for the pair constructor.

--- Manifold/Module+ChurchPair.swift
+++ Manifold/Module+ChurchPair.swift
@@ -4,7 +4,7 @@ extension Module {
 	public static var churchPair: Module {
 		let Pair = Declaration("Pair",
 			type: Recur.lambda(.Type, .Type, const(.Type)),
-			value: Recur.lambda(.Type, .Type, .Type) { A, B, Result in Recur.lambda(A, B, Recur.FunctionType(A, B, Result), const(Result)) })
+			value: Recur.lambda(.Type, .Type, .Type) { A, B, Result in Recur.lambda(Recur.FunctionType(A, B, Result), const(Result)) })
 
 		let pair = Declaration("pair",
 			type: Pair.ref,
