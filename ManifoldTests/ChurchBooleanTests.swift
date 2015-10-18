092dbed43f5c4ba4f1249de16e5ba05485f22929
Rob Rix <rob.rix@github.com>
2015-10-18 10:33:59 -0400

    Test the evaluation of `true`.

--- ManifoldTests/ChurchBooleanTests.swift
+++ ManifoldTests/ChurchBooleanTests.swift
@@ -4,10 +4,16 @@ final class ChurchBooleanTests: XCTestCase {
 	func testModuleTypechecks() {
 		module.typecheck().forEach { XCTFail($0.description) }
 	}
+
+	func testTrueReturnsItsFirstTermArgument() {
+		assert(`true`[.Type, .Boolean(true), .Boolean(false)].evaluate(module.environment), ==, true)
+	}
 }
 
 private let module = Module<Term>.churchBoolean
+private let `true`: Term = "true"
 
 
+import Assertions
 import Manifold
 import XCTest
