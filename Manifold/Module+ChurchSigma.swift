9eb2b2789603ad3a5cc2aa79268e165c41a0c2d0
Rob Rix <rob.rix@github.com>
2015-10-19 17:28:42 -0400

    Drop some pointless applications.

--- Manifold/Module+ChurchSigma.swift
+++ Manifold/Module+ChurchSigma.swift
@@ -7,16 +7,16 @@ extension Module {
 			value: .Type => { A in (A --> .Type, .Type) => { B, C in (A => { x in B[x] --> C }) --> C } })
 
 		let sigma = Declaration("sigma",
-			type: .Type => { A in (A --> .Type, A) => { B, x in B[x] --> Sigma.ref[A, A => { B[$0] }] } },
+			type: .Type => { A in (A --> .Type, A) => { (B, x: Recur) in B[x] --> Sigma.ref[A, B] } },
 			value: .Type => { A in (A --> .Type, A) => { B, x in (B[x], .Type) => { y, C in (A => { xʹ in B[xʹ] --> C }) => { f in f[x, y] } } } })
 
 		let first = Declaration("first",
-			type: .Type => { A in (A --> .Type) => { B in Sigma.ref[A, A => { x in B[x] }] --> A } },
-			value: .Type => { A in (A --> .Type) => { B in Sigma.ref[A, A => { x in B[x] }] => { v in v[A, A => { x in B[x] => const(x) }] } } })
+			type: .Type => { A in (A --> .Type) => { B in Sigma.ref[A, B] --> A } },
+			value: .Type => { A in (A --> .Type) => { B in Sigma.ref[A, B] => { v in v[A, A => { (x: Recur) in B[x] => const(x) }] } } })
 
 		let second = Declaration("second",
-			type: .Type => { A in (A --> .Type) => { B in Sigma.ref[A, A => { x in B[x] }] => { v in B[first.ref[A, v]] } } },
-			value: .Type => { A in (A --> .Type) => { B in Sigma.ref[A, A => { x in B[x] }] => { v in v[B, A => { x in B[x] => id }] } } })
+			type: .Type => { A in (A --> .Type) => { B in Sigma.ref[A, B] => { v in B[first.ref[A, v]] } } },
+			value: .Type => { A in (A --> .Type) => { B in Sigma.ref[A, B] => { v in v[B, A => { (x: Recur) in B[x] => id }] } } })
 
 		return Module("ChurchSigma", [ Sigma, sigma, first, second ])
 	}
