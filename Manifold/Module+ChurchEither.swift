491420f089d50abbab305e6103c69fc3d809842d
Rob Rix <rob.rix@github.com>
2015-10-18 20:29:28 -0400

    Add the right constructor.

--- Manifold/Module+ChurchEither.swift
+++ Manifold/Module+ChurchEither.swift
@@ -10,6 +10,10 @@ extension Module {
 			type: Recur.lambda(.Type, .Type) { L, R in .FunctionType(L, Either.ref[L, R]) },
 			value: Recur.lambda(.Type, .Type) { L, R in Recur.lambda(L, .Type) { l, Result in Recur.lambda(.FunctionType(L, Result), .FunctionType(R, Result)) { ifL, _ in ifL[l] } } })
 
-		return Module("ChurchEither", [ Either, left ])
+		let right = Declaration("right",
+			type: Recur.lambda(.Type, .Type) { L, R in .FunctionType(R, Either.ref[L, R]) },
+			value: Recur.lambda(.Type, .Type) { L, R in Recur.lambda(R, .Type) { r, Result in Recur.lambda(.FunctionType(L, Result), .FunctionType(R, Result)) { _, ifR in ifR[r] } } })
+
+		return Module("ChurchEither", [ Either, left, right ])
 	}
 }
