a2dbcc6d12035f5cc84ba19f799b56271a4e7219
Rob Rix <rob.rix@github.com>
2015-10-19 00:21:16 -0400

    Test that the module typechecks.

---
+++ ManifoldTests/ChurchSigmaTests.swift
@@ -0,0 +1,13 @@
+//  Copyright © 2015 Rob Rix. All rights reserved.
+
+final class ChurchSigmaTests: XCTestCase {
+	func testModuleTypechecks() {
+		module.typecheck().forEach { XCTFail($0.description) }
+	}
+}
+
+private let module = Module<Term>.churchSigma
+
+
+import Manifold
+import XCTest
