bd80355c7ad8818efefd6cd2ed35ddef27dec5a5
Rob Rix <rob.rix@github.com>
2015-10-19 01:28:18 -0400

    Use the function type constructor.

--- Manifold/Module+ChurchSigma.swift
+++ Manifold/Module+ChurchSigma.swift
@@ -3,15 +3,15 @@
 extension Module {
 	public static var churchSigma: Module {
 		let Sigma = Declaration("Sigma",
-			type: Recur.lambda(.Type) { A in .FunctionType(.FunctionType(A, .Type), .Type) },
-			value: Recur.lambda(.Type) { A in Recur.lambda(.FunctionType(A, .Type), .Type) { B, C in Recur.FunctionType(Recur.lambda(A) { x in .FunctionType(B[x], C) }, C) } })
+			type: Recur.lambda(.Type) { A in (A --> .Type) --> .Type },
+			value: Recur.lambda(.Type) { A in Recur.lambda(A --> .Type, .Type) { B, C in Recur.lambda(A) { x in B[x] --> C } --> C } })
 
 		let sigma = Declaration("sigma",
-			type: Recur.lambda(.Type) { A in Recur.lambda(.FunctionType(A, .Type), A) { B, x in .FunctionType(B[x], Sigma.ref[A, Recur.lambda(A) { B[$0] }]) } },
-			value: Recur.lambda(.Type) { A in Recur.lambda(.FunctionType(A, .Type), A) { B, x in Recur.lambda(B[x], .Type) { y, C in Recur.lambda(Recur.lambda(A) { xʹ in .FunctionType(B[xʹ], C) }) { f in f[x, y] } } } })
+			type: Recur.lambda(.Type) { A in Recur.lambda(A --> .Type, A) { B, x in B[x] --> Sigma.ref[A, Recur.lambda(A) { B[$0] }] } },
+			value: Recur.lambda(.Type) { A in Recur.lambda(A --> .Type, A) { B, x in Recur.lambda(B[x], .Type) { y, C in Recur.lambda(Recur.lambda(A) { xʹ in B[xʹ] --> C }) { f in f[x, y] } } } })
 
 		let first = Declaration("first",
-			type: Recur.lambda(.Type) { A in Recur.lambda(A --> .Type) { B in Sigma.ref[A, Recur.lambda(A) { x in B[x] }] --> A } },
+			type: Recur.lambda(.Type) { A in (A --> .Type) => { B in Sigma.ref[A, Recur.lambda(A) { x in B[x] }] --> A } },
 			value: .Type => { A in (A --> .Type) => { B in Sigma.ref[A, A => { x in B[x] }] => { v in v[A, A => { x in B[x] => const(x) }] } } })
 
 		return Module("ChurchSigma", [ Sigma, sigma, first ])
