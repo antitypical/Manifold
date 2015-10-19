3148ec536f54ab826a39ec3831f6aeb0c6974a44
Rob Rix <rob.rix@github.com>
2015-10-19 00:27:10 -0400

    Attempt an encoding of sigmas.

--- Manifold/Module+ChurchSigma.swift
+++ Manifold/Module+ChurchSigma.swift
@@ -2,6 +2,10 @@
 
 extension Module {
 	public static var churchSigma: Module {
-		return Module("ChurchSigma", [])
+		let Sigma = Declaration("Sigma",
+			type: Recur.lambda(.Type) { A in .FunctionType(.FunctionType(A, .Type), .Type) },
+			value: Recur.lambda(.Type) { A in Recur.lambda(.FunctionType(A, .Type), .Type) { B, C in Recur.FunctionType(Recur.lambda(A) { x in .FunctionType(B[x], C) }, C) } })
+
+		return Module("ChurchSigma", [ Sigma ])
 	}
 }
