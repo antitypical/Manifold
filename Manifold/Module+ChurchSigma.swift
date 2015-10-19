ce5f747392b133184f39ea143019bd742e3fbe06
Rob Rix <rob.rix@github.com>
2015-10-19 00:57:39 -0400

    Church-encoded sigma constructor.

--- Manifold/Module+ChurchSigma.swift
+++ Manifold/Module+ChurchSigma.swift
@@ -6,6 +6,10 @@ extension Module {
 			type: Recur.lambda(.Type) { A in .FunctionType(.FunctionType(A, .Type), .Type) },
 			value: Recur.lambda(.Type) { A in Recur.lambda(.FunctionType(A, .Type), .Type) { B, C in Recur.FunctionType(Recur.lambda(A) { x in .FunctionType(B[x], C) }, C) } })
 
-		return Module("ChurchSigma", [ Sigma ])
+		let sigma = Declaration("sigma",
+			type: Recur.lambda(.Type) { A in Recur.lambda(.FunctionType(A, .Type), A) { B, x in .FunctionType(B[x], Sigma.ref[A, Recur.lambda(A) { B[$0] }]) } },
+			value: Recur.lambda(.Type) { A in Recur.lambda(.FunctionType(A, .Type), A) { B, x in Recur.lambda(B[x], .Type) { y, C in Recur.lambda(Recur.lambda(A) { xʹ in .FunctionType(B[xʹ], C) }) { f in f[x, y] } } } })
+
+		return Module("ChurchSigma", [ Sigma, sigma ])
 	}
 }
