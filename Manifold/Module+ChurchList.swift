21dc3d174dbd22903920bf56b9df63ebe2766710
Rob Rix <rob.rix@github.com>
2015-10-21 22:44:45 -0400

    Add the type of encoded lists.

--- Manifold/Module+ChurchList.swift
+++ Manifold/Module+ChurchList.swift
@@ -2,6 +2,11 @@
 
 extension Module {
 	public static var churchList: Module {
-		return Module("ChurchList", [])
+		let List: Recur = "List"
+		let list = Declaration("List",
+			type: .Type --> .Type,
+			value: (.Type, .Type) => { A, B in (A --> List[A] --> B) --> (.UnitType --> B) --> B })
+
+		return Module("ChurchList", [ list ])
 	}
 }
