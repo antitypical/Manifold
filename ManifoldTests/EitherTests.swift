2d75b98576e276d674c81ae0baedb202ae8543e7
Rob Rix <rob.rix@github.com>
2015-10-24 12:43:44 -0400

    Test that the Either module has equivalent definitions to the ChurchEither module.

--- ManifoldTests/EitherTests.swift
+++ ManifoldTests/EitherTests.swift
@@ -4,10 +4,19 @@ final class EitherTests: XCTestCase {
 	func testModuleTypechecks() {
 		module.typecheck().forEach { XCTFail($0.description) }
 	}
+
+	func testAutomaticallyEncodedDefinitionsAreEquivalentToHandEncodedDefinitions() {
+		let churchModule = Module<Term>.churchEither
+		module.definitions.forEach { symbol, type, value in
+			assert(churchModule.context[symbol], ==, type)
+			assert(churchModule.environment[symbol], ==, value)
+		}
+	}
 }
 
 private let module = Module<Term>.either
 
 
+import Assertions
 @testable import Manifold
 import XCTest
