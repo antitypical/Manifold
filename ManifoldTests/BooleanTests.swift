f8c29e46feb1ca1008c5d6e82d445161ca11b258
Rob Rix <rob.rix@github.com>
2015-10-24 15:37:45 -0400

    Test Boolean against a private encoded module.

--- ManifoldTests/BooleanTests.swift
+++ ManifoldTests/BooleanTests.swift
@@ -6,17 +6,31 @@ final class BooleanTests: XCTestCase {
 	}
 
 	func testAutomaticallyEncodedDefinitionsAreEquivalentToHandEncodedDefinitions() {
-		let churchModule = Module<Term>.churchBoolean
-		module.definitions.forEach { symbol, type, value in
-			assert(churchModule.context[symbol], ==, type, message: "\(symbol)")
-			assert(churchModule.environment[symbol], ==, value, message: "\(symbol)")
+		expected.definitions.forEach { symbol, type, value in
+			assert(module.context[symbol], ==, type, message: "\(symbol)")
+			assert(module.environment[symbol], ==, value, message: "\(symbol)")
 		}
 	}
 }
 
 private let module = Module<Term>.boolean
+private let expected: Module<Term> = {
+	let Boolean = Declaration("Boolean",
+		type: .Type,
+		value: Term.lambda(.Type) { Term.lambda($0, $0, const($0)) })
 
+	let `true` = Declaration("true",
+		type: Boolean.ref,
+		value: Term.lambda(.Type) { A in Term.lambda(A, A) { a, _ in a } })
+
+	let `false` = Declaration("false",
+		type: Boolean.ref,
+		value: Term.lambda(.Type) { A in Term.lambda(A, A) { _, b in b } })
+
+	return Module("ChurchBoolean", [ Boolean, `true`, `false` ])
+}()
 
 import Assertions
 import Manifold
+import Prelude
 import XCTest
