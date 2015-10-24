1c3cb087c15ccb3b39ada830d20a10bc57850ce8
Rob Rix <rob.rix@github.com>
2015-10-24 15:33:45 -0400

    Add boolean functions to the datatype Boolean module.

--- Manifold/Module+Boolean.swift
+++ Manifold/Module+Boolean.swift
@@ -2,12 +2,35 @@
 
 extension Module {
 	public static var boolean: Module {
-		return Module("Boolean", [
-			.Datatype("Boolean", [
-				"true": .End,
-				"false": .End
-			])
+		let Boolean = Declaration<Recur>.Datatype("Boolean", [
+			"true": .End,
+			"false": .End
 		])
+
+		let `true`: Recur = "true"
+		let `false`: Recur = "false"
+
+		let not = Declaration("not",
+			type: Recur.FunctionType(Boolean.ref, Boolean.ref),
+			value: Recur.lambda(Boolean.ref, .Type) { b, A in Recur.lambda(A, A) { t, f in b[A, f, t] } })
+
+		let `if` = Declaration("if",
+			type: Recur.lambda(.Type, Boolean.ref) { A, condition in Recur.lambda(A, A, const(A)) },
+			value: Recur.lambda(.Type, Boolean.ref) { A, condition in Recur.lambda(A, A) { condition[A, $0, $1] } })
+
+		let and = Declaration("and",
+			type: Recur.FunctionType(Boolean.ref, Boolean.ref, Boolean.ref),
+			value: Recur.lambda(Boolean.ref, Boolean.ref) { p, q in p[Boolean.ref, q, `false`] })
+
+		let or = Declaration("or",
+			type: Recur.FunctionType(Boolean.ref, Boolean.ref, Boolean.ref),
+			value: Recur.lambda(Boolean.ref, Boolean.ref) { p, q in p[Boolean.ref, `true`, q] })
+
+		let xor = Declaration("xor",
+			type: Recur.FunctionType(Boolean.ref, Boolean.ref, Boolean.ref),
+			value: Recur.lambda(Boolean.ref, Boolean.ref, { p, q in p[Boolean.ref, not.ref[q], q] }))
+
+		return Module("Boolean", [ Boolean, `if`, `and`, `or`, `xor` ])
 	}
 }
 
