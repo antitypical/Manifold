038c08315f0b1ab14e86d8adb7cb06788ef95b0f
Rob Rix <rob.rix@github.com>
2015-10-24 14:09:16 -0400

    Test that hand-encoded booleans are equal to the automatic encodings.

--- ManifoldTests/BooleanTests.swift
+++ ManifoldTests/BooleanTests.swift
@@ -4,11 +4,19 @@ final class BooleanTests: XCTestCase {
 	func testModuleTypechecks() {
 		module.typecheck().forEach { XCTFail($0.description) }
 	}
-}
 
+	func testAutomaticallyEncodedDefinitionsAreEquivalentToHandEncodedDefinitions() {
+		let churchModule = Module<Term>.churchBoolean
+		module.definitions.forEach { symbol, type, value in
+			assert(churchModule.context[symbol], ==, type, message: "\(symbol)")
+			assert(churchModule.environment[symbol], ==, value, message: "\(symbol)")
+		}
+	}
+}
 
 private let module = Module<Term>.boolean
 
 
+import Assertions
 import Manifold
 import XCTest
