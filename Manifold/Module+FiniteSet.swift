5f2a5cd05fe3d42b10e73b7e3e19e36d764d6cb4
Rob Rix <rob.rix@github.com>
2015-10-24 23:15:30 -0400

    Make a plausible stab at FiniteSet’s value.

--- Manifold/Module+FiniteSet.swift
+++ Manifold/Module+FiniteSet.swift
@@ -3,9 +3,10 @@
 extension Module {
 	public static var finiteSet: Module {
 		let Natural: Recur = "Natural"
+		let FiniteSet: Recur = "FiniteSet"
 		let finiteSet = Declaration("FiniteSet",
 			type: Natural --> .Type,
-			value: .Type)
+			value: (Natural, .Type) => { n, A in n[.Type, A --> A, Natural => { (n: Recur) in (FiniteSet[n] --> A) --> A }] })
 
 		return Module("FiniteSet", [ natural ], [ finiteSet ])
 	}
