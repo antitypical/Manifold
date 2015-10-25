38c9ef0512386ddec076c77b5df674ae92826e75
Rob Rix <rob.rix@github.com>
2015-10-24 20:14:32 -0400

    Attempt a better type for Vectors.

--- Manifold/Module+Vector.swift
+++ Manifold/Module+Vector.swift
@@ -3,10 +3,11 @@
 extension Module {
 	public static var vector: Module {
 		let Natural: Recur = "Natural"
-		let Vector = Declaration("Vector",
+		let Vector: Recur = "Vector"
+		let vector = Declaration("Vector",
 			type: .Type --> Natural --> .Type,
-			value: (.Type, Natural) => { _ in .Type })
+			value: (.Type, Natural, .Type) => { A, n, B in (A --> Vector[A, n] --> B) --> B --> B })
 
-		return Module("Vector", [ natural ], [ Vector ])
+		return Module("Vector", [ natural ], [ vector ])
 	}
 }
