f5964a6c9de44f8e76ee519b531a0823c9872dce
Rob Rix <rob.rix@github.com>
2015-10-25 15:24:35 -0400

    :fire: catamorphic elaboration.
    1. It’s strictly inference-mode.
    2. It’s kind of a pain.

--- Manifold/TermType+Elaboration.swift
+++ Manifold/TermType+Elaboration.swift
@@ -42,44 +42,6 @@ extension TermType {
 			throw "unimplemented"
 		}
 	}
-
-	public func elaborate(environment: [Name:Self], _ context: [Name:Self]) -> Either<String, Elaborated<Self>> {
-		func assign(type: Self)(_ to: Self) -> Elaborated<Self> {
-			return .Unroll(type, to.out.map(assign(type)))
-		}
-		return cata {
-			switch $0 {
-			case let .Type(n):
-				return .Right(assign(.Type(n + 1))(self))
-
-			case let .Variable(name):
-				return context[name]
-					.map { Either.Right(assign($0)(self)) }
-					?? Either.Left("Unexpectedly free variable \(name) in context: \(Self.toString(context, separator: ":")), environment: \(Self.toString(environment, separator: "="))")
-
-			case let .Application(a, b):
-				return a.flatMap { A in
-					switch A.type.weakHeadNormalForm(environment).out {
-					case let .Lambda(i, type, body):
-						return b
-							.flatMap { B in
-								Self.equate(B.type, type, environment)
-									? Either.right(B)
-									: Either.Left("Type mismatch: expected '\(self)' to be of type '\(type)', but it was actually of type '\(B.type)' in context: \(Self.toString(context, separator: ":")), environment: \(Self.toString(environment, separator: "="))")
-							}
-							.map { B in
-								.Unroll(body.substitute(i, B.type), Expression.Application(A, B))
-							}
-					default:
-						return Either.Left("Illegal application of \(a) : \(A) to \(b) in context: \(Self.toString(context, separator: ":")), environment: \(Self.toString(environment, separator: "="))")
-					}
-				}
-
-			default:
-				return .Left("unimplemented")
-			}
-		}
-	}
 }
 
 
