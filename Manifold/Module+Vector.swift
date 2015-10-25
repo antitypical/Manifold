ba7e44a21bc1f1404a66eb6d4c0c6d90229314ea
Rob Rix <rob.rix@github.com>
2015-10-24 22:50:14 -0400

    `cons` eliminators don’t need to accept a parameter for `nil`.

--- Manifold/Module+Vector.swift
+++ Manifold/Module+Vector.swift
@@ -6,12 +6,12 @@ extension Module {
 		let Vector: Recur = "Vector"
 		let vector = Declaration("Vector",
 			type: .Type --> Natural --> .Type,
-			value: (.Type, Natural, .Type) => { A, n, B in n[.Type, B --> B, Natural => { n in (A --> Vector[A, n] --> B) --> B --> B }] })
+			value: (.Type, Natural, .Type) => { A, n, B in n[.Type, B --> B, Natural => { n in (A --> Vector[A, n] --> B) --> B }] })
 
 		let successor: Recur = "successor"
 		let cons = Declaration("cons",
 			type: (.Type, Natural)  => { A, n in A --> Vector[A, n] --> Vector[A, successor[n]] },
-			value: (.Type, Natural) => { (A: Recur, n) in (A, Vector[A, n], .Type) => { head, tail, B in (A --> Vector[A, n] --> B, B) => { ifCons, _ in ifCons[head, tail] } } })
+			value: (.Type, Natural) => { (A: Recur, n) in (A, Vector[A, n], .Type) => { head, tail, B in (A --> Vector[A, n] --> B) => { ifCons in ifCons[head, tail] } } })
 
 		let zero: Recur = "zero"
 		let `nil` = Declaration("nil",
