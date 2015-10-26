c175098ad0e37fbcf66aba83b1bdb8dfd454e624
Rob Rix <rob.rix@github.com>
2015-10-25 22:50:43 -0400

    Don’t bother with `stride`.

--- Manifold/Int.swift
+++ Manifold/Int.swift
@@ -2,10 +2,8 @@
 
 extension Int {
 	public func digits(base: Int) -> [Int] {
-		return 0.stride(to: self <= 0 ? 0 : Int(Double(self).log(Double(base))), by: 1)
-			.scan(self) { into, _ in
-				into / base
-			}
+		return (0..<(self <= 0 ? 0 : Int(Double(self).log(Double(base)))))
+			.scan(self) { into, _ in into / base }
 			.map { $0 % base }
 			.reverse()
 	}
