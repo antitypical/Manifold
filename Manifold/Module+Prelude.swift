884d476f0d277d87f0b3d06fba33595a77a9a45f
Rob Rix <rob.rix@github.com>
2015-10-25 23:23:08 -0400

    Replace all uses of TermType.FunctionType.

--- Manifold/Module+Prelude.swift
+++ Manifold/Module+Prelude.swift
@@ -3,16 +3,16 @@
 extension Module {
 	public static var prelude: Module {
 		let identity = Declaration("identity",
-			type: Recur.lambda(.Type) { A in Recur.FunctionType(A, A) },
+			type: Recur.lambda(.Type) { A in A --> A },
 			value: Recur.lambda(.Type) { A in Recur.lambda(A, id) })
 
 		let constant = Declaration("constant",
-			type: Recur.lambda(.Type, .Type) { A, B in Recur.FunctionType(A, B, A) },
+			type: Recur.lambda(.Type, .Type) { A, B in A --> B --> A },
 			value: Recur.lambda(.Type, .Type) { A, B in Recur.lambda(A) { Recur.lambda(B, const($0)) } })
 
 		let flip = Declaration("flip",
-			type: Recur.lambda(.Type, .Type, .Type) { A, B, C in Recur.FunctionType(Recur.FunctionType(A, B, C), Recur.FunctionType(B, A, C)) },
-			value: Recur.lambda(.Type, .Type, .Type) { A, B, C in Recur.lambda(Recur.FunctionType(A, B, C)) { f in Recur.lambda(B, A) { b, a in f[a, b] } } })
+			type: Recur.lambda(.Type, .Type, .Type) { A, B, C in ((A --> B --> C) --> (B --> A --> C)) },
+			value: Recur.lambda(.Type, .Type, .Type) { A, B, C in Recur.lambda((A --> B --> C)) { f in Recur.lambda(B, A) { b, a in f[a, b] } } })
 
 		return Module("Prelude", [ identity, constant, flip ])
 	}
