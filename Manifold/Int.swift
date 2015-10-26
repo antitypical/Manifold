5094f95fbdb8415ac5d7931ece61153de0af7553
Rob Rix <rob.rix@github.com>
2015-10-25 23:04:47 -0400

    Move the negative handling outside of the Repeat.

--- Manifold/Int.swift
+++ Manifold/Int.swift
@@ -2,7 +2,7 @@
 
 extension Int {
 	public func digits(base: Int) -> [Int] {
-		return Repeat(count: self <= 0 ? 0 : Int(log(Double(self)) / log(Double(base))), repeatedValue: 0)
+		return self <= 0 ? [self % base] : Repeat(count: Int(log(Double(self)) / log(Double(base))), repeatedValue: 0)
 			.reduce(([self % base], self)) { into, _ -> ([Int], Int) in ([ (into.1 / base) % base ] + into.0, into.1 / base) }.0
 	}
 }
