871d8f0c44478aaad4e73f1b96a7a9f26e7d5ecf
Rob Rix <rob.rix@github.com>
2015-10-25 22:56:27 -0400

    Use Darwin.log.

--- Manifold/Int.swift
+++ Manifold/Int.swift
@@ -2,9 +2,12 @@
 
 extension Int {
 	public func digits(base: Int) -> [Int] {
-		return Array(count: self <= 0 ? 0 : Int(Double(self).log(Double(base))), repeatedValue: 0)
+		return Array(count: self <= 0 ? 0 : Int(log(Double(self)) / log(Double(base))), repeatedValue: 0)
 			.reduce([self]) { into, _ in into + [ into.last! / base ] }
 			.map { $0 % base }
 			.reverse()
 	}
 }
+
+
+import Darwin
