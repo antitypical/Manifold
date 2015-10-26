8cf36e65b872721ee65ffae1366e41a2bd6e76f0
Rob Rix <rob.rix@github.com>
2015-10-25 23:00:55 -0400

    Combine the fold and map.

--- Manifold/Int.swift
+++ Manifold/Int.swift
@@ -3,8 +3,7 @@
 extension Int {
 	public func digits(base: Int) -> [Int] {
 		return Array(count: self <= 0 ? 0 : Int(log(Double(self)) / log(Double(base))), repeatedValue: 0)
-			.reduce([self]) { into, _ in into + [ into.last! / base ] }
-			.map { $0 % base }
+			.reduce(([self % base], self)) { into, _ -> ([Int], Int) in (into.0 + [ (into.1 / base) % base ], into.1 / base) }.0
 			.reverse()
 	}
 }
