cd77091e17c0375eaf0784ff91970b85e2e92852
Rob Rix <rob.rix@github.com>
2015-10-25 22:53:09 -0400

    Compute the digits with a fold and not a scan.

--- Manifold/Int.swift
+++ Manifold/Int.swift
@@ -3,7 +3,7 @@
 extension Int {
 	public func digits(base: Int) -> [Int] {
 		return (0..<(self <= 0 ? 0 : Int(Double(self).log(Double(base)))))
-			.scan(self) { into, _ in into / base }
+			.reduce([self]) { into, _ in into + [ into.last! / base ] }
 			.map { $0 % base }
 			.reverse()
 	}
