569cdf92d30762dab56272db4197ddb651e7577a
Rob Rix <rob.rix@github.com>
2015-10-25 23:01:41 -0400

    Don’t allocate an array.

--- Manifold/Int.swift
+++ Manifold/Int.swift
@@ -2,7 +2,7 @@
 
 extension Int {
 	public func digits(base: Int) -> [Int] {
-		return Array(count: self <= 0 ? 0 : Int(log(Double(self)) / log(Double(base))), repeatedValue: 0)
+		return Repeat(count: self <= 0 ? 0 : Int(log(Double(self)) / log(Double(base))), repeatedValue: 0)
 			.reduce(([self % base], self)) { into, _ -> ([Int], Int) in (into.0 + [ (into.1 / base) % base ], into.1 / base) }.0
 			.reverse()
 	}
