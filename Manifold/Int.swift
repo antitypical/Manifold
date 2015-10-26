8c550e863928fcfc4b319aff8e052d6dea4ef079
Rob Rix <rob.rix@github.com>
2015-10-25 23:02:17 -0400

    Combine the reverse into the fold.

--- Manifold/Int.swift
+++ Manifold/Int.swift
@@ -3,8 +3,7 @@
 extension Int {
 	public func digits(base: Int) -> [Int] {
 		return Repeat(count: self <= 0 ? 0 : Int(log(Double(self)) / log(Double(base))), repeatedValue: 0)
-			.reduce(([self % base], self)) { into, _ -> ([Int], Int) in (into.0 + [ (into.1 / base) % base ], into.1 / base) }.0
-			.reverse()
+			.reduce(([self % base], self)) { into, _ -> ([Int], Int) in ([ (into.1 / base) % base ] + into.0, into.1 / base) }.0
 	}
 }
 
