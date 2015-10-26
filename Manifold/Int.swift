041d5f92c2036d07e5c3a7d40da2f5072db7f270
Rob Rix <rob.rix@github.com>
2015-10-25 23:06:49 -0400

    Factor out the common base.

--- Manifold/Int.swift
+++ Manifold/Int.swift
@@ -2,8 +2,8 @@
 
 extension Int {
 	public func digits(base: Int) -> [Int] {
-		return self <= 0 ? [self % base] : Repeat(count: Int(log(Double(self)) / log(Double(base))), repeatedValue: 0)
-			.reduce(([self % base], self)) { into, _ -> ([Int], Int) in ([ (into.1 / base) % base ] + into.0, into.1 / base) }.0
+		return (self <= 0 ? [] : Repeat(count: Int(log(Double(self)) / log(Double(base))), repeatedValue: 0)
+			.reduce(([], self)) { into, _ -> ([Int], Int) in ([ (into.1 / base) % base ] + into.0, into.1 / base) }.0) + [self % base]
 	}
 }
 
