e6f03b78c28e9b4bbfacfc48d0e52fc0f0f7dd0c
Rob Rix <rob.rix@github.com>
2015-10-27 03:42:34 -0400

    Substitution is defined over Term.

--- Manifold/Term+Substitution.swift
+++ Manifold/Term+Substitution.swift
@@ -1,13 +1,13 @@
 //  Copyright © 2015 Rob Rix. All rights reserved.
 
-extension TermType {
-	public func substitute(i: Int, _ expression: Self) -> Self {
-		return cata { (t: Expression<Self>) in
+extension Term {
+	public func substitute(i: Int, _ expression: Term) -> Term {
+		return cata { (t: Expression<Term>) in
 			switch t {
 			case let .Variable(.Local(j)) where i == j:
 				return expression
 			default:
-				return Self(t)
+				return Term(t)
 			}
 		}
 	}
