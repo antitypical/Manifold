0cae95808543db94742ade958a041d6abf46bf57
Rob Rix <rob.rix@github.com>
2015-10-27 03:46:37 -0400

    Term is no longer a TermType.

--- Manifold/Term.swift
+++ Manifold/Term.swift
@@ -1,10 +1,8 @@
 //  Copyright © 2015 Rob Rix. All rights reserved.
 
-public enum Term: TermType, TermContainerType {
+public enum Term: IntegerLiteralConvertible, StringLiteralConvertible, TermContainerType {
 	case In(() -> Expression<Term>)
 
-
-	// MARK: TermType
 
 	public init(_ expression: () -> Expression<Term>) {
 		self = .In(expression)
