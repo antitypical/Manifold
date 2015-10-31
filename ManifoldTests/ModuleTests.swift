fca25acd02b24de666a7e45abc5c5359beeaad43
Rob Rix <rob.rix@github.com>
2015-10-30 20:58:12 -0400

    Test all modules in the directory.

---
+++ ManifoldTests/ModuleTests.swift
@@ -0,0 +1,13 @@
+//  Copyright © 2015 Rob Rix. All rights reserved.
+
+final class ModuleTests: XCTestCase {
+	func testModulesTypecheck() {
+		for (_, module) in Module.modules {
+			module.typecheck().forEach { XCTFail($0) }
+		}
+	}
+}
+
+
+import Manifold
+import XCTest
