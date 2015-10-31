696db22ca8582eacaa6178b54f199385de29a0f8
Rob Rix <rob.rix@github.com>
2015-10-31 13:59:12 -0400

    Implement List.map recursively over cons cells.

--- Manifold/Module+List.swift
+++ Manifold/Module+List.swift
@@ -11,9 +11,10 @@ extension Module {
 
 		let cons: Term = "cons"
 		let `nil`: Term = "nil"
+		let listMap: Term = "List.map"
 		let map = Declaration("List.map",
 			type: (.Type, .Type) => { A, B in (A --> B) --> List.ref[A] --> List.ref[B] },
-			value: { A, B in (A --> B, nil) => { transform, list in list[List.ref[B], () => { cons[B, transform[$0]] }, `nil`[B]] } })
+			value: { A, B in (A --> B, nil) => { transform, list in list[List.ref[B], () => { cons[B, transform[$0], listMap[A, B, transform, $1]] }, `nil`[B]] } })
 
 		return Module("List", [ List, map ])
 	}
