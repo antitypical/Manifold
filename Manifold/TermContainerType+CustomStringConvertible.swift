ccfd1db9b40d48c4b9f89e3a0362da3ff47da52c
Rob Rix <rob.rix@github.com>
2015-12-13 09:37:57 -0500

    Single-quote embedded values.
    This makes it a little clearer when e.g. we’re referring to `String` (a
    term in scope) vs. `Swift.String` (an embedded value used to define the
    term).

--- Manifold/TermContainerType+CustomStringConvertible.swift
+++ Manifold/TermContainerType+CustomStringConvertible.swift
@@ -29,7 +29,7 @@ extension TermContainerType {
 				return ("λ \(Name.Local(variable)) . \(body)", true)
 
 			case let .Embedded(value, (_, (type, _))):
-				return ("\(value) : \(type)", true)
+				return ("'\(value)' : \(type)", true)
 			}
 		}
 		return out
