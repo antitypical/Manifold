2904ad9d2dc05ea2860ecb4e9414282804802ff6
Rob Rix <rob.rix@github.com>
2015-12-13 11:02:03 -0500

    Use guard clauses to tidy up evaluation.

--- Manifold/Term+Evaluation.swift
+++ Manifold/Term+Evaluation.swift
@@ -4,17 +4,13 @@ extension Term {
 	public func evaluate(environment: [Name:Term] = [:]) throws -> Term {
 		switch out {
 		case let .Variable(i):
-			if let found = environment[i] {
-				return found
-			}
-			throw "Illegal free variable \(i)"
+			guard let found = environment[i] else { throw "Illegal free variable \(i)" }
+			return found
 
 		case let .Application(a, b):
 			let a = try a.evaluate(environment)
-			if case let .Lambda(i, _, body) = a.out {
-				return try body.substitute(i, b.evaluate(environment)).evaluate(environment)
-			}
-			throw "Illegal application of non-lambda term \(a) to \(b)"
+			guard case let .Lambda(i, _, body) = a.out else { throw "Illegal application of non-lambda term \(a) to \(b)" }
+			return try body.substitute(i, b.evaluate(environment)).evaluate(environment)
 
 		default:
 			return self
