a9e14b9a247c1d24a7ddbb84e5fa58d51a6abf56
Rob Rix <rob.rix@github.com>
2015-12-20 00:08:05 -0500

    Recur directly in `equate`.

--- Manifold/Term+DefinitionalEquality.swift
+++ Manifold/Term+DefinitionalEquality.swift
@@ -2,10 +2,6 @@
 
 extension Term {
 	public static func equate(left: Term, _ right: Term, _ environment: [Name:Term]) -> Term? {
-		let recur: (Term, Term) -> Term? = {
-			equate($0, $1, environment)
-		}
-
 		let normalize: Term -> Term = { term in
 			term.weakHeadNormalForm(environment, shouldRecur: false)
 		}
@@ -26,11 +22,11 @@ extension Term {
 			return right
 
 		case let (.Application(a1, a2), .Application(b1, b2)):
-			guard let first = recur(a1, b1), second = recur(a2, b2) else { return nil }
+			guard let first = equate(a1, b1, environment), second = equate(a2, b2, environment) else { return nil }
 			return .Application(first, second)
 
 		case let (.Lambda(_, a1, a2), .Lambda(i, b1, b2)):
-			guard let type = recur(a1, b1), body = recur(a2, b2) else { return nil }
+			guard let type = equate(a1, b1, environment), body = equate(a2, b2, environment) else { return nil }
 			return .Lambda(i, type, body)
 
 		default:
