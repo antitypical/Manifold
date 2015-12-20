f080077c7f3520b0f14dc00034080fba273b68f7
Rob Rix <rob.rix@github.com>
2015-12-20 13:28:37 -0500

    Diff/unify terms recursively.

--- Manifold/TermDiff.swift
+++ Manifold/TermDiff.swift
@@ -7,4 +7,32 @@ enum TermDiff {
 	init(_ term: Term) {
 		self = .Roll(term.out.map(TermDiff.init))
 	}
+
+	init(_ left: Term, _ right: Term, _ environment: [Name:Term], var _ visited: Set<Term> = []) {
+		let (leftʹ, visitedLeft) = left.weakHeadNormalForm(environment, shouldRecur: true, visited: visited)
+		visited.unionInPlace(visitedLeft)
+		let (rightʹ, visitedRight) = right.weakHeadNormalForm(environment, shouldRecur: true, visited: visited)
+		visited.unionInPlace(visitedRight)
+
+		guard leftʹ != rightʹ else { self = TermDiff(right) ; return }
+
+		switch (leftʹ.out, rightʹ.out) {
+		case (.Implicit, _):
+			self = TermDiff(right)
+		case (_, .Implicit):
+			self = TermDiff(left)
+
+		case (.Type, .Type):
+			self = TermDiff(right)
+
+		case let (.Application(a1, b1), .Application(a2, b2)):
+			self = .Roll(.Application(TermDiff(a1, a2, environment, visited), TermDiff(b1, b2, environment, visited)))
+
+		case let (.Lambda(_, type1, body1), .Lambda(i, type2, body2)):
+			self = .Roll(.Lambda(i, TermDiff(type1, type2, environment, visited), TermDiff(body1, body2, environment, visited)))
+
+		default:
+			self = .Patch(left, right)
+		}
+	}
 }
