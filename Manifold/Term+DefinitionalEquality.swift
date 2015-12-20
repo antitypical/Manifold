00c79a9433539b79aab8c359cec78fcaee622964
Rob Rix <rob.rix@github.com>
2015-12-20 13:52:27 -0500

    Remove the public `visited` set parameter.

--- Manifold/Term+DefinitionalEquality.swift
+++ Manifold/Term+DefinitionalEquality.swift
@@ -1,7 +1,7 @@
 //  Copyright © 2015 Rob Rix. All rights reserved.
 
 extension Term {
-	public static func equate(left: Term, _ right: Term, _ environment: [Name:Term], var visited: Set<Term> = []) -> Term? {
+	public static func equate(left: Term, _ right: Term, _ environment: [Name:Term]) -> Term? {
 		return TermDiff(left, right, environment).unified
 	}
 }
