532672ad8ea4d563824ef1d7eab985a4b4924fb1
Rob Rix <rob.rix@github.com>
2015-12-20 13:33:27 -0500

    Use an inner `unify` function.

--- Manifold/TermDiff.swift
+++ Manifold/TermDiff.swift
@@ -9,30 +9,33 @@ enum TermDiff {
 	}
 
 	init(_ left: Term, _ right: Term, _ environment: [Name:Term], var _ visited: Set<Term> = []) {
-		let (leftʹ, visitedLeft) = left.weakHeadNormalForm(environment, shouldRecur: true, visited: visited)
-		visited.unionInPlace(visitedLeft)
-		let (rightʹ, visitedRight) = right.weakHeadNormalForm(environment, shouldRecur: true, visited: visited)
-		visited.unionInPlace(visitedRight)
+		func unify(left: Term, _ right: Term) -> TermDiff {
+			let (leftʹ, visitedLeft) = left.weakHeadNormalForm(environment, shouldRecur: true, visited: visited)
+			visited.unionInPlace(visitedLeft)
+			let (rightʹ, visitedRight) = right.weakHeadNormalForm(environment, shouldRecur: true, visited: visited)
+			visited.unionInPlace(visitedRight)
 
-		guard leftʹ != rightʹ else { self = TermDiff(right) ; return }
+			guard leftʹ != rightʹ else { return TermDiff(right) }
 
-		switch (leftʹ.out, rightʹ.out) {
-		case (.Implicit, _):
-			self = TermDiff(right)
-		case (_, .Implicit):
-			self = TermDiff(left)
+			switch (leftʹ.out, rightʹ.out) {
+			case (.Implicit, _):
+				return TermDiff(right)
+			case (_, .Implicit):
+				return TermDiff(left)
 
-		case (.Type, .Type):
-			self = TermDiff(right)
+			case (.Type, .Type):
+				return TermDiff(right)
 
-		case let (.Application(a1, b1), .Application(a2, b2)):
-			self = .Roll(.Application(TermDiff(a1, a2, environment, visited), TermDiff(b1, b2, environment, visited)))
+			case let (.Application(a1, b1), .Application(a2, b2)):
+				return .Roll(.Application(unify(a1, a2), unify(b1, b2)))
 
-		case let (.Lambda(_, type1, body1), .Lambda(i, type2, body2)):
-			self = .Roll(.Lambda(i, TermDiff(type1, type2, environment, visited), TermDiff(body1, body2, environment, visited)))
+			case let (.Lambda(_, type1, body1), .Lambda(i, type2, body2)):
+				return .Roll(.Lambda(i, unify(type1, type2), unify(body1, body2)))
 
-		default:
-			self = .Patch(left, right)
+			default:
+				return .Patch(left, right)
+			}
 		}
+		self = unify(left, right)
 	}
 }
