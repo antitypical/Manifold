2c40b7358f68eb75e3b21141f3a039d86697737b
Rob Rix <rob.rix@github.com>
2015-12-19 19:24:20 -0500

    Substitution does not replace shadowed variables.

--- Manifold/Term+Substitution.swift
+++ Manifold/Term+Substitution.swift
@@ -2,13 +2,19 @@
 
 extension Term {
 	public func substitute(i: Int, _ expression: Term) -> Term {
-		return cata { (t: Expression<Term>) in
-			switch t {
-			case let .Variable(.Local(j)) where i == j:
-				return expression
-			default:
-				return Term(t)
-			}
+		switch out {
+		case let .Variable(.Local(j)) where i == j:
+			return expression
+		case let .Lambda(j, type, body):
+			return .Lambda(j, type?.substitute(i, expression), i == j
+				? body
+				: body.substitute(i, expression))
+		case let .Application(a, b):
+			return .Application(a.substitute(i, expression), b.substitute(i, expression))
+		case let .Embedded(a, eq, type):
+			return .Embedded(a, eq, type.substitute(i, expression))
+		default:
+			return self
 		}
 	}
 }
