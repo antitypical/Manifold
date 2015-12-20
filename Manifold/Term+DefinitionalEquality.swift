22248d6457edcb158cfe63edfa0f8973446f2a48
Rob Rix <rob.rix@github.com>
2015-12-20 00:51:20 -0500

    Equate terms with respect to a set of visited terms.

--- Manifold/Term+DefinitionalEquality.swift
+++ Manifold/Term+DefinitionalEquality.swift
@@ -1,13 +1,13 @@
 //  Copyright © 2015 Rob Rix. All rights reserved.
 
 extension Term {
-	public static func equate(left: Term, _ right: Term, _ environment: [Name:Term]) -> Term? {
-		let normalize: Term -> Term = { term in
-			term.weakHeadNormalForm(environment, shouldRecur: false)
-		}
+	public static func equate(left: Term, _ right: Term, _ environment: [Name:Term], var visited: Set<Term> = []) -> Term? {
+		if left == right { return right }
 
-		let leftʹ = normalize(left)
-		let rightʹ = normalize(right)
+		let (leftʹ, visitedLeft) = left.weakHeadNormalForm(environment, shouldRecur: false, visited: visited)
+		let (rightʹ, visitedRight) = right.weakHeadNormalForm(environment, shouldRecur: false, visited: visited)
+		visited.unionInPlace(visitedLeft)
+		visited.unionInPlace(visitedRight)
 
 		if leftʹ == rightʹ { return rightʹ }
 
@@ -22,11 +22,11 @@ extension Term {
 			return rightʹ
 
 		case let (.Application(a1, a2), .Application(b1, b2)):
-			guard let first = equate(a1, b1, environment), second = equate(a2, b2, environment) else { return nil }
+			guard let first = equate(a1, b1, environment, visited: visited), second = equate(a2, b2, environment, visited: visited) else { return nil }
 			return .Application(first, second)
 
 		case let (.Lambda(_, a1, a2), .Lambda(i, b1, b2)):
-			guard let type = equate(a1, b1, environment), body = equate(a2, b2, environment) else { return nil }
+			guard let type = equate(a1, b1, environment, visited: visited), body = equate(a2, b2, environment, visited: visited) else { return nil }
 			return .Lambda(i, type, body)
 
 		default:
