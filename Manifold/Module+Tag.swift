447cc08a89b766504b3e692ea015e6a293cd202b
Rob Rix <rob.rix@github.com>
2015-12-20 19:39:34 -0500

    Add a `here` binding.

--- Manifold/Module+Tag.swift
+++ Manifold/Module+Tag.swift
@@ -3,6 +3,7 @@
 extension Module {
 	public static var tag: Module {
 		let List: Term = "List"
+		let cons: Term = "cons"
 		let String: Term = "String"
 		let Enum = Declaration("Enum",
 			type: .Type,
@@ -12,6 +13,10 @@ extension Module {
 			type: Enum.ref --> .Type,
 			value: (Enum.ref, .Type) => { e, Motive in (String --> Motive) --> Motive })
 
-		return Module("Tag", [ list, string ], [ Enum, Tag ])
+		let here = Declaration("here",
+			type: (String, List[String]) => { l, E in Tag.ref[cons[nil, l, E]] },
+			value: (String, List[String], .Type) => { s, _, Motive in (String --> Motive) => { f in f[s] }  })
+
+		return Module("Tag", [ list, string ], [ Enum, Tag, here ])
 	}
 }
