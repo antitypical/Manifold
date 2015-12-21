ad9bb65888661c66eaef6dfd7fd22ff9e2f2a0fe
Rob Rix <rob.rix@github.com>
2015-12-20 22:22:40 -0500

    Define `El` by eliminating datatypes.

--- Manifold/Module+Datatype.swift
+++ Manifold/Module+Datatype.swift
@@ -19,7 +19,21 @@ extension Module {
 			type: .Type --> .Type(1),
 			value: .Type => { I in I --> .Type })
 
+		let Identical: Term = "≡"
+		let Pair: Term = "Pair"
+		let Sigma: Term = "Sigma"
 		let El: Term = "El"
+		let el = Declaration("El",
+			type: .Type => { I in _Datatype[I] --> (I --> .Type) --> I --> .Type },
+			value: .Type => { I in
+				(_Datatype[I], IType.ref[I], I) => { D, X, i in
+					D[.Type,
+						I => { j in Identical[I, i, j] },
+						(I, _Datatype[I]) => { j, D in Pair[X[j], El[D, X, i]] },
+						(.Type) => { A in (A --> _Datatype[I]) => { B in Sigma[A, nil => { a in El[B[a], X, i] }] } }]
+				}
+			})
+
 		let `init` = Declaration("init",
 			type: nil => { I in (_Datatype[I], I) => { D, i in El[D, μ.ref[D], i] --> μ.ref[D, i] } },
 			value: nil)
