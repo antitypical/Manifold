b3ab1ecd3d186a8ea9061fcf40b0179a70d3a3df
Rob Rix <rob.rix@github.com>
2015-12-25 17:15:35 -0500

    Evaluate over scoping.

--- Manifold/Term+Evaluation.swift
+++ Manifold/Term+Evaluation.swift
@@ -2,12 +2,12 @@
 
 extension Term {
 	public func evaluate(environment: [Name:Term] = [:]) throws -> Term {
-		switch out {
+		switch scoping {
 		case let .Variable(i):
 			guard let found = environment[i] else { throw "Illegal free variable \(i)" }
 			return found
 
-		case let .Application(a, b):
+		case let .Identity(.Application(a, b)):
 			let aʹ = try a.evaluate(environment)
 			switch aʹ.out {
 			case let .Lambda(i, _, body):
