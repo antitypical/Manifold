b2f1a1b20a007b38cfaac70943165f4baa0cde65
Rob Rix <rob.rix@github.com>
2015-12-25 18:28:31 -0500

    Scoping is specific to Expression.
    This simplifies quite a lot of things.

--- Manifold/Scoping.swift
+++ Manifold/Scoping.swift
@@ -1,21 +1,21 @@
 //  Copyright © 2015 Rob Rix. All rights reserved.
 
-public enum Scoping<Syntax, Term> {
+public enum Scoping<Term> {
 	case Variable(Name)
 	case Abstraction(Name, Term)
-	case Identity(Syntax)
+	case Identity(Expression<Term>)
 
 
 	// MARK: Equatable
 
-	public static func equal(syntaxEqual syntaxEqual: (Syntax, Syntax) -> Bool, termEqual: (Term, Term) -> Bool)(_ left: Scoping, _ right: Scoping) -> Bool {
+	public static func equal(termEqual: (Term, Term) -> Bool)(_ left: Scoping, _ right: Scoping) -> Bool {
 		switch (left, right) {
 		case let (.Variable(name1), .Variable(name2)):
 			return name1 == name2
 		case let (.Abstraction(name1, scope1), .Abstraction(name2, scope2)):
 			return name1 == name2 && termEqual(scope1, scope2)
-		case let (.Identity(syntax1), .Identity(syntax2)):
-			return syntaxEqual(syntax1, syntax2)
+		case let (.Identity(expression1), .Identity(expression2)):
+			return Expression.equal(termEqual)(expression1, expression2)
 		default:
 			return false
 		}
@@ -23,6 +23,6 @@ public enum Scoping<Syntax, Term> {
 }
 
 
-public func == <Syntax: Equatable, Term: Equatable> (left: Scoping<Syntax, Term>, right: Scoping<Syntax, Term>) -> Bool {
-	return Scoping.equal(syntaxEqual: ==, termEqual: ==)(left, right)
+public func == <Term: Equatable> (left: Scoping<Term>, right: Scoping<Term>) -> Bool {
+	return Scoping.equal(==)(left, right)
 }
