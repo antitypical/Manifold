7028c62d09800cf31823f81aace18ad6dce95cb6
Rob Rix <rob.rix@github.com>
2015-12-25 14:55:32 -0500

    Move Scoping to its own file.

--- Manifold/ABT.swift
+++ Manifold/ABT.swift
@@ -1,31 +1,5 @@
 //  Copyright © 2015 Rob Rix. All rights reserved.
 
-public enum Scoping<Syntax, Term> {
-	case Variable(Name)
-	case Abstraction(Name, Term)
-	case Identity(Syntax)
-
-
-	public static func equal(syntaxEqual syntaxEqual: (Syntax, Syntax) -> Bool, termEqual: (Term, Term) -> Bool)(_ left: Scoping, _ right: Scoping) -> Bool {
-		switch (left, right) {
-		case let (.Variable(name1), .Variable(name2)):
-			return name1 == name2
-		case let (.Abstraction(name1, scope1), .Abstraction(name2, scope2)):
-			return name1 == name2 && termEqual(scope1, scope2)
-		case let (.Identity(syntax1), .Identity(syntax2)):
-			return syntaxEqual(syntax1, syntax2)
-		default:
-			return false
-		}
-	}
-}
-
-
-public func == <Syntax: Equatable, Term: Equatable> (left: Scoping<Syntax, Term>, right: Scoping<Syntax, Term>) -> Bool {
-	return Scoping.equal(syntaxEqual: ==, termEqual: ==)(left, right)
-}
-
-
 indirect enum ABTTerm {
 	case In(Set<Name>, Scoping<AST<ABTTerm>, ABTTerm>)
 
