4023bc709a08523b5b100d7581dfd87a9ce37a28
Rob Rix <rob.rix@github.com>
2015-12-25 14:25:57 -0500

    Move MonoidType to its own file.

--- Manifold/ABT.swift
+++ Manifold/ABT.swift
@@ -88,11 +88,6 @@ enum AST<Recur> {
 	}
 }
 
-protocol MonoidType {
-	static var mempty: Self { get }
-	func mappend(other: Self) -> Self
-}
-
 extension Set: MonoidType {
 	static var mempty: Set {
 		return []
