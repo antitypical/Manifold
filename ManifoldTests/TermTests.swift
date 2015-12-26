2a41c7d60f77df0c5ab6ae46cc2867fc64b61e4a
Rob Rix <rob.rix@github.com>
2015-12-25 20:11:25 -0500

    Correct some more tests.

--- ManifoldTests/TermTests.swift
+++ ManifoldTests/TermTests.swift
@@ -22,8 +22,8 @@ final class TermTests: XCTestCase {
 
 	func testHigherOrderConstruction() {
 		assert(.Type => id, ==, .Lambda(.Local(0), .Type, 0))
-		assert(identity.value, ==, .Lambda(.Type, .Lambda(1, 0)))
-		assert(constant, ==, .Lambda(.Type, .Lambda(.Type, .Lambda(2, .Lambda(1, 0)))))
+		assert(identity.value, ==, .Lambda(.Local(1), .Type, .Lambda(.Local(0), 1, 0)))
+		assert(constant, ==, .Lambda(.Local(3), .Type, .Lambda(.Local(2), .Type, .Lambda(.Local(1), 2, .Lambda(.Local(0), 1, 0)))))
 	}
 
 	func testChurchEncodedBooleanConstruction() {
