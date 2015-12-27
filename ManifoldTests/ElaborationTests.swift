8ea52707bb84e9d91912703ed382acecbfd32c42
Rob Rix <rob.rix@github.com>
2015-12-26 21:46:29 -0500

    Test the elaboration of checked function types.

--- ManifoldTests/ElaborationTests.swift
+++ ManifoldTests/ElaborationTests.swift
@@ -5,6 +5,10 @@ final class ElaborationTests: XCTestCase {
 		let term: Term = .Type
 		assert(try? term.elaborateType(nil, [:], [:]), ==, .Unroll(.Type(1), .Identity(.Type(0))))
 	}
+
+	func testChecksLambdasAgainstFunctionTypes() {
+		assert(try? Term.Lambda(.Local(0), .Type, 0).elaborateType(.Type --> .Type, [:], [:]), ==, .Unroll(.Type --> .Type, .Identity(.Lambda(.Unroll(.Type, .Identity(.Type(0))), .Unroll(.Type, .Abstraction(.Local(0), .Unroll(.Type, .Variable(.Local(0)))))))))
+	}
 }
 
 
