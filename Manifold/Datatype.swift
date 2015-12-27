319114c3f95343b364881854de27bedaccd5acce
Rob Rix <rob.rix@github.com>
2015-12-27 18:41:49 -0500

    Leave space for the motive type.

--- Manifold/Datatype.swift
+++ Manifold/Datatype.swift
@@ -54,15 +54,13 @@ public enum Datatype: DictionaryLiteralConvertible {
 		case let .Argument(type, rest):
 			return (name, type) => self.value(symbol, telescope: rest, constructors: constructors, index: index + 1, parameters: parameters + [ .Variable(name) ])(recur)
 		case .End:
-			return .Type => { motive in
-				constructors.map {
-					($0, $1.fold(recur, terminal: motive, index: index, combine: -->))
-				}.reverse().reduce(id, combine: { into, each in
-					each.0 == symbol
-						? { _ in each.1 => { into(parameters.reduce($0, combine: { $0[$1] })) } }
-						: into >>> { each.1 --> $0 }
-				})(motive)
-			}
+			return (name, .Type) => constructors.map {
+				($0, $1.fold(recur, terminal: .Variable(name), index: index + 1, combine: -->))
+			}.reverse().reduce(id, combine: { into, each in
+				each.0 == symbol
+					? { _ in each.1 => { into(parameters.reduce($0, combine: { $0[$1] })) } }
+					: into >>> { each.1 --> $0 }
+			})(.Variable(name))
 		}
 	}
 
