f4e9197dc9c077054a2fb175681f9bb3a492c797
Rob Rix <rob.rix@github.com>
2015-12-26 21:26:07 -0500

    Test that the type of Type is elaborated.

--- ManifoldTests/ElaborationTests.swift
+++ ManifoldTests/ElaborationTests.swift
@@ -1,8 +1,13 @@
 //  Copyright © 2015 Rob Rix. All rights reserved.
 
 final class ElaborationTests: XCTestCase {
-	
+	func testElaboratesTheTypeOfType() {
+		let term: Term = .Type
+		assert(try? term.elaborateType(nil, [:], [:]), ==, AnnotatedTerm<Term>.Unroll(.Type(1), .Identity(.Type(0))))
+	}
 }
 
+
+import Assertions
 import Manifold
 import XCTest
