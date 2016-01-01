ead002fd57cbc66abaf10607d8df3e7d2a9500ce
Rob Rix <rob.rix@github.com>
2016-01-01 00:22:50 -0500

    Annotate a type.

--- Manifold/Module+Sigma.swift
+++ Manifold/Module+Sigma.swift
@@ -10,7 +10,7 @@ extension Module {
 
 		let first = Declaration("first",
 			type: nil => { A in (A --> .Type) => { B in Sigma.ref[A, B] --> A } },
-			value: nil => { A in nil => { B in nil => { v in v[nil, nil => { x in B[x] => const(x) }] } } })
+			value: nil => { A in nil => { B in nil => { v in v[A, nil => { x in B[x] => const(x) }] } } })
 
 		let second = Declaration("second",
 			type: nil => { A in (A --> .Type) => { B in Sigma.ref[A, B] => { v in B[first.ref[A, B, v]] } } },
